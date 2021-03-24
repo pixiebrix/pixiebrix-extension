6d304b2d84c2d7db38d2f49616ecce9c7a4b2e56
Todd Schiller <todd.schiller@gmail.com>
2021-03-24 09:20:40 -0400

    302: custom modal support in flows

---
+++ src/blocks/transformers/modal.tsx
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2021 Pixie Brix, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "@rjsf/core";
+import { Transformer } from "@/types";
+import { BlockArg, Schema } from "@/core";
+import { registerBlock } from "@/blocks/registry";
+import { v4 as uuidv4 } from "uuid";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const theme = require("!!raw-loader!bootstrap/dist/css/bootstrap.min.css?esModule=false")
+  .default;
+
+export class ModalTransformer extends Transformer {
+  constructor() {
+    super(
+      "@pixiebrix/form-modal",
+      "Show a modal form",
+      "Show a modal form and return the input",
+      "faCode"
+    );
+  }
+
+  inputSchema: Schema = {
+    type: "object",
+    properties: {
+      schema: {
+        type: "object",
+        additionalProperties: true,
+      },
+      uiSchema: {
+        type: "object",
+        additionalProperties: true,
+      },
+    },
+    required: ["schema"],
+  };
+
+  async transform({ schema, uiSchema = {} }: BlockArg): Promise<object> {
+    const container = document.createElement("div");
+    const shadowRoot = container.attachShadow({ mode: "closed" });
+
+    document.body.append(container);
+
+    const resultPromise = new Promise<object>((resolve, reject) => {
+      const id = `modal-${uuidv4()}`;
+      const form = (
+        <React.Fragment>
+          <style
+            type="text/css"
+            dangerouslySetInnerHTML={{ __html: theme.toString() }}
+          />
+          <div className="modal-backdrop show"></div>
+          <div
+            id={id}
+            className="modal show"
+            tabIndex={-1}
+            role="dialog"
+            style={{ display: "block", paddingRight: 15 }}
+          >
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    onSubmit={({ formData }) => {
+                      resolve(formData as object);
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+      ReactDOM.render(form, shadowRoot);
+      $(`#${id}`).on("hide.bs.modal", () =>
+        reject(new Error("User closed modal"))
+      );
+    });
+
+    const data = await resultPromise;
+    container.remove();
+    return data;
+  }
+}
+
+registerBlock(new ModalTransformer());
