51e2bbbf7a04b0044650e5fca8b40a89c3cad5fb
Todd Schiller <todd.schiller@gmail.com>
2021-04-19 20:33:33 -0400

    #357: include serialized error in deployment alert

--- src/blocks/combinators.ts
+++ src/blocks/combinators.ts
@@ -46,6 +46,7 @@ import { getLoggingConfig } from "@/background/logging";
 import { NotificationCallbacks, notifyProgress } from "@/contentScript/notify";
 import { sendDeploymentAlert } from "@/background/telemetry";
 import { reportError } from "@/telemetry/logging";
+import { serializeError } from "serialize-error";
 
 export type ReaderConfig =
   | string
@@ -376,15 +377,20 @@ export async function reducePipeline(
     } catch (ex) {
       if (stage.onError?.alert) {
         if (logger.context.deploymentId) {
-          sendDeploymentAlert({
-            deploymentId: logger.context.deploymentId,
-            data: {
-              id: stage.id,
-              args: currentArgs,
-            },
-          }).catch((err) => {
+          try {
+            sendDeploymentAlert({
+              deploymentId: logger.context.deploymentId,
+              data: {
+                id: stage.id,
+                args: currentArgs,
+                error: serializeError(ex),
+              },
+            }).catch((err) => {
+              reportError(err);
+            });
+          } catch (err) {
             reportError(err);
-          });
+          }
         } else {
           console.warn("Can only send alert from deployment context");
         }
