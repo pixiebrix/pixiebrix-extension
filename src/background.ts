58025e0aa1eab25987e13a6a8db5d7dd047b1363
Federico <me@fregante.com>
2021-06-25 22:37:34 +0700

    Do a full reload if a partial reload was likely

--- src/background.ts
+++ src/background.ts
@@ -56,3 +56,20 @@ if (isChrome) {
   script.src = "https://apis.google.com/js/client.js?onload=onGAPILoad";
   document.head.append(script);
 }
+
+// `web-ext run` reloads the extension without reloading the manifest.
+// This forces a full reload if the version hasn't changed since the last run.
+if (process.env.ENVIRONMENT === "development") {
+  const { version_name } = chrome.runtime.getManifest();
+
+  if (localStorage.getItem("dev:last-version") === version_name) {
+    localStorage.removeItem("dev:last-version");
+    chrome.runtime.reload();
+  }
+
+  chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason === "update") {
+      localStorage.setItem("dev:last-version", version_name);
+    }
+  });
+}
