89fd64497c39f232ac05626b4f44c62ea05cac2a
Federico <me@fregante.com>
2021-06-25 22:43:12 +0700

    Exclude Firefox

--- src/background.ts
+++ src/background.ts
@@ -57,9 +57,9 @@ if (isChrome) {
   document.head.append(script);
 }
 
-// `web-ext run` reloads the extension without reloading the manifest.
+// In Chrome, `web-ext run` reloads the extension without reloading the manifest.
 // This forces a full reload if the version hasn't changed since the last run.
-if (process.env.ENVIRONMENT === "development") {
+if (process.env.ENVIRONMENT === "development" && isChrome) {
   const { version_name } = chrome.runtime.getManifest();
 
   if (localStorage.getItem("dev:last-version") === version_name) {
