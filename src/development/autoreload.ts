50e2de94f8fa3929d37700b800d4d4dd84196cc1
Federico <me@fregante.com>
2021-06-27 22:44:57 +0700

    Improve and add comments

--- src/development/autoreload.ts
+++ src/development/autoreload.ts
@@ -6,10 +6,13 @@ if (process.env.ENVIRONMENT === "development" && isChrome) {
   const { version_name } = chrome.runtime.getManifest();
 
   if (localStorage.getItem("dev:last-version") === version_name) {
+    // Removing the key ensures that it does not go into a reloading loop
+    // by making the above condition false after the reload
     localStorage.removeItem("dev:last-version");
     chrome.runtime.reload();
   }
 
+  // Chrome only calls this function if the extension is reloaded
   chrome.runtime.onInstalled.addListener(({ reason }) => {
     if (reason === "update") {
       localStorage.setItem("dev:last-version", version_name);
