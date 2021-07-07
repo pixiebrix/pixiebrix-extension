12f9822924f407eaccac6f33eea98ae9c8b93e5f
Federico <me@fregante.com>
2021-07-08 00:02:35 +0700

    Follow review

--- src/devTools/get-selected-element.ts
+++ src/devTools/get-selected-element.ts
@@ -3,18 +3,22 @@ import { evaluableFunction } from "@/utils";
 
 export let selectedElement: HTMLElement; // Live binding
 
+const GET_SELECTED_DEV_TOOLS_ELEMENT = "@@pixiebrix/devtools/get-selected";
+
 export function addListenerForUpdateSelectedElement(): void {
   expectContentScript();
-  window.addEventListener("@@pixiebrix/devtools/get-selected", (el) => {
+  window.addEventListener(GET_SELECTED_DEV_TOOLS_ELEMENT, (el) => {
     selectedElement = el.target as HTMLElement;
   });
 }
 
 export function updateSelectedElement(): void {
+  let $0: Element; // Unused, type only, don't move it inside `evaluableFunction`
+
   chrome.devtools.inspectedWindow.eval(
     evaluableFunction(() => {
-      // This function must be self-contained
-      // @ts-expect-error -- $0 is from the dev Tools API. It might also be undefined at first run
+      // This function does not have access outside its scope,
+      // don't use the `GET_SELECTED_DEV_TOOLS_ELEMENT` constant
       ($0 ?? document.body).dispatchEvent(
         new CustomEvent("@@pixiebrix/devtools/get-selected", { bubbles: true })
       );
