3a80fb2f8055f024b6d6699ea5d3dacfa2ca4f10
Federico Brigante <me@fregante.com>
2021-07-13 20:57:44 +0700

    Attempt to trigger codacy

--- src/blocks/readers/ArrayCompositeReader.ts
+++ src/blocks/readers/ArrayCompositeReader.ts
@@ -54,7 +54,7 @@ class ArrayCompositeReader extends Reader {
 
   async isAvailable(): Promise<boolean> {
     const availability = await Promise.all(
-      this._readers.map((x) => x.isAvailable())
+      this._readers.map((y) => y.isAvailable())
     );
     return availability.every(identity);
   }
