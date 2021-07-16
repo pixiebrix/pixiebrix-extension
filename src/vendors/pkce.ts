da0e39f63911d8ae3d6ccf9959f8ae35b374c07d
Federico Brigante <me@fregante.com>
2021-07-17 03:54:06 +0700

    Restore pkce.ts

--- src/vendors/pkce.ts
+++ src/vendors/pkce.ts
@@ -16,13 +16,13 @@ export const MIN_VERIFIER_LENGTH = 43;
 export const MAX_VERIFIER_LENGTH = 128;
 export const DEFAULT_CODE_CHALLENGE_METHOD = "S256";
 
-// Converts a string to base64 (url/filename safe variant)
+// converts a string to base64 (url/filename safe variant)
 export function stringToBase64Url(str: string): string {
   var b64 = btoa(str);
   return base64ToBase64Url(b64);
 }
 
-// Converts a standard base64-encoded string to a "url/filename safe" variant
+// converts a standard base64-encoded string to a "url/filename safe" variant
 export function base64ToBase64Url(b64: string): string {
   return b64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
 }
@@ -44,7 +44,6 @@ export function generateVerifier(prefix?: string): string {
     verifier =
       verifier + getRandomString(MIN_VERIFIER_LENGTH - verifier.length);
   }
-
   return encodeURIComponent(verifier).slice(0, MAX_VERIFIER_LENGTH);
 }
 
@@ -54,7 +53,7 @@ export function computeChallenge(str: string): PromiseLike<any> {
     var hash = String.fromCharCode.apply(null, [
       ...new Uint8Array(arrayBuffer),
     ]);
-    var b64u = stringToBase64Url(hash); // Url-safe base64 variant
+    var b64u = stringToBase64Url(hash); // url-safe base64 variant
     return b64u;
   });
 }
