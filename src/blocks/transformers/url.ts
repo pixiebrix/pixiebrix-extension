04d019d1d323c65b0bfcceddee47da389dd10533
Todd Schiller <todd.schiller@gmail.com>
2021-07-27 08:56:26 -0400

    #936: add construct URL brick (#937)

---
+++ src/blocks/transformers/url.ts
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2021 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Transformer } from "@/types";
+import { BlockArg, Schema } from "@/core";
+import { registerBlock } from "@/blocks/registry";
+
+export const SPACE_ENCODING_DEFAULT = "plus";
+const SPACE_ENCODED_VALUE = "%20";
+
+export const URL_INPUT_SPEC: Schema = {
+  $schema: "https://json-schema.org/draft/2019-09/schema#",
+  type: "object",
+  properties: {
+    url: {
+      type: "string",
+      description: "The URL",
+      format: "uri",
+    },
+    params: {
+      type: "object",
+      description: "URL parameters, will be automatically encoded",
+      additionalProperties: { type: ["string", "number", "boolean"] },
+    },
+    spaceEncoding: {
+      type: "string",
+      description: "Encode space using %20 vs. +",
+      default: SPACE_ENCODING_DEFAULT,
+      enum: ["percent", "plus"],
+    },
+  },
+  required: ["url"],
+};
+
+export function makeURL(
+  url: string,
+  params: Record<string, string | number | boolean> | undefined = {},
+  spaceEncoding: "plus" | "percent" = SPACE_ENCODING_DEFAULT
+): string {
+  // https://javascript.info/url#searchparams
+  const result = new URL(url);
+  for (const [name, value] of Object.entries(params ?? {})) {
+    if ((value ?? "") !== "") {
+      result.searchParams.append(name, String(value));
+    }
+  }
+
+  const fullURL = result.toString();
+
+  if (spaceEncoding === "plus" || result.search.length === 0) {
+    return fullURL;
+  }
+
+  return fullURL.replace(
+    result.search,
+    result.search.replaceAll("+", SPACE_ENCODED_VALUE)
+  );
+}
+
+export class UrlParams extends Transformer {
+  constructor() {
+    super(
+      "@pixiebrix/url-params",
+      "Construct URL",
+      "Construct a URL with encoded search parameter",
+      "faCode"
+    );
+  }
+
+  inputSchema: Schema = URL_INPUT_SPEC;
+
+  outputSchema: Schema = {
+    type: "object",
+    $schema: "https://json-schema.org/draft/2019-09/schema#",
+    properties: {
+      url: {
+        type: "string",
+        format: "uri",
+      },
+    },
+    required: ["url"],
+  };
+
+  async transform({
+    url,
+    params,
+    spaceEncoding = SPACE_ENCODING_DEFAULT,
+  }: BlockArg): Promise<{ url: string }> {
+    return {
+      url: makeURL(url, params, spaceEncoding),
+    };
+  }
+}
+
+registerBlock(new UrlParams());
