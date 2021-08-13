3413059c8cc8caf320e5aaf1f87d5235d5f18825
Federico <me@fregante.com>
2021-08-14 01:36:56 +0700

    Type-check in parallel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 const { uniq, isEmpty } = require("lodash");
 const Policy = require("csp-parse");
 const mergeWithShared = require("./webpack.shared-config.js");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 // Include defaults required for webpack here. Add defaults for the extension bundle to EnvironmentPlugin
 const defaults = {
@@ -240,7 +241,6 @@ module.exports = (env, options) =>
     ),
 
     resolve: {
-      extensions: [".ts", ".tsx", ".jsx", ".js"],
       alias: {
         ...mockHeavyDependencies(),
 
@@ -277,6 +277,7 @@ module.exports = (env, options) =>
     },
     plugins: [
       ...getConditionalPlugins(isProd(options)),
+      new ForkTsCheckerWebpackPlugin(),
 
       new NodePolyfillPlugin(),
       new WebExtensionTarget(),
--- webpack.shared-config.js
+++ webpack.shared-config.js
@@ -51,6 +51,9 @@ const shared = {
         test: /\.tsx?$/,
         loader: "ts-loader",
         exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
       },
       {
         test: /\.(svg|png|jpe?g|gif)?$/,
