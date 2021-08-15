pixiebrix/pixiebrix-extension

commit 22c70ca76b58e8bc8d7fdf7ad6827b837901e583

#1078: reshape redux state for options/extensions state (#1079)

src/hooks/useDeployments.ts
@@ -23,7 +23,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fromPairs } from "lodash";
 import { reportEvent } from "@/telemetry/events";
 import { optionsSlice } from "@/options/slices";
-import { selectInstalledExtensions } from "@/options/selectors";
+import { selectExtensions } from "@/options/selectors";
 import { getErrorMessage } from "@/errors";
 import useNotifications from "@/hooks/useNotifications";
 import { getExtensionToken } from "@/auth/token";
@@ -126,7 +126,7 @@ type DeploymentState = {
 function useDeployments(): DeploymentState {
   const notify = useNotifications();
   const dispatch = useDispatch();
-  const installed = useSelector(selectInstalledExtensions);
+  const installed = useSelector(selectExtensions);
 
   const [deployments] = useAsyncState(async () => fetchDeployments(installed), [
     installed,

src/options/pages/installed/InstalledPage.tsx
@@ -18,12 +18,12 @@
 import { connect } from "react-redux";
 import React, { useContext, useMemo } from "react";
 import { groupBy, isEmpty, sortBy } from "lodash";
-import { optionsSlice, OptionsState } from "@/options/slices";
+import { optionsSlice } from "@/options/slices";
 import { PageTitle } from "@/layout/Page";
 import { faCubes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { Card, Col, Row, Table } from "react-bootstrap";
-import { ExtensionIdentifier, IExtension } from "@/core";
+import { ExtensionRef, IExtension } from "@/core";
 import "./InstalledPage.scss";
 import { uninstallContextMenu } from "@/background/contextMenus";
 import { reportError } from "@/telemetry/logging";
@@ -31,14 +31,15 @@ import AuthContext from "@/auth/AuthContext";
 import { reportEvent } from "@/telemetry/events";
 import { reactivate } from "@/background/navigation";
 import { Dispatch } from "redux";
-import { selectInstalledExtensions } from "@/options/selectors";
+import { selectExtensions } from "@/options/selectors";
 import { useTitle } from "@/hooks/title";
 import NoExtensionsPage from "@/options/pages/installed/NoExtensionsPage";
 import RecipeEntry from "@/options/pages/installed/RecipeEntry";
+import { OptionsState } from "@/store/extensions";
 
 const { removeExtension } = optionsSlice.actions;
 
-type RemoveAction = (identifier: ExtensionIdentifier) => void;
+type RemoveAction = (identifier: ExtensionRef) => void;
 
 const InstalledTable: React.FunctionComponent<{
   extensions: IExtension[];
@@ -139,12 +140,11 @@ const InstalledPage: React.FunctionComponent<{
   );
 };
 
-const mapStateToProps = (state: { options: OptionsState }) => ({
-  extensions: selectInstalledExtensions(state),
-});
+const mapStateToProps = (state: { options: OptionsState }) =>
+  selectExtensions(state);
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  onRemove: (ref: ExtensionIdentifier) => {
+  onRemove: (ref: ExtensionRef) => {
     reportEvent("ExtensionRemove", {
       extensionId: ref.extensionId,
     });

src/options/slices.ts
@@ -15,13 +15,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { createSlice } from "@reduxjs/toolkit";
-import { IExtension, RawServiceConfiguration, RegistryId } from "@/core";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  ExtensionRef,
+  IExtension,
+  RawServiceConfiguration,
+  RegistryId,
+  UUID,
+} from "@/core";
 import { orderBy } from "lodash";
 import { reportEvent } from "@/telemetry/events";
 import { preloadMenus } from "@/background/preload";
 import { selectEventData } from "@/telemetry/deployments";
 import { uuidv4 } from "@/types/helpers";
+import { Except } from "type-fest";
+import { OptionsState } from "@/store/extensions";
 
 type InstallMode = "local" | "remote";
 
@@ -50,14 +58,6 @@ export interface ServicesState {
 const initialServicesState: ServicesState = {
   configured: {},
 };
-
-export interface OptionsState {
-  extensions: {
-    [extensionPointId: string]: {
-      [extensionId: string]: IExtension;
-    };
-  };
-}
 
 type RecentBrick = {
   id: string;
@@ -88,11 +88,8 @@ const initialWorkshopState: WorkshopState = {
 };
 
 const initialOptionsState: OptionsState = {
-  extensions: {},
+  extensions: [],
 };
-
-/* The object access in servicesSlice and optionsSlice should be safe because slice reducers use immer under the hood */
-/* eslint-disable security/detect-object-injection */
 
 export const workshopSlice = createSlice({
   name: "workshop",
@@ -132,30 +129,39 @@ export const workshopSlice = createSlice({
 });
 
 export const servicesSlice = createSlice({
+  /* The object access in servicesSlice and optionsSlice should be safe because type-checker enforced UUID */
+  /* eslint-disable security/detect-object-injection */
+
   name: "services",
   initialState: initialServicesState,
   reducers: {
-    deleteServiceConfig(state, { payload: { id } }) {
+    deleteServiceConfig(
+      state,
+      { payload: { id } }: PayloadAction<{ id: UUID }>
+    ) {
       if (!state.configured[id]) {
         throw new Error(`Service configuration ${id} does not exist`);
       }
 
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete -- type-checked as UUID
       delete state.configured[id];
       return state;
     },
     updateServiceConfig(state, { payload: { id, serviceId, label, config } }) {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- branding with nominal type
       state.configured[id] = {
-        _rawServiceConfigurationBrand: undefined,
         id,
         serviceId,
         label,
         config,
-      };
+      } as RawServiceConfiguration;
     },
     resetServices(state) {
       state.configured = {};
     },
   },
+
+  /* eslint-enable security/detect-object-injection */
 });
 
 export const optionsSlice = createSlice({
@@ -163,7 +169,7 @@ export const optionsSlice = createSlice({
   initialState: initialOptionsState,
   reducers: {
     resetOptions(state) {
-      state.extensions = {};
+      state.extensions = [];
     },
     installRecipe(state, { payload }) {
       const {
@@ -184,11 +190,7 @@ export const optionsSlice = createSlice({
           throw new Error("extensionPointId is required");
         }
 
-        if (state.extensions[extensionPointId] == null) {
-          state.extensions[extensionPointId] = {};
-        }
-
-        const extensionConfig: IExtension = {
+        const extension: IExtension = {
           id: extensionId,
           _deployment: deployment
             ? {
@@ -201,6 +203,7 @@ export const optionsSlice = createSlice({
           services: Object.entries(services ?? {}).map(
             ([outputKey, id]: [string, RegistryId]) => ({
               outputKey,
+              // eslint-disable-next-line security/detect-object-injection -- type-checked as RegistryId
               config: auths[id],
               id,
             })
@@ -210,14 +213,20 @@ export const optionsSlice = createSlice({
           config,
         };
 
-        reportEvent("ExtensionActivate", selectEventData(extensionConfig));
+        reportEvent("ExtensionActivate", selectEventData(extension));
 
-        state.extensions[extensionPointId][extensionId] = extensionConfig;
+        state.extensions.push(extension);
 
-        void preloadMenus({ extensions: [extensionConfig] });
+        void preloadMenus({ extensions: [extension] });
       }
     },
-    saveExtension(state, { payload }) {
+    // XXX: why do we expose a `extensionId` in addition IExtension's `id` prop here?
+    saveExtension(
+      state,
+      {
+        payload,
+      }: PayloadAction<Except<IExtension, "_recipe"> & { extensionId?: UUID }>
+    ) {
       const {
         id,
         extensionId,
@@ -229,16 +238,16 @@ export const optionsSlice = createSlice({
       } = payload;
       // Support both extensionId and id to keep the API consistent with the shape of the stored extension
       if (extensionId == null && id == null) {
-        throw new Error("extensionId is required");
+        throw new Error("id or extensionId is required");
       } else if (extensionPointId == null) {
         throw new Error("extensionPointId is required");
       }
 
-      if (state.extensions[extensionPointId] == null) {
-        state.extensions[extensionPointId] = {};
-      }
+      const index = state.extensions.findIndex(
+        (x) => x.id === extensionId ?? id
+      );
 
-      state.extensions[extensionPointId][extensionId ?? id] = {
+      const extension: IExtension = {
         id: extensionId ?? id,
         extensionPointId,
         _recipe: null,
@@ -247,20 +256,19 @@ export const optionsSlice = createSlice({
         services,
         config,
       };
-    },
-    removeExtension(state, { payload }) {
-      const { extensionPointId, extensionId } = payload;
-      const extensions = state.extensions[extensionPointId] ?? {};
-      if (extensions[extensionId]) {
-        delete extensions[extensionId];
+
+      if (index >= 0) {
+        // eslint-disable-next-line security/detect-object-injection -- array index from findIndex
+        state.extensions[index] = extension;
       } else {
-        // It's already removed
-        console.debug(
-          `Extension id ${extensionId} does not exist for extension point ${extensionPointId}`
-        );
+        state.extensions.push(extension);
       }
+    },
+    removeExtension(
+      state,
+      { payload: { extensionId } }: PayloadAction<ExtensionRef>
+    ) {
+      state.extensions = state.extensions.filter((x) => x.id === extensionId);
     },
   },
 });
-
-/* eslint-enable security/detect-object-injection */

src/selectors.ts
@@ -18,9 +18,9 @@
 import { useMemo } from "react";
 import extensionPointRegistry from "@/extensionPoints/registry";
 import { useSelector } from "react-redux";
-import { RootState } from "@/options/store";
-import { IExtension, IExtensionPoint } from "@/core";
+import { IExtension, IExtensionPoint, RegistryId, UUID } from "@/core";
 import { useAsyncState } from "@/hooks/common";
+import { selectExtensions } from "@/options/selectors";
 
 interface ExtensionResult {
   extensionPoint: IExtensionPoint | null;
@@ -29,42 +29,32 @@ interface ExtensionResult {
 }
 
 export function useExtension(
-  extensionPointId: string,
-  extensionId: string
+  extensionPointId: RegistryId,
+  extensionId: UUID
 ): ExtensionResult {
   console.debug("useExtension", { extensionPointId, extensionId });
 
-  const options = useSelector((state: RootState) => state.options);
+  const extensions = useSelector(selectExtensions);
 
   const extensionConfig = useMemo(() => {
-    let config;
-
     if (!extensionId) {
       return null;
     }
 
-    if (extensionPointId) {
-      config = options.extensions[extensionPointId][extensionId];
-    } else {
-      for (const pointExtensions of Object.values(options.extensions)) {
-        const pointConfig = pointExtensions[extensionId];
-        if (pointConfig) {
-          config = pointConfig;
-          break;
-        }
-      }
-    }
+    const config = extensions.find(
+      (x) => x.extensionPointId === extensionPointId && x.id === extensionId
+    );
 
     if (!config) {
       throw new Error(
         `Could not locate configuration for extension ${extensionId} (extension point: ${
-          extensionPointId ?? "<unknown>"
+          extensionPointId ?? "[[ unknown ]]"
         })`
       );
     }
 
     return config;
-  }, [options, extensionId, extensionPointId]);
+  }, [extensions, extensionId, extensionPointId]);
 
   const [extensionPoint, isPending] = useAsyncState(async () => {
     if (extensionConfig) {

src/store/extensions.ts
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2021 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { MigrationManifest, PersistedState } from "redux-persist/es/types";
+import { localStorage } from "redux-persist-webextension-storage";
+import { createMigrate } from "redux-persist";
+import { boolean } from "@/utils";
+import { IExtension } from "@/core";
+
+const migrations: MigrationManifest = {
+  1: (state: PersistedState & OptionsState) => migrateOptionsState(state),
+};
+
+export const persistOptionsConfig = {
+  key: "extensionOptions",
+  storage: localStorage,
+  version: 1,
+  // https://github.com/rt2zz/redux-persist#migrations
+  migrate: createMigrate(migrations, { debug: boolean(process.env.DEBUG) }),
+};
+
+/**
+ * @deprecated use ExtensionsState - this is only used in the migration
+ */
+type LegacyExtensionsState = {
+  // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style -- documentation
+  extensions: {
+    // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style -- documentation
+    [extensionPointId: string]: {
+      [extensionId: string]: IExtension;
+    };
+  };
+};
+
+export type ExtensionsOptionsState = {
+  extensions: IExtension[];
+};
+
+// Putting here because it was causing circular dependencies
+export function migrateOptionsState<T>(
+  state: T & (LegacyExtensionsState | ExtensionsOptionsState)
+): T & ExtensionsOptionsState {
+  if (state.extensions == null) {
+    console.info("Repairing redux state");
+    return { ...state, extensions: [] };
+  }
+
+  if (Array.isArray(state.extensions)) {
+    // Already migrated
+    console.debug("Redux state already up-to-date");
+    return state as T & ExtensionsOptionsState;
+  }
+
+  console.info("Migrating redux state");
+
+  return {
+    ...state,
+    extensions: Object.values(state.extensions).flatMap((extensionMap) =>
+      Object.values(extensionMap)
+    ),
+  };
+}
+
+export type OptionsState = LegacyExtensionsState | ExtensionsOptionsState;

src/options/selectors.ts
@@ -15,28 +15,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { OptionsState } from "@/options/slices";
 import { IExtension } from "@/core";
+import { OptionsState } from "@/store/extensions";
 
 export function selectExtensions({
   options,
 }: {
   options: OptionsState;
 }): IExtension[] {
-  return Object.values(options.extensions).flatMap((extensionPointOptions) =>
-    Object.values(extensionPointOptions)
-  );
-}
+  if (!Array.isArray(options.extensions)) {
+    console.warn("state migration has not been applied yet", {
+      options,
+    });
+    throw new TypeError("state migration has not been applied yet");
+  }
 
-export function selectInstalledExtensions(state: {
-  options: OptionsState;
-}): IExtension[] {
-  return Object.entries(state.options.extensions).flatMap(
-    ([extensionPointId, pointExtensions]) =>
-      Object.entries(pointExtensions).map(([extensionId, extension]) => ({
-        id: extensionId,
-        extensionPointId,
-        ...extension,
-      }))
-  );
+  return options.extensions;
 }

commit 396f3c7bd1a448605f735534f31eeeecfb4e1805

#1078: fix bugs introduced in #1079

src/devTools/editor/hooks/useRemove.ts
@@ -51,7 +51,7 @@ function useRemove(element: FormState): () => void {
       return;
     }
 
-    const target = {
+    const ref = {
       extensionPointId: values.extensionPoint.metadata.id,
       extensionId: values.uuid,
     };
@@ -59,12 +59,12 @@ function useRemove(element: FormState): () => void {
     try {
       // Remove from storage first so it doesn't get re-added by any subsequent steps
       if (values.installed) {
-        dispatch(optionsSlice.actions.removeExtension(target));
+        dispatch(optionsSlice.actions.removeExtension(ref));
       }
 
       void Promise.allSettled([
-        nativeOperations.uninstallContextMenu(port, target),
-        nativeOperations.uninstallActionPanelPanel(port, target),
+        nativeOperations.uninstallContextMenu(port, ref),
+        nativeOperations.uninstallActionPanelPanel(port, ref),
       ]);
 
       // Remove from page editor

src/hooks/useDeployments.ts
@@ -93,7 +93,6 @@ function activateDeployments(
       if (extension._recipe?.id === deployment.package.package_id) {
         dispatch(
           actions.removeExtension({
-            extensionPointId: extension.extensionPointId,
             extensionId: extension.id,
           })
         );

src/options/pages/installed/InstalledPage.tsx
@@ -140,8 +140,9 @@ const InstalledPage: React.FunctionComponent<{
   );
 };
 
-const mapStateToProps = (state: { options: OptionsState }) =>
-  selectExtensions(state);
+const mapStateToProps = (state: { options: OptionsState }) => ({
+  extensions: selectExtensions(state),
+});
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   onRemove: (ref: ExtensionRef) => {

src/options/slices.ts
@@ -17,10 +17,10 @@
 
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import {
-  ExtensionRef,
   IExtension,
   RawServiceConfiguration,
   RegistryId,
+  UserOptions,
   UUID,
 } from "@/core";
 import { orderBy } from "lodash";
@@ -29,7 +29,9 @@ import { preloadMenus } from "@/background/preload";
 import { selectEventData } from "@/telemetry/deployments";
 import { uuidv4 } from "@/types/helpers";
 import { Except } from "type-fest";
-import { OptionsState } from "@/store/extensions";
+import { OptionsState, requireLatestState } from "@/store/extensions";
+import { ExtensionPointConfig, RecipeDefinition } from "@/types/definitions";
+import { Deployment } from "@/types/contract";
 
 type InstallMode = "local" | "remote";
 
@@ -171,7 +173,21 @@ export const optionsSlice = createSlice({
     resetOptions(state) {
       state.extensions = [];
     },
-    installRecipe(state, { payload }) {
+
+    installRecipe(
+      state,
+      {
+        payload,
+      }: PayloadAction<{
+        recipe: RecipeDefinition;
+        services?: Record<RegistryId, UUID>;
+        extensionPoints: ExtensionPointConfig[];
+        optionsArgs?: UserOptions;
+        deployment?: Pick<Deployment, "id" | "updated_at">;
+      }>
+    ) {
+      requireLatestState(state);
+
       const {
         recipe,
         services: auths,
@@ -182,10 +198,11 @@ export const optionsSlice = createSlice({
       for (const {
         id: extensionPointId,
         label,
-        services,
+        services = {},
         config,
       } of extensionPoints) {
         const extensionId = uuidv4();
+
         if (extensionPointId == null) {
           throw new Error("extensionPointId is required");
         }
@@ -200,11 +217,11 @@ export const optionsSlice = createSlice({
             : undefined,
           _recipe: recipe.metadata,
           optionsArgs,
-          services: Object.entries(services ?? {}).map(
+          services: Object.entries(services).map(
             ([outputKey, id]: [string, RegistryId]) => ({
               outputKey,
-              // eslint-disable-next-line security/detect-object-injection -- type-checked as RegistryId
-              config: auths[id],
+              // @ts-expect-error -- TypeScript is complaining that RegistryId can't index Record<RegistryId, ...>
+              config: auths[id], // eslint-disable-line security/detect-object-injection -- type-checked as RegistryId
               id,
             })
           ),
@@ -227,6 +244,8 @@ export const optionsSlice = createSlice({
         payload,
       }: PayloadAction<Except<IExtension, "_recipe"> & { extensionId?: UUID }>
     ) {
+      requireLatestState(state);
+
       const {
         id,
         extensionId,
@@ -266,9 +285,10 @@ export const optionsSlice = createSlice({
     },
     removeExtension(
       state,
-      { payload: { extensionId } }: PayloadAction<ExtensionRef>
+      { payload: { extensionId } }: PayloadAction<{ extensionId: UUID }>
     ) {
-      state.extensions = state.extensions.filter((x) => x.id === extensionId);
+      requireLatestState(state);
+      state.extensions = state.extensions.filter((x) => x.id !== extensionId);
     },
   },
 });

src/pages/marketplace/useReinstall.ts
@@ -73,7 +73,6 @@ function useReinstall(): Reinstall {
           await uninstallContextMenu({ extensionId: extension.id });
           dispatch(
             removeExtension({
-              extensionPointId: extension.extensionPointId,
               extensionId: extension.id,
             })
           );

src/selectors.ts
@@ -41,31 +41,20 @@ export function useExtension(
       return null;
     }
 
-    const config = extensions.find(
-      (x) => x.extensionPointId === extensionPointId && x.id === extensionId
-    );
+    const config = extensions.find((x) => x.id === extensionId);
 
     if (!config) {
       throw new Error(
-        `Could not locate configuration for extension ${extensionId} (extension point: ${
-          extensionPointId ?? "[[ unknown ]]"
-        })`
+        `Could not locate configuration for extension: ${extensionId}`
       );
     }
 
     return config;
-  }, [extensions, extensionId, extensionPointId]);
+  }, [extensions, extensionId]);
 
   const [extensionPoint, isPending] = useAsyncState(async () => {
-    if (extensionConfig) {
-      return extensionPointRegistry.lookup(extensionConfig.extensionPointId);
-    }
-
-    if (extensionPointId) {
-      return extensionPointRegistry.lookup(extensionPointId);
-    }
-
-    return null;
+    const id = extensionConfig?.extensionPointId ?? extensionPointId;
+    return id ? extensionPointRegistry.lookup(id) : null;
   }, [extensionPointRegistry, extensionConfig, extensionPointId]);
 
   return { extensionPoint, extensionConfig, isPending };

src/store/extensions.ts
@@ -76,3 +76,11 @@ export function migrateOptionsState<T>(
 }
 
 export type OptionsState = LegacyExtensionsState | ExtensionsOptionsState;
+
+export function requireLatestState(
+  state: OptionsState
+): asserts state is ExtensionsOptionsState {
+  if (!Array.isArray(state.extensions)) {
+    throw new TypeError("redux state has not been migrated");
+  }
+}

src/types/contract.ts
@@ -49,6 +49,7 @@ export type ConfigurableAuth = components["schemas"]["EditableAuth"] & {
 };
 
 export type Deployment = components["schemas"]["DeploymentDetail"] & {
+  id: UUID;
   package: { config: RecipeDefinition };
 };
 