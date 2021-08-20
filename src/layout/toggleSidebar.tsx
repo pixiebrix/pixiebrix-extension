d0ac7b65acaf8776f155ab8ac86873d1b78d7237
Alexandr <zhmurik@yandex.ru>
2021-08-20 16:34:07 +0200

    #1075 close sidebar on navigation (#1130)

---
+++ src/layout/toggleSidebar.tsx
@@ -0,0 +1,23 @@
+import { sidebarId } from "./Sidebar";
+
+const smallScreenMediaQuery = "(max-width: 991px)";
+const sidebarActiveClassName = "active"; // Used to show/hide navbar on small screen
+const sidebarIconOnlyClassName = "sidebar-icon-only"; // Used to show/hide navbar on big screen
+
+export const toggleSidebar = () => {
+  if (window.matchMedia(smallScreenMediaQuery).matches) {
+    document
+      .querySelector(`#${sidebarId}`)
+      .classList.toggle(sidebarActiveClassName);
+  } else {
+    document.body.classList.toggle(sidebarIconOnlyClassName);
+  }
+};
+
+export const closeSidebarOnSmallScreen = () => {
+  if (window.matchMedia(smallScreenMediaQuery).matches) {
+    document
+      .querySelector(`#${sidebarId}`)
+      .classList.remove(sidebarActiveClassName);
+  }
+};
