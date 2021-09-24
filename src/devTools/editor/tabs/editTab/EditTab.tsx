d6a29a62ea5b409699bab80a81cada0e50038763
Ben Loe <below413@gmail.com>
2021-09-24 11:55:27 -0400

    Change active tab to preview for form builder nodes (#1456)
    * change active tab to preview for form nodes
    
    * use instance id for data panel key
    
    Co-authored-by: Ben Loe <ben@pixiebrix.com>

--- src/devTools/editor/tabs/editTab/EditTab.tsx
+++ src/devTools/editor/tabs/editTab/EditTab.tsx
@@ -191,6 +191,8 @@ const EditTab: React.FC<{
     [onSelectNode, pipelineFieldHelpers, blockPipeline]
   );
 
+  const blockInstanceId = blockPipeline[activeNodeIndex - 1]?.instanceId;
+
   return (
     <Tab.Pane eventKey={eventKey} className={styles.tabPane}>
       <div className={styles.paneContent}>
@@ -237,9 +239,9 @@ const EditTab: React.FC<{
         </div>
         <div className={styles.dataPanel}>
           <DataPanel
+            key={blockInstanceId}
             blockFieldName={blockFieldName}
-            // eslint-disable-next-line security/detect-object-injection
-            instanceId={blockPipeline[activeNodeIndex - 1]?.instanceId}
+            instanceId={blockInstanceId}
           />
         </div>
       </div>
