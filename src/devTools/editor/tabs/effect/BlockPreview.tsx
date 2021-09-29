369174c04802d292104d03e1214684d6d091b433
Ben Loe <ben@pixiebrix.com>
2021-09-29 18:06:58 -0400

    expand common nodes in the data panel by default so they don't have to be clicked every time

--- src/devTools/editor/tabs/effect/BlockPreview.tsx
+++ src/devTools/editor/tabs/effect/BlockPreview.tsx
@@ -96,6 +96,7 @@ const BlockPreview: React.FunctionComponent<{
 
   const [isRunning, setIsRunning] = useState(false);
   const [output, setOutput] = useState<unknown | undefined>();
+  const [outputKey, setOutputKey] = useState<string>(blockConfig.outputKey);
 
   const [blockInfo, blockLoading, blockError] = usePreviewInfo(blockConfig.id);
 
@@ -108,6 +109,7 @@ const BlockPreview: React.FunctionComponent<{
           args,
         });
         const { outputKey } = blockConfig;
+        setOutputKey(outputKey);
         setOutput(outputKey ? { [`@${outputKey}`]: result } : result);
       } catch (error: unknown) {
         setOutput(error);
@@ -185,7 +187,14 @@ const BlockPreview: React.FunctionComponent<{
       )}
 
       {output && !isError && !isEmpty(output) && (
-        <JsonTree data={output} searchable copyable />
+        <JsonTree
+          data={output}
+          searchable
+          copyable
+          shouldExpandNode={(keyPath) =>
+            keyPath.length === 1 && keyPath[0] === `@${outputKey}`
+          }
+        />
       )}
 
       {output && !isError && isEmpty(output) && (
