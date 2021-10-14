db06e1338e6ba0964512eef14c2dbc47bc0be267
Alex <zhmurik@yandex.ru>
2021-10-14 10:43:31 +0200

    Validate if renderer is not the last block

--- src/devTools/editor/validation/validateRenderers.ts
+++ src/devTools/editor/validation/validateRenderers.ts
@@ -5,6 +5,10 @@ import {
 } from "@/devTools/editor/tabs/editTab/editTabTypes";
 import { ElementType } from "@/devTools/editor/extensionPoints/elementConfig";
 
+export const MULTIPLE_RENDERERS_ERROR_MESSAGE =
+  "Pipeline can have only one renderer. There is another renderer defined later in the pipeline.";
+export const RENDERER_MUST_BE_LAST_BLOCK_ERROR_MESSAGE =
+  "Renderer must be placed in the very end of the pipeline.";
 function validateRenderers(
   pipelineErrors: FormikErrorTree,
   pipeline: BlockPipeline,
@@ -16,21 +20,29 @@ function validateRenderers(
   }
 
   let hasGotRenderer = false;
-  for (let blockIndex = 0; blockIndex !== pipeline.length; ++blockIndex) {
+  for (let blockIndex = pipeline.length - 1; blockIndex >= 0; --blockIndex) {
     // eslint-disable-next-line security/detect-object-injection
     const pipelineBlock = pipeline[blockIndex];
     const blockType = allBlocks[pipelineBlock.id]?.type;
+    const blockErrors = [];
 
     if (blockType !== "renderer") {
       continue;
     }
 
     if (hasGotRenderer) {
-      // eslint-disable-next-line security/detect-object-injection
-      pipelineErrors[blockIndex] =
-        "There is another renderer defined in the pipeline.";
+      blockErrors.push(MULTIPLE_RENDERERS_ERROR_MESSAGE);
     } else {
       hasGotRenderer = true;
+    }
+
+    if (blockIndex !== pipeline.length - 1) {
+      blockErrors.push(RENDERER_MUST_BE_LAST_BLOCK_ERROR_MESSAGE);
+    }
+
+    if (blockErrors.length > 0) {
+      // eslint-disable-next-line security/detect-object-injection
+      pipelineErrors[blockIndex] = blockErrors.join(" ");
     }
   }
 }
