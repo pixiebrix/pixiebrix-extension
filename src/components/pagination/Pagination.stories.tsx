7ffff6ccdb0e42cf41cb208f5dc3960a031819a0
Alex <zhmurik@yandex.ru>
2021-10-18 15:48:17 +0200

    Pagination on Workshop page. Pagination fix.

---
+++ src/components/pagination/Pagination.stories.tsx
@@ -0,0 +1,36 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React, { useState } from "react";
+import Pagination from "./Pagination";
+
+const componentMeta: ComponentMeta<typeof Pagination> = {
+  title: "Components/Pagination",
+  component: Pagination,
+  argTypes: {
+    setPage: {
+      action: "setPage",
+    },
+  },
+};
+
+const Template: ComponentStory<typeof Pagination> = ({
+  numPages,
+  setPage,
+  ...rest
+}) => {
+  const [page, setPageInternal] = useState(0);
+  const onSetPage = (nextPage: number) => {
+    setPageInternal(nextPage);
+    setPage?.(nextPage);
+  };
+
+  return (
+    <Pagination page={page} setPage={onSetPage} numPages={numPages} {...rest} />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  numPages: 10,
+};
+
+export default componentMeta;
