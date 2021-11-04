1d0494ff062a27e9aa860fb305b73f6bd0ec3b33
Federico <me@fregante.com>
2021-11-05 00:50:37 +0100

    Extract generic "showModal" function

--- src/blocks/transformers/modalForm/modal.tsx
+++ src/blocks/transformers/modalForm/modal.tsx
@@ -26,6 +26,52 @@ import { expectContext } from "@/utils/expectContext";
 import { whoAmI } from "@/background/messenger/api";
 import { scrollbarWidth } from "@xobotyi/scrollbar-width";
 
+function showModal(url: URL, signal: AbortSignal): void {
+  // Using `<style>` will avoid overriding the site’s inline styles
+  const style = document.createElement("style");
+
+  const scrollableRoot =
+    window.getComputedStyle(document.body).overflowY === "scroll"
+      ? "body"
+      : "html";
+  style.textContent += `${scrollableRoot} {overflow: hidden !important}`; // Disable scrollbar
+
+  // Preserve space initially taken by scrollbar
+  style.textContent += `html {padding-inline-end: ${scrollbarWidth()}px  !important}`;
+
+  const container = document.createElement("div");
+  const shadowRoot = container.attachShadow({ mode: "closed" });
+  document.body.append(container, style);
+  render(
+    <dialog
+      ref={(dialog) => dialog.showModal()}
+      style={{
+        border: 0,
+        width: "500px",
+        height: "100vh", // TODO: Replace with frame auto-sizer via messaging
+        display: "flex", // Fit iframe inside
+        background: "none",
+      }}
+    >
+      <iframe
+        src={url.href}
+        style={{
+          border: "0",
+          flexGrow: 1, // Fit dialog
+          colorScheme: "normal", // Match parent color scheme #1650
+        }}
+      />
+    </dialog>,
+    shadowRoot
+  );
+
+  signal.addEventListener("abort", () => {
+    unmountComponentAtNode(container);
+    style.remove();
+    container.remove();
+  });
+}
+
 export class ModalTransformer extends Transformer {
   defaultOutputKey = "form";
 
@@ -87,43 +133,8 @@ export class ModalTransformer extends Transformer {
       JSON.stringify({ tabId: tab.id, frameId })
     );
 
-    // Using `<style>` will avoid overriding the site’s inline styles
-    const style = document.createElement("style");
-
-    const scrollableRoot =
-      window.getComputedStyle(document.body).overflowY === "scroll"
-        ? "body"
-        : "html";
-    style.textContent += `${scrollableRoot} {overflow: hidden !important}`; // Disable scrollbar
-
-    // Preserve space initially taken by scrollbar
-    style.textContent += `html {padding-inline-end: ${scrollbarWidth()}px  !important}`;
-
-    const container = document.createElement("div");
-    const shadowRoot = container.attachShadow({ mode: "closed" });
-    document.body.append(container, style);
-    render(
-      <dialog
-        ref={(dialog) => dialog.showModal()}
-        style={{
-          border: 0,
-          width: "500px",
-          height: "100vh", // TODO: Replace with frame auto-sizer via messaging
-          display: "flex", // Fit iframe inside
-          background: "none",
-        }}
-      >
-        <iframe
-          src={frameSrc.href}
-          style={{
-            border: "0",
-            flexGrow: 1, // Fit dialog
-            colorScheme: "normal", // Match parent color scheme #1650
-          }}
-        />
-      </dialog>,
-      shadowRoot
-    );
+    const controller = new AbortController();
+    showModal(frameSrc, controller.signal);
 
     try {
       return await registerForm(nonce, {
@@ -133,9 +144,7 @@ export class ModalTransformer extends Transformer {
         submitCaption,
       });
     } finally {
-      unmountComponentAtNode(container);
-      style.remove();
-      container.remove();
+      controller.abort();
     }
   }
 }
