c0a18726f64d89705dc68d193e36d3a570f5a864
mnholtz <mnholtz@ucsc.edu>
2021-11-09 14:02:43 -0800

    refactor extension filtering into sort method

--- src/core.ts
+++ src/core.ts
@@ -246,7 +246,6 @@ export interface Metadata {
   readonly name: string;
   readonly version?: string;
   readonly description?: string;
-  readonly sharing?: Sharing;
 
   /**
    * @deprecated experimental prop that will likely be removed in the future
@@ -267,11 +266,11 @@ export interface Metadata {
 
 export interface Sharing {
   readonly public: boolean;
-  readonly organizations: string[];
+  readonly organizations: UUID[];
 }
 
 export function selectMetadata(metadata: Metadata): Metadata {
-  return pick(metadata, ["id", "name", "version", "description", "sharing"]);
+  return pick(metadata, ["id", "name", "version", "description"]);
 }
 
 export type Config = Record<string, unknown>;
@@ -351,6 +350,17 @@ export type ExtensionRef = {
   extensionPointId: RegistryId;
 };
 
+/**
+ * Recipe with Sharing information.
+ * @see optionsSlice
+ * We created this type as an alternative to Metadata
+ * in order to include information about the origin of an
+ * extension, e.g. on the ActiveBricks page.
+ */
+export type RecipeMetadata = Metadata & {
+  sharing?: Sharing;
+};
+
 export type IExtension<T extends Config = EmptyConfig> = {
   /**
    * UUID of the extension.
@@ -378,7 +388,7 @@ export type IExtension<T extends Config = EmptyConfig> = {
    * Metadata about the recipe used to install the extension, or `undefined` if the user created this extension
    * directly.
    */
-  _recipe: Metadata | undefined;
+  _recipe: RecipeMetadata | undefined;
 
   /**
    * A human-readable label for the extension.
--- src/options/pages/installed/ActiveBricksCard.tsx
+++ src/options/pages/installed/ActiveBricksCard.tsx
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { useMemo } from "react";
+import React, { useContext, useMemo } from "react";
 import { MessageContext, ResolvedExtension, UUID } from "@/core";
 import { ExportBlueprintAction, RemoveAction } from "./installedPageTypes";
 import { Card, Col, Row, Table } from "react-bootstrap";
@@ -25,6 +25,7 @@ import { groupBy } from "lodash";
 import ExtensionRows from "./ExtensionRows";
 import { isDeploymentActive } from "@/options/deploymentUtils";
 import { useGetOrganizationsQuery } from "@/services/api";
+import AuthContext from "@/auth/AuthContext";
 
 const groupByRecipe = (
   extensions: ResolvedExtension[]
@@ -33,16 +34,20 @@ const groupByRecipe = (
 
 const groupByOrganizationId = (
   extensions: ResolvedExtension[]
-): Array<[string, ResolvedExtension[]]> =>
-  Object.entries(
+): Array<[UUID, ResolvedExtension[]]> =>
+  (Object.entries(
     groupBy(
       extensions,
+      // For the uncommon scenario that a user would be a part of two or more organizations
+      // with which the recipe is shared, we arbitrarily choose the first one
       (extension) => extension._recipe.sharing.organizations[0]
     )
-  );
+    // Could not figure out nominal type for UUID
+  ) as unknown) as Array<[UUID, ResolvedExtension[]]>;
 
 const isPublic = (extension: ResolvedExtension) =>
   extension._recipe?.sharing?.public;
+
 const hasOrganization = (extension: ResolvedExtension) =>
   extension._recipe?.sharing?.organizations.length > 0;
 
@@ -52,42 +57,70 @@ const ActiveBricksCard: React.FunctionComponent<{
   onExportBlueprint: ExportBlueprintAction;
 }> = ({ extensions, onRemove, onExportBlueprint }) => {
   const { data: organizations = [] } = useGetOrganizationsQuery();
+  const { scope } = useContext(AuthContext);
 
   const getOrganizationName = (organization_uuid: UUID) =>
     organizations.find((organization) => organization.id === organization_uuid)
       ?.name;
 
-  const personalExtensions = extensions.filter(
-    (extension) =>
-      (!extension._recipe && !extension._deployment) ||
-      (!isPublic(extension) && !hasOrganization(extension))
-  );
+  const isPersonal = (extension: ResolvedExtension) =>
+    extension._recipe?.id.includes(scope) ||
+    (!extension._recipe && !extension._deployment);
+
+  const sortedExtensions = useMemo(() => {
+    const personal = [];
+    const marketplace = [];
+    const team = [];
+    const deployment = [];
+    const other = [];
+
+    for (const extension of extensions) {
+      if (isPersonal(extension)) {
+        personal.push(extension);
+        continue;
+      }
+
+      if (extension._deployment) {
+        deployment.push(extension);
+        continue;
+      }
+
+      if (isPublic(extension)) {
+        marketplace.push(extension);
+        continue;
+      }
+
+      if (hasOrganization(extension)) {
+        team.push(extension);
+        continue;
+      }
+
+      other.push(extension);
+    }
+
+    return { personal, marketplace, team, deployment, other };
+  }, [extensions, scope]);
+
+  const personalExtensions = sortedExtensions.personal;
 
   const marketplaceExtensionGroups = groupByRecipe(
-    extensions.filter(
-      (extension) => isPublic(extension) && !extension._deployment
-    )
+    sortedExtensions.marketplace
   );
 
   const teamExtensionGroups = useMemo(
     () =>
-      groupByOrganizationId(
-        extensions.filter(
-          (extension) =>
-            hasOrganization(extension) &&
-            !isPublic(extension) &&
-            !extension._deployment
-        )
-      ).map(([organization_uuid, extensions]) => ({
-        organization_uuid,
-        extensions: groupByRecipe(extensions),
-      })),
-    [extensions]
+      groupByOrganizationId(sortedExtensions.team).map(
+        ([organization_uuid, extensions]) => ({
+          organization_uuid,
+          extensions: groupByRecipe(extensions),
+        })
+      ),
+    [sortedExtensions]
   );
 
-  const deploymentGroups = groupByRecipe(
-    extensions.filter((extension) => extension._deployment)
-  );
+  const deploymentExtensionGroups = groupByRecipe(sortedExtensions.deployment);
+
+  const otherExtensionGroups = groupByRecipe(sortedExtensions.other);
 
   return (
     <Row>
@@ -104,6 +137,29 @@ const ActiveBricksCard: React.FunctionComponent<{
                     onRemove={onRemove}
                     onExportBlueprint={onExportBlueprint}
                   />
+                </>
+              )}
+
+              {otherExtensionGroups.length > 0 && (
+                <>
+                  <ExtensionGroupHeader label="Marketplace Bricks" />
+                  {otherExtensionGroups.map((extensions) => {
+                    const recipe = extensions[0]._recipe;
+                    const messageContext: MessageContext = {
+                      blueprintId: recipe.id,
+                    };
+
+                    return (
+                      <ExtensionGroup
+                        key={recipe.id}
+                        label={recipe.name}
+                        extensions={extensions}
+                        groupMessageContext={messageContext}
+                        onRemove={onRemove}
+                        onExportBlueprint={onExportBlueprint}
+                      />
+                    );
+                  })}
                 </>
               )}
 
@@ -135,7 +191,7 @@ const ActiveBricksCard: React.FunctionComponent<{
                   <ExtensionGroupHeader
                     key={index}
                     label={`${getOrganizationName(
-                      team.organization_uuid as UUID
+                      team.organization_uuid
                     )} Bricks`}
                   />
                   {team.extensions.map((extensions) => {
@@ -158,10 +214,10 @@ const ActiveBricksCard: React.FunctionComponent<{
                 </>
               ))}
 
-              {deploymentGroups.length > 0 && (
+              {deploymentExtensionGroups.length > 0 && (
                 <>
                   <ExtensionGroupHeader label="Automatic Team Deployments" />
-                  {deploymentGroups.map((extensions) => {
+                  {deploymentExtensionGroups.map((extensions) => {
                     const recipe = extensions[0]._recipe;
                     const deployment = extensions[0]._deployment;
                     const messageContext: MessageContext = {
