70024a4671660632fb1ef72546c52732d51dce1b
Todd Schiller <todd.schiller@gmail.com>
2021-11-19 11:40:47 -0500

    #1930: fix field labels

--- src/devTools/editor/tabs/trigger/TriggerConfiguration.tsx
+++ src/devTools/editor/tabs/trigger/TriggerConfiguration.tsx
@@ -39,6 +39,7 @@ const TriggerConfiguration: React.FC<{
       if (e.currentTarget.value) {
         setFieldValue("extensionPoint.definition.rootSelector", null);
         setFieldValue("extensionPoint.definition.attachMode", null);
+        setFieldValue("extensionPoint.definition.targetMode", null);
       }
 
       setFieldValue("extensionPoint.definition.trigger", e.currentTarget.value);
@@ -87,7 +88,7 @@ const TriggerConfiguration: React.FC<{
                 elements are added to the page.
               </p>
             }
-            {...makeLockableFieldProps("Trigger", isLocked)}
+            {...makeLockableFieldProps("Attach Mode", isLocked)}
           >
             <option value="once">once</option>
             <option value="watch">watch</option>
@@ -107,7 +108,7 @@ const TriggerConfiguration: React.FC<{
                 element selector.
               </p>
             }
-            {...makeLockableFieldProps("Trigger", isLocked)}
+            {...makeLockableFieldProps("Target Mode", isLocked)}
           >
             <option value="eventTarget">eventTarget</option>
             <option value="root">root</option>
