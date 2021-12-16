44568df8c86521c8ff8038e36f7d14fc67072e3e
Federico Brigante <me@fregante.com>
2021-12-17 02:23:32 +0700

    Include `attrs` in frameWorkReader

--- src/blocks/readers/frameworkReader.ts
+++ src/blocks/readers/frameworkReader.ts
@@ -62,7 +62,7 @@ export function frameworkReadFactory(
       waitMillis,
       traverseUp,
       optional,
-      attrs,
+      attrs: attributes,
       pathSpec,
     } = reader;
 
@@ -79,9 +79,9 @@ export function frameworkReadFactory(
       waitMillis,
       optional,
       traverseUp,
-      pathSpec: attrs
+      pathSpec: attributes
         ? Object.fromEntries(
-            castArray(attrs).map((attribute) => [
+            castArray(attributes).map((attribute) => [
               attribute,
               `attrs.${attribute}`,
             ])
