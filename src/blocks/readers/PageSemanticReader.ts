d9cca8171fc7df5550b5f07763ff4734e5aa1c97
Federico Brigante <me@fregante.com>
2022-01-04 16:00:50 +0800

    Merge HTML parsers into one bundle

--- src/blocks/readers/PageSemanticReader.ts
+++ src/blocks/readers/PageSemanticReader.ts
@@ -35,8 +35,8 @@ export class PageSemanticReader extends Reader {
 
   async read(): Promise<ReaderOutput> {
     const [{ Handler }, { Parser }] = await Promise.all([
-      import(/* webpackChunkName: "htmlmetaparser" */ "htmlmetaparser"),
-      import(/* webpackChunkName: "htmlparser2" */ "htmlparser2"),
+      import(/* webpackChunkName: "htmlparsers" */ "htmlmetaparser"),
+      import(/* webpackChunkName: "htmlparsers" */ "htmlparser2"),
     ]);
 
     return new Promise((resolve, reject) => {
