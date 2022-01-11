2727c1cc5f7b679456d3e80b6ec5587ee0e788ce
Ben Loe <below413@gmail.com>
2022-01-11 16:12:07 -0500

    #2315 Mark element dirty in redux state after auto-upgrading an extension to v3 (#2353)

--- src/devTools/editor/extensionPoints/actionPanel.tsx
+++ src/devTools/editor/extensionPoints/actionPanel.tsx
@@ -50,7 +50,6 @@ import {
 } from "@/devTools/editor/extensionPoints/elementConfig";
 import React from "react";
 import { Except } from "type-fest";
-import { upgradePipelineToV3 } from "@/devTools/editor/extensionPoints/upgrade";
 
 type Extension = BaseExtensionState & Except<ActionPanelConfig, "body">;
 
@@ -139,7 +138,6 @@ export async function fromExtensionPoint(
   return {
     uuid: uuidv4(),
     apiVersion: PAGE_EDITOR_DEFAULT_BRICK_API_VERSION,
-    showV3UpgradeMessage: false,
     installed: true,
     type: extensionPoint.definition.type,
     label: heading,
@@ -176,22 +174,9 @@ async function fromExtension(
 
   const base = baseFromExtension(config, extensionPoint.definition.type);
   const extension = extensionWithNormalizedPipeline(config.config, "body");
-  let showV3UpgradeMessage = false;
-  let { apiVersion } = base;
-
-  if (apiVersion === "v2") {
-    extension.blockPipeline = await upgradePipelineToV3(
-      extension.blockPipeline
-    );
-    showV3UpgradeMessage = true;
-    apiVersion = "v3";
-  }
 
   return {
     ...base,
-
-    apiVersion,
-    showV3UpgradeMessage,
 
     extension,
 
