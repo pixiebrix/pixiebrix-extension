444473419389f468570e590a103054c0de1e3c6c
Alex <zhmurik@yandex.ru>
2022-01-11 21:14:05 +0100

    2306 updating recipe.definitions.apiVerstion

--- src/devTools/editor/panes/save/saveHelpers.ts
+++ src/devTools/editor/panes/save/saveHelpers.ts
@@ -140,6 +140,14 @@ export function replaceRecipeExtension(
         sourceRecipe.extensionPoints.length <= 1;
       if (canUpdateRecipeApiVersion) {
         draft.apiVersion = element.apiVersion;
+
+        const extensionPointId = sourceRecipe.extensionPoints[0]?.id;
+        // eslint-disable-next-line security/detect-object-injection -- getting a property by extension id
+        const extensionPointDefinition = draft.definitions?.[extensionPointId];
+
+        if (extensionPointDefinition?.apiVersion != null) {
+          extensionPointDefinition.apiVersion = element.apiVersion;
+        }
       } else {
         throw new Error(
           `Element's API Version (${element.apiVersion}) does not match recipe's API Version (${sourceRecipe.apiVersion}) and recipe's API Version cannot be updated`
