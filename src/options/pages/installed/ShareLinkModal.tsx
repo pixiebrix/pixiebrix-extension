665d92b3f1ce2f304d98dedaee383a53ad23812d
Alex <zhmurik@yandex.ru>
2022-01-19 17:00:56 +0100

    Updating the Modal UI:

--- src/options/pages/installed/ShareLinkModal.tsx
+++ src/options/pages/installed/ShareLinkModal.tsx
@@ -16,12 +16,14 @@
  */
 
 import React from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, InputGroup, Modal } from "react-bootstrap";
 import { push } from "connected-react-router";
 import { useParams } from "react-router-dom";
 import copy from "copy-to-clipboard";
 import useNotifications from "@/hooks/useNotifications";
 import { useDispatch } from "react-redux";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ShareLinkModal = () => {
   const dispatch = useDispatch();
@@ -42,18 +44,32 @@ const ShareLinkModal = () => {
 
   return (
     <Modal show onHide={hideModal}>
-      <Modal.Header>
-        <Modal.Title>Copy the installation link to share</Modal.Title>
+      <Modal.Header closeButton>
+        <Modal.Title>Share</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <a href={installationLink}>{installationLink}</a>
+        <Form.Group>
+          <Form.Label>Copy the activation link to share</Form.Label>
+          <InputGroup>
+            <Form.Control
+              type="text"
+              readOnly
+              defaultValue={installationLink}
+            />
+            <InputGroup.Append>
+              <Button variant="info" onClick={copyLink}>
+                <FontAwesomeIcon icon={faCopy} />
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
       </Modal.Body>
+
       <Modal.Footer>
         <Button variant="link" onClick={hideModal}>
           Close
         </Button>
-        <Button onClick={copyLink}>Copy</Button>
       </Modal.Footer>
     </Modal>
   );
