36dfe3a5622305569e557242c877a4bad45dfff3
Todd Schiller <todd.schiller@gmail.com>
2022-01-20 15:14:02 -0500

    #2361: fix lint in utility method

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -16,29 +16,34 @@
  */
 
 export function timeSince(date: number): string {
-  // Copied from: https://stackoverflow.com/a/3177838/402560
+  // Adapted from: https://stackoverflow.com/a/3177838/402560
   const seconds = Math.floor((Date.now() - date) / 1000);
 
-  let interval = seconds / 31536000;
+  let interval = seconds / 31_536_000;
 
   if (interval > 1) {
-    return Math.floor(interval) + " years";
+    return `${Math.floor(interval)} years`;
   }
-  interval = seconds / 2592000;
+
+  interval = seconds / 2_592_000;
   if (interval > 1) {
-    return Math.floor(interval) + " months";
+    return `${Math.floor(interval)} months`;
   }
-  interval = seconds / 86400;
+
+  interval = seconds / 86_400;
   if (interval > 1) {
-    return Math.floor(interval) + " days";
+    return `${Math.floor(interval)} days`;
   }
+
   interval = seconds / 3600;
   if (interval > 1) {
-    return Math.floor(interval) + " hours";
+    return `${Math.floor(interval)} hours`;
   }
+
   interval = seconds / 60;
   if (interval > 1) {
-    return Math.floor(interval) + " minutes";
+    return `${Math.floor(interval)} minutes`;
   }
-  return Math.floor(seconds) + " seconds";
+
+  return `${Math.floor(seconds)} seconds`;
 }
