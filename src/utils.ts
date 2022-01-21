256c21149d1dedf6a5d878a10e4187d01a9a1e02
Ben Loe <below413@gmail.com>
2022-01-21 11:46:53 -0500

    #2427 Handle empty expression values properly (#2452)

--- src/utils.ts
+++ src/utils.ts
@@ -172,14 +172,14 @@ export function isPrimitive(value: unknown): value is Primitive {
 }
 
 /**
- * Recursively pick entries that match property
+ * Recursively pick entries that match a predicate
  * @param obj an object
  * @param predicate predicate returns true to include an entry
  * @see pickBy
  */
 export function deepPickBy(
   obj: unknown,
-  predicate: (value: unknown) => boolean
+  predicate: (value: unknown, parent?: unknown) => boolean
 ): unknown {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null
   // `typeof null === "object"`, so have to check for it before the "object" check below
@@ -193,7 +193,7 @@ export function deepPickBy(
 
   if (typeof obj === "object") {
     return mapValues(
-      pickBy(obj, (value) => predicate(value)),
+      pickBy(obj, (value) => predicate(value, obj)),
       (value) => deepPickBy(value, predicate)
     );
   }
