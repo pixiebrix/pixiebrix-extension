b0cd98c3cef12d54f5c09d5850f61f1f3e8a8d32
Alex <zhmurik@yandex.ru>
2022-01-25 17:53:11 +0100

    Other extensions warning

--- src/devTools/editor/tabs/blueprintOptionsTab/BlueprintOptionsTab.tsx
+++ src/devTools/editor/tabs/blueprintOptionsTab/BlueprintOptionsTab.tsx
@@ -16,7 +16,7 @@
  */
 import { useFormikContext } from "formik";
 import React, { useMemo, useState } from "react";
-import { Col, Container, Row, Tab } from "react-bootstrap";
+import { Alert, Col, Container, Row, Tab } from "react-bootstrap";
 import { FormState } from "@/devTools/editor/slices/editorSlice";
 import styles from "./BlueprintOptionsTab.module.scss";
 import { RJSFSchema } from "@/components/formBuilder/formBuilderTypes";
@@ -29,6 +29,9 @@ import FieldRuntimeContext, {
 import ErrorBoundary from "@/components/ErrorBoundary";
 import { isEmpty } from "lodash";
 import { FIELD_TYPE_OPTIONS } from "@/components/formBuilder/formBuilderHelpers";
+import { useGetRecipesQuery } from "@/services/api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 const fieldTypes = FIELD_TYPE_OPTIONS.filter((type) =>
   ["Single line text", "Number", "Checkbox"].includes(type.label)
@@ -39,6 +42,9 @@ const BlueprintOptionsTab: React.VoidFunctionComponent<{
 }> = ({ eventKey }) => {
   const [activeField, setActiveField] = useState<string>();
   const { values: formState } = useFormikContext<FormState>();
+  const { data: recipes } = useGetRecipesQuery();
+  const recipe = recipes?.find((x) => x.metadata.id === formState.recipe.id);
+  const countOfExtensionsInRecipe = recipe.extensionPoints.length;
 
   const formRuntimeContext = useMemo<RuntimeContext>(
     () => ({
@@ -66,15 +72,31 @@ const BlueprintOptionsTab: React.VoidFunctionComponent<{
 
   return (
     <Tab.Pane eventKey={eventKey} className={styles.root}>
-      <Container>
+      <Container className="pt-3">
         <Row>
           <ErrorBoundary>
             <Col md="8">
-              <div>
+              <h5 className="mb-3">
                 Editing Options for Blueprint &quot;{formState.recipe.name}
                 &quot;
-              </div>
-              {noOptions && <div>No options defined for this Blueprint</div>}
+              </h5>
+
+              {countOfExtensionsInRecipe > 1 && (
+                <Alert variant="warning">
+                  <FontAwesomeIcon icon={faExclamationTriangle} />
+                  This options are shared with{" "}
+                  {countOfExtensionsInRecipe > 2
+                    ? `other ${countOfExtensionsInRecipe - 1} extensions`
+                    : "another extension"}{" "}
+                  in the blueprint.
+                </Alert>
+              )}
+
+              {noOptions && (
+                <div className="mb-3">
+                  No options defined for this Blueprint
+                </div>
+              )}
               <FieldRuntimeContext.Provider value={formRuntimeContext}>
                 <FormEditor
                   name="optionsDefinition"
@@ -86,7 +108,7 @@ const BlueprintOptionsTab: React.VoidFunctionComponent<{
               </FieldRuntimeContext.Provider>
             </Col>
             <Col md="4">
-              <div>Preview</div>
+              <h5 className="mb-3">Preview</h5>
               <FormPreview
                 rjsfSchema={formState.optionsDefinition as RJSFSchema}
                 activeField={activeField}
