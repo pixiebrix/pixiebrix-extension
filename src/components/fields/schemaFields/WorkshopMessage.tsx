9c2d44f43506cfefed76a1918dd40c0e2ea3f865
Ben Loe <ben@pixiebrix.com>
2022-01-27 19:23:42 -0500

    show workshop message for uipath bricks

---
+++ src/components/fields/schemaFields/WorkshopMessage.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import styles from "@/devTools/editor/tabs/editTab/UpgradedToApiV3.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { Alert } from "react-bootstrap";
+
+const WorkshopMessage: React.FC = () => (
+  <Alert variant="info" className={styles.alert}>
+    <FontAwesomeIcon icon={faExclamationCircle} size="lg" className="mt-1" />
+    <p>
+      This brick contains advanced configurations not yet supported in the page
+      editor, please open this extension in the Workshop if you would like to
+      make changes.{" "}
+      <a
+        href="https://docs.pixiebrix.com/runtime"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Read more about runtime configurations here.
+      </a>
+    </p>
+  </Alert>
+);
+
+export default WorkshopMessage;
