a9f96e39be6e79565f44c357bb57a9873faba6c4
Todd Schiller <todd.schiller@gmail.com>
2022-02-04 14:40:24 -0500

    #2592: remove stray quotes

--- src/extensionPoints/menuItemExtension.ts
+++ src/extensionPoints/menuItemExtension.ts
@@ -558,7 +558,6 @@ export abstract class MenuItemExtensionPoint extends ExtensionPoint<MenuItemExte
             extension.id,
             merge({}, onCancel, DEFAULT_ACTION_RESULTS.cancel)
           );
-          ``;
         } else {
           extensionLogger.error(error);
           notifyResult(
