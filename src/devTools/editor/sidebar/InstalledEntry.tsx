5f4842d68bf282378ba5fd8994c85b90cbab0c39
Todd Schiller <todd.schiller@gmail.com>
2022-02-08 14:13:23 -0500

    #2670: fix indirect import of browser polyfill (#2671)

--- src/devTools/editor/sidebar/InstalledEntry.tsx
+++ src/devTools/editor/sidebar/InstalledEntry.tsx
@@ -25,7 +25,7 @@ import {
   selectType,
 } from "@/devTools/editor/extensionPoints/adapter";
 import { actions } from "@/devTools/editor/slices/editorSlice";
-import { reportError } from "@/errors";
+import reportError from "@/telemetry/reportError";
 import { ListGroup } from "react-bootstrap";
 import {
   NotAvailableIcon,
