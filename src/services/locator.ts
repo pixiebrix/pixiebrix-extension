92d4c84379a000c3655b10f05a5d96076e18561c
Todd Schiller <todd.schiller@gmail.com>
2022-02-13 11:42:06 -0500

    #2715: [WIP] block fetch calls from the content script

--- src/services/locator.ts
+++ src/services/locator.ts
@@ -39,6 +39,7 @@ import { fetch } from "@/hooks/fetch";
 import { validateRegistryId } from "@/types/helpers";
 import { PIXIEBRIX_SERVICE_ID } from "@/services/constants";
 import { ExtensionNotLinkedError } from "@/errors";
+import { forbidContext } from "@/utils/expectContext";
 
 const REF_SECRETS = [
   "https://app.pixiebrix.com/schemas/key#",
