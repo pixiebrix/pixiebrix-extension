2c620e0b8c3ce633498ea347c3ac87f4eef335ae
Federico Brigante <me@fregante.com>
2022-03-01 13:34:32 +0800

    GHA does not have a recent curl version, so use --fail

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -39,11 +39,14 @@ sed s/dist\\/// | \
 # curl https://etc.etc -F minified_url=etc/etc/bundles/icons.js
 # etc
 #
-# `curl` automatically retries if the server is busy
-# https://curl.se/docs/manpage.html#--fail-with-body
+# `curl` automatically retries if the server is busy. --fail throws on HTTP errors
 parallel curl https://api.rollbar.com/api/1/sourcemap/download \
-	--fail-with-body \
+	--fail \
 	--no-progress-meter \
 	-F version="$SOURCE_VERSION" \
 	-F access_token="$ROLLBAR_POST_SERVER_ITEM_TOKEN" \
 	-F minified_url="$SOURCE_MAP_URL_BASE/$SOURCE_MAP_PATH/{}"
+
+# TODO: Replace --fail with --fail-with-body when curl is updated on GitHub Actions (min 7.76.0)
+# https://github.com/actions/virtual-environments/blob/main/images/linux/Ubuntu2004-Readme.md
+# https://curl.se/docs/manpage.html#--fail-with-body
