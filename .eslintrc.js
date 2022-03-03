dc106c337c6e745f34cf99de146fa3b8f613b9ea
Federico Brigante <me@fregante.com>
2022-03-03 22:05:01 +0800

    Extract rules to shared config

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,9 +38,6 @@ module.exports = {
       },
     ],
 
-    // Only enable this on tsx files
-    "filenames/match-exported": "off",
-
     // Avoid imports with side effects
     "import/no-unassigned-import": [
       "error",
@@ -63,16 +60,6 @@ module.exports = {
     // Incorrectly suggests to use `runtime.sendMessage` instead of `browser.runtime.sendMessage`
     "import/no-named-as-default-member": "off",
 
-    // TODO: The rule is currently broken, it should accept `throw unknown` but doesn't
-    "@typescript-eslint/no-throw-literal": "off",
-
-    // TODO: Import extended config from app, after improving it
-    "@typescript-eslint/naming-convention": "off",
-
-    // The rule is unreasonably slow (90 sec lint -> 5 minutes)
-    // https://github.com/pixiebrix/pixiebrix-extension/issues/1080
-    "import/no-cycle": "off",
-
     // Rules that depend on https://github.com/pixiebrix/pixiebrix-extension/issues/775
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/restrict-template-expressions": "warn",
@@ -85,24 +72,13 @@ module.exports = {
     "@typescript-eslint/consistent-type-assertions": "warn",
   },
   ignorePatterns: [
-    "node_modules",
-    ".idea",
-    "dist",
     "artifacts",
-    "scripts/bin",
+    "selenium",
     "src/vendors",
     "src/types/swagger.ts",
-    "src/nativeEditor/Overlay.tsx",
-    "selenium",
+    "scripts/bin",
   ],
   overrides: [
-    {
-      files: ["**/*.tsx", "**/use*.ts"],
-      excludedFiles: ["*.test.tsx", "*.stories.tsx"],
-      rules: {
-        "filenames/match-exported": "error",
-      },
-    },
     {
       files: [
         "webpack.*.js",
@@ -116,17 +92,6 @@ module.exports = {
         jest: true,
       },
       extends: ["pixiebrix/server"],
-      rules: {
-        // TODO: Import extended config from app, after improving it
-        "@typescript-eslint/naming-convention": "off",
-      },
-    },
-    {
-      files: ["*.stories.tsx", "**/__mocks__/**"],
-      rules: {
-        "unicorn/filename-case": "off",
-        "import/no-anonymous-default-export": "off",
-      },
     },
   ],
 };
