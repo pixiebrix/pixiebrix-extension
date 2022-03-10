15c5d22921fd88946b5af168399dcf03bdc2b5be
Federico Brigante <me@fregante.com>
2022-03-10 22:57:17 +0800

    Allow disabling redux-logger via ENV (#2918)

--- src/options/store.ts
+++ src/options/store.ts
@@ -65,7 +65,7 @@ export const persistWorkshopConfig = {
 };
 
 const conditionalMiddleware: Middleware[] = [];
-if (process.env.NODE_ENV === "development") {
+if (typeof createLogger === "function") {
   // Allow tree shaking of logger in production
   // https://github.com/LogRocket/redux-logger/issues/6
   conditionalMiddleware.push(
