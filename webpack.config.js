5e54c26c48a536a1f620253c28d8fac20039d6f8
Federico Brigante <me@fregante.com>
2022-03-23 14:47:27 +0800

    Fix CSP

--- webpack.config.js
+++ webpack.config.js
@@ -150,8 +150,10 @@ function customizeManifest(manifest, isProduction) {
   if (!isProduction) {
     // React Dev Tools app. See https://github.com/pixiebrix/pixiebrix-extension/wiki/Development-commands#react-dev-tools
     policy.add("script-src", "http://localhost:8097");
+    policy.add("script-src", "'unsafe-eval"); // `react-fast-refresh`
     policy.add("connect-src", "ws://localhost:8097/");
     policy.add("connect-src", "ws://localhost:8080/");
+    policy.add("connect-src", "ws://127.0.0.1:8080/");
     policy.add("img-src", "https://pixiebrix-marketplace-dev.s3.amazonaws.com");
   }
 
