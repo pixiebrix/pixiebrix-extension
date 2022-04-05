e1fedb5be53dcae507ca38b91cdee5cc8b930ce2
Ben Loe <ben@pixiebrix.com>
2022-04-05 18:52:45 -0400

    remove change to useReset

--- src/pageEditor/panes/save/useSavingWizard.ts
+++ src/pageEditor/panes/save/useSavingWizard.ts
@@ -133,7 +133,7 @@ const useSavingWizard = () => {
     };
 
     dispatch(editorActions.addElement(personalElement));
-    await reset({ element, shouldShowConfirmation: false });
+    reset({ element, shouldShowConfirmation: false });
     const error = await create({ element: personalElement, pushToCloud: true });
     if (!error) {
       dispatch(editorActions.removeElement(element.uuid));
