24270e981e21ef1b417eef95a6be5df6e2cc83b9
Alex <zhmurik@yandex.ru>
2022-04-06 17:17:26 +0200

    Revert "3026 fix lint"
    This reverts commit 406b5ec5010e0f6aad71b3b98d0047c4508768f1.

--- src/pageEditor/panes/save/useSavingWizard.test.tsx
+++ src/pageEditor/panes/save/useSavingWizard.test.tsx
@@ -41,6 +41,7 @@ import {
 import { selectElements } from "@/pageEditor/slices/editorSelectors";
 import { uuidv4 } from "@/types/helpers";
 import menuItem from "@/pageEditor/extensionPoints/menuItem";
+import pDefer from "p-defer";
 import { pick } from "lodash";
 import extensionsSlice from "@/store/extensionsSlice";
 import { MINIMAL_UI_SCHEMA } from "@/components/formBuilder/formBuilderHelpers";
