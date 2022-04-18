e24db77e918ee9d05ba12dd33433600f858a90ca
Ben Loe <ben@pixiebrix.com>
2022-04-18 12:40:39 -0400

    newline comment

--- src/pageEditor/panes/save/saveHelpers.ts
+++ src/pageEditor/panes/save/saveHelpers.ts
@@ -295,7 +295,8 @@ export function buildRecipe({
   options,
   metadata,
 }: RecipeParts): UnsavedRecipeDefinition {
-  // If there's no source recipe, then we're creating a new one, so we start with an empty recipe definition that will be filled in
+  // If there's no source recipe, then we're creating a new one, so we
+  // start with an empty recipe definition that will be filled in
   const recipe: UnsavedRecipeDefinition = sourceRecipe ?? {
     apiVersion: PAGE_EDITOR_DEFAULT_BRICK_API_VERSION,
     kind: "recipe",
