f59947725aa47acbf8c36f6a44bd278ad3c4e5a1
Federico Brigante <me@fregante.com>
2022-05-12 01:53:03 +0800

    /2

--- src/services/errors.ts
+++ src/services/errors.ts
@@ -77,9 +77,9 @@ export class ProxiedRemoteServiceError extends BusinessError {
 }
 
 /**
- * Abstract base class for request errors from client to 3rd-party service.
+ * Base class for request errors from client to 3rd-party service.
  */
-export abstract class ClientRequestError extends BusinessError {
+export class ClientRequestError extends BusinessError {
   override name = "ClientRequestError";
   override readonly cause: SerializableAxiosError;
   constructor(message: string, options: { cause: AxiosError }) {
