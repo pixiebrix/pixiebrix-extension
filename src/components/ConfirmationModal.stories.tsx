e29efc96181fd22806920ab0e6dd2570b6430347
abdullah4244 <islamabdullah4244@gmail.com>
2022-05-20 17:53:27 +0500

    indentation fixed pre-commit

--- src/components/ConfirmationModal.stories.tsx
+++ src/components/ConfirmationModal.stories.tsx
@@ -15,64 +15,73 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React,{ComponentProps , useCallback} from "react";
-import { ComponentMeta ,Story } from "@storybook/react";
+import React, { ComponentProps, useCallback } from "react";
+import { ComponentMeta, Story } from "@storybook/react";
 import AsyncButton from "./AsyncButton";
-import {ModalProvider ,useModals} from "@/components/ConfirmationModal";
+import { ModalProvider, useModals } from "@/components/ConfirmationModal";
 
 export default {
   title: "Components/ConfirmationModal",
   component: ModalProvider,
-  } as ComponentMeta<typeof ModalProvider>;
+} as ComponentMeta<typeof ModalProvider>;
 
 type StoryType = ComponentProps<typeof ModalProvider> & {
-    title?: string;
-    message?:string;
-    submitCaption?: string;
-    cancelCaption?: string;
-  };
+  title?: string;
+  message?: string;
+  submitCaption?: string;
+  cancelCaption?: string;
+};
 type ChildComponentType = {
-    title?: string;
-    message?: string;
-    submitCaption?: string;
-    cancelCaption?: string;
-}
-  const ChildComponent = ({title,message,submitCaption,cancelCaption} : ChildComponentType) => {
-    const {showConfirmation} = useModals();
-    const buttonAction = useCallback(async () => {
-         const confirm = await showConfirmation({
-           title,
-           message,
-           submitCaption,
-           cancelCaption
-         });
-     
-         if (!confirm) {}
-         // Do any action here if confirm === true
-       }, [showConfirmation]);
-       return (
-        <AsyncButton onClick={buttonAction}>
-        Confirm Modal
-        </AsyncButton>
-       )
-  }
+  title?: string;
+  message?: string;
+  submitCaption?: string;
+  cancelCaption?: string;
+};
+const ChildComponent = ({
+  title,
+  message,
+  submitCaption,
+  cancelCaption,
+}: ChildComponentType) => {
+  const { showConfirmation } = useModals();
+  const buttonAction = useCallback(async () => {
+    const confirm = await showConfirmation({
+      title,
+      message,
+      submitCaption,
+      cancelCaption,
+    });
 
-const Template: Story<StoryType>= (args) => {
-    const {title,message,submitCaption,cancelCaption} = args;
-    
-return (
-  <ModalProvider {...args} >
-      <ChildComponent title={title} message={message} submitCaption={submitCaption} cancelCaption={cancelCaption}/>
-  </ModalProvider>
-)};
+    if (!confirm) {
+    }
+    // Do any action here if confirm === true
+  }, [showConfirmation]);
+  return <AsyncButton onClick={buttonAction}>Confirm Modal</AsyncButton>;
+};
+
+const Template: Story<StoryType> = (args) => {
+  const { title, message, submitCaption, cancelCaption } = args;
+
+  return (
+    <ModalProvider {...args}>
+      <ChildComponent
+        title={title}
+        message={message}
+        submitCaption={submitCaption}
+        cancelCaption={cancelCaption}
+      />
+    </ModalProvider>
+  );
+};
 
 export const DefaultContent = Template.bind({});
 DefaultContent.args = null;
 
 export const CustomContent = Template.bind({});
 CustomContent.args = {
-    title: "Delete campaign?",
-    message: "Are you sure you want to delete campaign? This action cannot be undone.",
-    submitCaption: "Yes, delete it",
-    cancelCaption: "Back to safety",
+  title: "Delete campaign?",
+  message:
+    "Are you sure you want to delete campaign? This action cannot be undone.",
+  submitCaption: "Yes, delete it",
+  cancelCaption: "Back to safety",
 };
