089301d2726b1e8df9696ba5a22d65194dc1576d
Federico Brigante <me@fregante.com>
2022-06-04 11:55:58 +0800

    Drop save button

--- src/options/pages/settings/AdvancedSettings.tsx
+++ src/options/pages/settings/AdvancedSettings.tsx
@@ -19,16 +19,15 @@ import styles from "./AdvancedSettings.module.scss";
 
 import { Button, Card, Form } from "react-bootstrap";
 import { DEFAULT_SERVICE_URL, useConfiguredHost } from "@/services/baseService";
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import { clearExtensionAuth } from "@/auth/token";
 import chromeP from "webext-polyfill-kinda";
 import notify from "@/utils/notify";
 import useFlags from "@/hooks/useFlags";
 import settingsSlice from "@/store/settingsSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { assertHttpsUrl, sleep } from "@/utils";
+import { assertHttpsUrl } from "@/utils";
 import { selectSettings } from "@/store/settingsSelectors";
-import AsyncButton from "@/components/AsyncButton";
 import { uuidv4 } from "@/types/helpers";
 import pTimeout from "p-timeout";
 
@@ -41,7 +40,6 @@ const AdvancedSettings: React.FunctionComponent = () => {
   const { partnerId } = useSelector(selectSettings);
 
   const [serviceURL, setServiceURL] = useConfiguredHost();
-  const [newPixiebrixUrl, setNewPixiebrixUrl] = useState<string>(null);
 
   const clear = useCallback(async () => {
     await clearExtensionAuth();
@@ -57,7 +55,7 @@ const AdvancedSettings: React.FunctionComponent = () => {
     browser.runtime.reload();
   }, []);
 
-  const update = useCallback(async () => {
+  const requestExtensionUpdate = useCallback(async () => {
     const status = await chromeP.runtime.requestUpdateCheck();
     if (status === "update_available") {
       browser.runtime.reload();
@@ -68,62 +66,55 @@ const AdvancedSettings: React.FunctionComponent = () => {
     }
   }, []);
 
-  const handleUpdate = useCallback(
-    async (event: React.ChangeEvent<HTMLInputElement>) => {
-      console.log({ willSet: event.target.value.trim() });
+  const handleServiceURLUpdate = useCallback(
+    async (event: React.FocusEvent<HTMLInputElement>) => {
+      const newPixiebrixUrl = event.target.value.trim();
+      console.debug("Update service URL", { newPixiebrixUrl, serviceURL });
 
-      setNewPixiebrixUrl(event.target.value.trim());
+      try {
+        // Ensure it's a valid URL
+        if (newPixiebrixUrl) {
+          assertHttpsUrl(newPixiebrixUrl);
+        }
+      } catch (error) {
+        notify.error({
+          id: SAVING_URL_NOTIFICATION_ID,
+          error,
+          reportError: false,
+        });
+        return;
+      }
+
+      try {
+        if (newPixiebrixUrl) {
+          // Ensure it's connectable
+          const response = await pTimeout(
+            fetch(new URL("api/me", newPixiebrixUrl).href),
+            SAVING_URL_TIMEOUT_MS
+          );
+
+          // Ensure it returns a JSON response. It's just `{}` when the user is logged out.
+          await response.json();
+        }
+      } catch {
+        notify.error({
+          id: SAVING_URL_NOTIFICATION_ID,
+          message: "The URL does not appear to point to a PixieBrix server",
+          reportError: false,
+        });
+        return;
+      }
+
+      await setServiceURL(newPixiebrixUrl);
+      notify.success({
+        id: SAVING_URL_NOTIFICATION_ID,
+        message: "Service URL updated. The extension must be reloaded",
+        dismissable: false,
+        duration: Number.POSITIVE_INFINITY,
+      });
     },
-    [setNewPixiebrixUrl]
+    [serviceURL, setServiceURL]
   );
-
-  const saveAndReload = useCallback(async () => {
-    console.debug("Update service URL", { newPixiebrixUrl, serviceURL });
-
-    try {
-      // Ensure it's a valid URL
-      if (newPixiebrixUrl) {
-        assertHttpsUrl(newPixiebrixUrl);
-      }
-    } catch (error) {
-      notify.error({
-        id: SAVING_URL_NOTIFICATION_ID,
-        error,
-        reportError: false,
-      });
-      return;
-    }
-
-    try {
-      if (newPixiebrixUrl) {
-        // Ensure it's connectable
-        const response = await pTimeout(
-          fetch(new URL("api/me", newPixiebrixUrl).href),
-          SAVING_URL_TIMEOUT_MS
-        );
-
-        // Ensure it returns a JSON response. It's just `{}` when the user is logged out.
-        await response.json();
-      }
-    } catch {
-      notify.error({
-        id: SAVING_URL_NOTIFICATION_ID,
-        message: "The URL does not appear to point to a PixieBrix server",
-        reportError: false,
-      });
-      return;
-    }
-
-    await setServiceURL(newPixiebrixUrl);
-    notify.success({
-      id: SAVING_URL_NOTIFICATION_ID,
-      message: "Updated the service URL, the extension will be reloaded now",
-      dismissable: false,
-      duration: 9000,
-    });
-    await sleep(3000);
-    browser.runtime.reload();
-  }, [newPixiebrixUrl, serviceURL, setServiceURL]);
 
   return (
     <Card>
@@ -139,15 +130,10 @@ const AdvancedSettings: React.FunctionComponent = () => {
               type="text"
               placeholder={DEFAULT_SERVICE_URL}
               defaultValue={serviceURL}
-              onInput={handleUpdate}
+              onBlur={handleServiceURLUpdate}
               disabled={restrict("service-url")}
             />
           </Form.Group>
-          {newPixiebrixUrl == null || (
-            <AsyncButton variant="success" onClick={saveAndReload}>
-              Save and reload the extension
-            </AsyncButton>
-          )}
           {flagOn("partner-theming") && (
             <Form.Group controlId="partnerId">
               <Form.Label>Partner ID</Form.Label>
@@ -155,7 +141,7 @@ const AdvancedSettings: React.FunctionComponent = () => {
                 type="text"
                 placeholder="my-company"
                 defaultValue={partnerId ?? ""}
-                onBlur={(event: React.ChangeEvent<HTMLInputElement>) => {
+                onBlur={(event: React.FocusEvent<HTMLInputElement>) => {
                   dispatch(
                     settingsSlice.actions.setPartnerId({
                       partnerId: event.target.value,
@@ -173,7 +159,7 @@ const AdvancedSettings: React.FunctionComponent = () => {
           Reload Extension
         </Button>
 
-        <Button variant="info" onClick={update}>
+        <Button variant="info" onClick={requestExtensionUpdate}>
           Check Updates
         </Button>
 
