893c9e52db93b2332e52e6ec2367baa3befd1294
Todd Schiller <todd.schiller@gmail.com>
2022-06-05 20:01:18 -0400

    #3570: elimate flickering/FOUC in panels

--- src/blocks/renderers/BootstrapStylesheet.tsx
+++ src/blocks/renderers/BootstrapStylesheet.tsx
@@ -15,11 +15,33 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import theme from "bootstrap/dist/css/bootstrap.min.css?loadAsUrl";
+import { noop } from "lodash";
 
-const BootstrapStylesheet: React.FC = () => (
-  <link rel="stylesheet" href={theme} />
-);
+type BootstrapStylesheetProps = {
+  // Allow parent to avoid a FOUC: https://webkit.org/blog/66/the-fouc-problem/ by waiting for the stylesheet to have
+  // had a chance to load.
+  // The load event fires once the stylesheet and all of its imported content has been loaded and parsed, and
+  // immediately before the styles start being applied to the content.
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link#stylesheet_load_events
+  onLoad?: () => void;
+};
+
+const BootstrapStylesheet: React.FC<BootstrapStylesheetProps> = ({
+  onLoad = noop,
+}) => {
+  const linkRef = useRef<HTMLLinkElement>();
+
+  useEffect(() => {
+    linkRef.current.addEventListener("load", () => {
+      requestAnimationFrame(() => {
+        onLoad();
+      });
+    });
+  }, [onLoad]);
+
+  return <link rel="stylesheet" href={theme} ref={linkRef} />;
+};
 
 export default BootstrapStylesheet;
