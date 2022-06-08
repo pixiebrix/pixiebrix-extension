1bc206c1b04e28776b999ded0e975e22118cf89a
Federico Brigante <me@fregante.com>
2022-06-08 13:01:15 +0700

    Lint

--- src/pageEditor/sidebar/Sidebar.tsx
+++ src/pageEditor/sidebar/Sidebar.tsx
@@ -65,7 +65,7 @@ import {
   selectIsAddToRecipeModalVisible,
 } from "@/pageEditor/slices/editorSelectors";
 import { useDispatch, useSelector } from "react-redux";
-import { EditorState, FormState } from "@/pageEditor/pageEditorTypes";
+import { FormState } from "@/pageEditor/pageEditorTypes";
 import { selectExtensions } from "@/store/extensionsSelectors";
 import { useGetRecipesQuery } from "@/services/api";
 import { getIdForElement, getRecipeIdForElement } from "@/pageEditor/utils";
