eddb18f9f819aae0d81187076f50a3afe78667bd
Todd Schiller <todd.schiller@gmail.com>
2022-08-15 18:04:53 -0400

    #3992: include workspace in AA bot options in Page Editor (#4054)

--- src/contrib/automationanywhere/BotOptions.tsx
+++ src/contrib/automationanywhere/BotOptions.tsx
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useMemo } from "react";
 import { partial } from "lodash";
 import { BlockOptionProps } from "@/components/fields/schemaFields/genericOptionsFactory";
 import { COMMON_PROPERTIES } from "@/contrib/automationanywhere/RunBot";
@@ -29,16 +29,24 @@ import RemoteSelectWidget from "@/components/form/widgets/RemoteSelectWidget";
 import { joinName } from "@/utils";
 import RequireServiceConfig from "@/contrib/RequireServiceConfig";
 import {
+  cachedFetchBotFile,
   cachedFetchBots,
   cachedFetchDevicePools,
   cachedFetchDevices,
   cachedFetchRunAsUsers,
   cachedFetchSchema,
 } from "@/contrib/automationanywhere/aaApi";
-import { AUTOMATION_ANYWHERE_SERVICE_ID } from "./contract";
+import { AUTOMATION_ANYWHERE_SERVICE_ID, WorkspaceType } from "./contract";
 import { isCommunityControlRoom } from "@/contrib/automationanywhere/aaUtils";
 import BooleanWidget from "@/components/fields/schemaFields/widgets/BooleanWidget";
 import RemoteMultiSelectWidget from "@/components/form/widgets/RemoteMultiSelectWidget";
+import SelectWidget from "@/components/form/widgets/SelectWidget";
+import { useAsyncEffect } from "use-async-effect";
+
+const WORKSPACE_OPTIONS = [
+  { value: "public", label: "Public" },
+  { value: "private", label: "Private/Local" },
+];
 
 const BotOptions: React.FunctionComponent<BlockOptionProps> = ({
   name,
@@ -50,21 +58,47 @@ const BotOptions: React.FunctionComponent<BlockOptionProps> = ({
     AUTOMATION_ANYWHERE_SERVICE_ID
   );
 
+  const [{ value: workspaceType }, , { setValue: setWorkspaceType }] =
+    useField<string>(configName("workspaceType"));
+
   const [{ value: fileId }] = useField<string>(configName("fileId"));
+
+  // Default the workspaceType based on the file id
+  useAsyncEffect(async () => {
+    if (config && isCommunityControlRoom(config.config.controlRoomUrl)) {
+      // In community edition, each user just works in their own private workspace
+      setWorkspaceType("private");
+    }
+
+    if (hasPermissions && config && workspaceType == null && fileId) {
+      const result = await cachedFetchBotFile(config, fileId);
+      const workspaceType =
+        result.workspaceType === "PUBLIC" ? "public" : "private";
+      setWorkspaceType(workspaceType);
+    }
+    // Leave setWorkspaceType off the dependency list because Formik changes reference on each render
+  }, [config, fileId, hasPermissions, workspaceType]);
+
+  const factoryArgs = useMemo(
+    () => ({
+      workspaceType: (workspaceType ?? "private") as WorkspaceType,
+    }),
+    [workspaceType]
+  );
 
   const [remoteSchema, remoteSchemaPending, remoteSchemaError] =
     useAsyncState(async () => {
       if (hasPermissions && config) {
         // HACK: hack to avoid concurrent requests to the proxy. Simultaneous calls to get the token causes a
         // server error on community edition
-        await cachedFetchDevices(config);
-        await cachedFetchBots(config);
-        await cachedFetchRunAsUsers(config);
+        await cachedFetchDevices(config, factoryArgs);
+        await cachedFetchBots(config, factoryArgs);
+        await cachedFetchRunAsUsers(config, factoryArgs);
         return cachedFetchSchema(config, fileId);
       }
 
       return null;
-    }, [config, fileId, hasPermissions]);
+    }, [config, fileId, factoryArgs, hasPermissions]);
 
   return (
     <RequireServiceConfig
@@ -73,12 +107,24 @@ const BotOptions: React.FunctionComponent<BlockOptionProps> = ({
     >
       {({ config }) => (
         <>
+          {!isCommunityControlRoom(config.config.controlRoomUrl) && (
+            <ConnectedFieldTemplate
+              label="Workspace"
+              name={configName("workspaceType")}
+              description="The Control Room Workspace"
+              as={SelectWidget}
+              defaultValue="private"
+              options={WORKSPACE_OPTIONS}
+            />
+          )}
+
           <ConnectedFieldTemplate
             label="Bot"
             name={configName("fileId")}
             description="The Automation Anywhere bot"
             as={RemoteSelectWidget}
             optionsFactory={cachedFetchBots}
+            factoryArgs={factoryArgs}
             config={config}
           />
 
@@ -89,27 +135,34 @@ const BotOptions: React.FunctionComponent<BlockOptionProps> = ({
               description="The device to run the bot on"
               as={RemoteSelectWidget}
               optionsFactory={cachedFetchDevices}
+              factoryArgs={factoryArgs}
               config={config}
             />
           ) : (
             <>
-              <ConnectedFieldTemplate
-                label="Run as Users"
-                name={configName("runAsUserIds")}
-                description="The user(s) to run the bots"
-                as={RemoteMultiSelectWidget}
-                optionsFactory={cachedFetchRunAsUsers}
-                config={config}
-              />
-              <ConnectedFieldTemplate
-                label="Device Pools"
-                name={configName("poolIds")}
-                description="A device pool that has at least one active device (optional)"
-                as={RemoteMultiSelectWidget}
-                optionsFactory={cachedFetchDevicePools}
-                blankValue={[]}
-                config={config}
-              />
+              {workspaceType === "public" && (
+                <>
+                  <ConnectedFieldTemplate
+                    label="Run as Users"
+                    name={configName("runAsUserIds")}
+                    description="The user(s) to run the bots"
+                    as={RemoteMultiSelectWidget}
+                    optionsFactory={cachedFetchRunAsUsers}
+                    factoryArgs={factoryArgs}
+                    config={config}
+                  />
+                  <ConnectedFieldTemplate
+                    label="Device Pools"
+                    name={configName("poolIds")}
+                    description="A device pool that has at least one active device (optional)"
+                    as={RemoteMultiSelectWidget}
+                    optionsFactory={cachedFetchDevicePools}
+                    factoryArgs={factoryArgs}
+                    blankValue={[]}
+                    config={config}
+                  />
+                </>
+              )}
               <ConnectedFieldTemplate
                 label="Await Result"
                 name={configName("awaitResult")}
