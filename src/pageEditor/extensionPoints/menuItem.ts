11509b7e6cb87b3f8483fa8dce57985b5235604e
Todd Schiller <todd.schiller@gmail.com>
2022-08-18 13:24:00 -0400

    #4010: provide toggle for default success message on menu item extensions (#4066)

--- src/pageEditor/extensionPoints/menuItem.ts
+++ src/pageEditor/extensionPoints/menuItem.ts
@@ -79,6 +79,7 @@ function fromNativeElement(
       caption: button.item.caption,
       blockPipeline: [],
       dynamicCaption: false,
+      onSuccess: true,
     },
   };
 }
@@ -115,6 +116,7 @@ function selectExtension(
       ? extension.blockPipeline
       : omitEditorMetadata(extension.blockPipeline),
     dynamicCaption: extension.dynamicCaption,
+    onSuccess: extension.onSuccess,
   };
   return removeEmptyValues({
     ...baseSelectExtension(state),
@@ -146,6 +148,7 @@ async function fromExtensionPoint(
       caption:
         extensionPoint.definition.defaultOptions?.caption ?? "Custom Action",
       blockPipeline: [],
+      onSuccess: true,
     },
 
     // There's no containerInfo for the page because the user did not select it during the session
