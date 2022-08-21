feec13bf66d5666aedbe0e9da82efcc261fd371e
balehok <zhmurik@yandex.ru>
2022-08-21 21:02:55 +0200

    4020 notify warning

---
+++ src/utils/notify.stories.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2022 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "react-bootstrap";
+import notify, { initToaster, NotificationType } from "./notify";
+
+const notificationTypes = ["info", "success", "error", "warning", "loading"];
+initToaster();
+
+const NotifyButton = ({ type }: { type: NotificationType }) => (
+  <Button
+    variant="primary"
+    onClick={() => {
+      notify.show(type, String(type));
+    }}
+  >
+    Show notification
+  </Button>
+);
+
+export default {
+  title: "Common/notify",
+  component: NotifyButton,
+  argTypes: {
+    type: {
+      options: notificationTypes,
+      control: { type: "select" },
+    },
+  },
+} as ComponentMeta<typeof NotifyButton>;
+
+export const Default: ComponentStory<typeof NotifyButton> = (args) => (
+  <NotifyButton {...args} />
+);
+Default.args = {
+  type: "info",
+};
