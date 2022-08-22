244369cdc4072c282d6000b3ed2b6ed24857544b
Federico Brigante <me@fregante.com>
2022-08-22 23:16:01 +0700

    #4021: Replace confusing "error  modal in option page" UI with a plain alert (#4080)

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -32,7 +32,6 @@ import Sidebar from "@/options/Sidebar";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import EnvironmentBanner from "@/layout/EnvironmentBanner";
-import ErrorModal from "@/layout/ErrorModal";
 import ActivateBlueprintPage from "@/options/pages/marketplace/ActivateBlueprintPage";
 import ActivateExtensionPage from "@/options/pages/activateExtension/ActivatePage";
 import useRefresh from "@/hooks/useRefresh";
@@ -80,7 +79,6 @@ const Layout = () => {
             <RefreshBricks />
             <Sidebar />
             <div className="main-panel">
-              <ErrorModal />
               <BrowserBanner />
               <EnvironmentBanner />
               <UpdateBanner />
