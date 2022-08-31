08dfb1dde7586e996316e30b183144d7129dedd3
Ben Loe <ben@pixiebrix.com>
2022-08-31 18:50:47 -0400

    dont remove action buttons from tab layout

--- src/pageEditor/panes/RecipePane.tsx
+++ src/pageEditor/panes/RecipePane.tsx
@@ -25,41 +25,20 @@ import EditorTabLayout, {
   ActionButton,
   TabItem,
 } from "@/components/tabLayout/EditorTabLayout";
-import {
-  faHistory,
-  faQuestionCircle,
-  faSave,
-  faTrash,
-} from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import AskQuestionModal from "@/pageEditor/askQuestion/AskQuestionModal";
-import useSaveRecipe from "@/pageEditor/hooks/useSaveRecipe";
-import useRemoveRecipe from "@/pageEditor/hooks/useRemoveRecipe";
 import Logs from "@/pageEditor/tabs/Logs";
 import EditRecipe from "@/pageEditor/tabs/editRecipeTab/EditRecipe";
 import { MessageContext } from "@/core";
 import { logActions } from "@/components/logViewer/logSlice";
 import useLogsBadgeState from "@/pageEditor/tabs/logs/useLogsBadgeState";
 import RecipeOptions from "@/pageEditor/tabs/RecipeOptions";
-import useResetRecipe from "@/pageEditor/hooks/useResetRecipe";
 
 const RecipePane: React.VFC = () => {
   const activeRecipeId = useSelector(selectActiveRecipeId);
   const [showQuestionModal, setShowQuestionModal] = useState(false);
 
-  // We need to force the component to re-render when the recipe is reset
-  // from the action button, or when a different recipe is selected, so
-  // we'll use a key prop on the tab layout that is composed of a useState()
-  // variable counter that we can increment manually, and the recipeId.
-  const [layoutCounter, setLayoutCounter] = useState(0);
-  const layoutKey = `${activeRecipeId}-${layoutCounter}`;
-  const forceRefreshLayout = () => {
-    setLayoutCounter(layoutCounter + 1);
-  };
-
-  const { save: saveRecipe, isSaving: isSavingRecipe } = useSaveRecipe();
   const dispatch = useDispatch();
-  const resetRecipe = useResetRecipe();
-  const removeRecipe = useRemoveRecipe();
 
   useEffect(() => {
     const messageContext: MessageContext = {
@@ -98,38 +77,6 @@ const RecipePane: React.VFC = () => {
       caption: "Ask a question",
       icon: faQuestionCircle,
     },
-    {
-      // Save
-      variant: "primary",
-      onClick() {
-        // Recipe saver has internal error handling, so we can fire-and-forget here
-        void saveRecipe(activeRecipeId);
-      },
-      caption: "Save",
-      disabled: isSavingRecipe,
-      icon: faSave,
-    },
-    {
-      // Reset
-      variant: "warning",
-      async onClick() {
-        await resetRecipe(activeRecipeId);
-        forceRefreshLayout();
-      },
-      caption: "Reset",
-      disabled: isSavingRecipe,
-      icon: faHistory,
-    },
-    {
-      // Remove
-      variant: "danger",
-      onClick() {
-        void removeRecipe({ recipeId: activeRecipeId });
-        forceRefreshLayout();
-      },
-      caption: "Remove",
-      icon: faTrash,
-    },
   ];
 
   if (!activeRecipeId) {
@@ -143,7 +90,7 @@ const RecipePane: React.VFC = () => {
   return (
     <div className={styles.root}>
       <EditorTabLayout
-        key={layoutKey}
+        key={activeRecipeId}
         tabs={tabItems}
         actionButtons={buttons}
       />
