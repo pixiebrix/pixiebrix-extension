8912df9fee6b7938b57d20161b2591114f13810f
Misha Holtz <mishmish@Mishas-MacBook-Pro.local>
2022-09-20 12:18:57 -0700

    remove unused dependency

--- src/options/pages/blueprints/onboardingView/OnboardingView.tsx
+++ src/options/pages/blueprints/onboardingView/OnboardingView.tsx
@@ -22,9 +22,7 @@ import { Button, Card, Col, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import marketplaceImage from "@img/marketplace.svg";
-import useOnboarding, {
-  OnboardingType,
-} from "@/options/pages/blueprints/onboardingView/useOnboarding";
+import { OnboardingType } from "@/options/pages/blueprints/onboardingView/useOnboarding";
 import blueprintsSlice from "@/options/pages/blueprints/blueprintsSlice";
 import { useDispatch } from "react-redux";
 import workshopImage from "@img/workshop.svg";
