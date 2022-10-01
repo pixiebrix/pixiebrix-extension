78080f684a6ffc84e91aec4d3abeea7dd8c63c73
Federico Brigante <me@fregante.com>
2022-10-01 13:49:28 +0700

    #4357: Use `fetch` in background worker (#4374)

--- src/background/background.ts
+++ src/background/background.ts
@@ -22,8 +22,7 @@ import "@/development/autoreload";
 import "@/development/errorsBadge";
 
 // Required for MV3; Service Workers don't have XMLHttpRequest
-// TODO: Disabled due to https://github.com/pixiebrix/pixiebrix-extension/issues/3020
-// import "@/background/axiosFetch";
+import "@/background/axiosFetch";
 
 import "webext-dynamic-content-scripts";
 
