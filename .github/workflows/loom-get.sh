c2c88282fdd8299d007c358aabf385b94cb97e04
Federico Brigante <me@fregante.com>
2022-10-19 13:19:30 +0700

    Cleanup

--- .github/workflows/loom-get.sh
+++ .github/workflows/loom-get.sh
@@ -5,14 +5,9 @@ die () {
 }
 
 [ "$#" -eq 1 ] || die "PR number required as argument"
-PR=$1
 
-LOOM_REGEX="https://www.loom.com/share/\w\{32\}"
-
-printf "\nFIRST POST\n----------\n"
-gh pr view $PR --repo $GITHUB_REPOSITORY | tee FIRST_POST
-
-printf "\nLOOM LINK\n---------\n"
-grep $LOOM_REGEX FIRST_POST --max-count 1 --only-matching | tee LOOM_LINK || true
+gh pr view $1 --repo $GITHUB_REPOSITORY |
+grep "https://www.loom.com/share/\w\{32\}" --max-count 1 --only-matching |
+tee LOOM_LINK || true
 
 echo "LOOM_LINK=$(cat LOOM_LINK)" >> $GITHUB_ENV
