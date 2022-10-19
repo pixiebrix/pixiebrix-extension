6d0d291fc2c02154cb1b38c80afae456beff4d4d
Federico Brigante <me@fregante.com>
2022-10-19 14:00:05 +0700

    Comments

--- .github/workflows/loom-get.sh
+++ .github/workflows/loom-get.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-die () {
-    echo >&2 "$@"
-    exit 1
-}
 
-[ "$#" -eq 1 ] || die "PR number required as argument"
+gh pr view $1 | # Get first post
+grep "https://www.loom.com/share/\w\{32\}" --max-count 1 --only-matching | # Find first loom
+tee LOOM_LINK || true # Save it in ./LOOM_LINK and don't throw if not found
 
-gh pr view $1 |
-grep "https://www.loom.com/share/\w\{32\}" --max-count 1 --only-matching |
-tee LOOM_LINK || true
-
+# Also store it in a $LOOM_LINK global env
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
 echo "LOOM_LINK=$(cat LOOM_LINK)" >> $GITHUB_ENV
