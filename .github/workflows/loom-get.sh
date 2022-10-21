221fd3cb91e65c6bbb05a762c1c6ab4ec5925f89
Federico Brigante <me@fregante.com>
2022-10-21 19:43:25 +0700

    Improve "PR to Slack" automation (for reusability) (#4515)

--- .github/workflows/loom-get.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gh pr view $1 | # Get first post
-grep "https://www.loom.com/share/\w\{32\}" --max-count 1 --only-matching | # Find first loom
-tee LOOM_LINK || true # Save it in ./LOOM_LINK and don't throw if not found
-
-# Also store it in a $LOOM_LINK global env
-# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
-echo "LOOM_LINK=$(cat LOOM_LINK)" >> $GITHUB_ENV
