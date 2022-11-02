64f6ca1057f31da6c727d3defb2876a1cf5797fc
balehok <zhmurik@yandex.ru>
2022-11-02 11:29:09 +0100

    4485 performance measurements

---
+++ src/utils/performance.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { recordWarning } from "@/background/logging";
+
+/**
+ * Adds a performance mark for the initialization of an app
+ */
+export function markAppStart() {
+  performance.mark("app-start");
+}
+
+/**
+ * Measures the time between the app start and the current time
+ * @param action String message to log. A pattern to follow is <Place of measurement>:<Action>. For example, "sidebarExpanded:allRecipesLoaded"
+ * @param reportThreshold The threshold in milliseconds to report the measurement as warning to rollbar. Pass null to not report
+ */
+export function measurePerformanceFromAppStart(
+  action: string,
+  reportThreshold: number | null = 5000
+) {
+  const { duration } = performance.measure("app-start");
+  const message = `Performance: "${action}" took ${Math.round(duration)}ms`;
+  const data = {
+    duration,
+    threshold: reportThreshold,
+    timestamp: new Date().toISOString(),
+  };
+  console.debug(message, data);
+
+  if (reportThreshold != null && duration > reportThreshold) {
+    void recordWarning(null, message, data);
+  }
+}
