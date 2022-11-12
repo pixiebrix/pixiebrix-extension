4fe03f62e4baab987ce146f9eb68a4c3418997f3
Todd Schiller <todd.schiller@gmail.com>
2022-11-11 21:50:04 -0500

    #4593: handle unnamed fields in form reader (#4636)

--- src/blocks/transformers/FormData.test.ts
+++ src/blocks/transformers/FormData.test.ts
@@ -23,28 +23,26 @@ import { FormData } from "./FormData";
 describe("FormData block", () => {
   test("Basic form serialization", async () => {
     const brick = new FormData();
-    document.body.insertAdjacentHTML(
-      "afterbegin",
-      html`
-        <form id="serialize">
-          <input name="no-value" />
 
-          <input name="no-type" value="mongolia" />
+    document.body.innerHTML = html`
+      <form id="serialize">
+        <input name="no-value" />
 
-          <input type="text" name="text" value="georgia" />
+        <input name="no-type" value="mongolia" />
 
-          <input type="checkbox" name="check" value="uruguay" />
-          <input type="checkbox" name="check" value="burundi" checked />
+        <input type="text" name="text" value="georgia" />
 
-          <input type="checkbox" name="check-no-val" checked />
+        <input type="checkbox" name="check" value="uruguay" />
+        <input type="checkbox" name="check" value="burundi" checked />
 
-          <input type="checkbox" name="check-no-val-unchecked" />
+        <input type="checkbox" name="check-no-val" checked />
 
-          <input type="radio" name="radio" value="bahamas" />
-          <input type="radio" name="radio" value="benin" checked />
-        </form>
-      `
-    );
+        <input type="checkbox" name="check-no-val-unchecked" />
+
+        <input type="radio" name="radio" value="bahamas" />
+        <input type="radio" name="radio" value="benin" checked />
+      </form>
+    `;
 
     const arg = unsafeAssumeValidArg({ selector: "#serialize" });
 
@@ -64,6 +62,61 @@ describe("FormData block", () => {
       "check-no-val": true,
       "check-no-val-unchecked": false,
       radio: "benin",
+    });
+  });
+
+  test("Django Admin Logout Form Serialization", async () => {
+    const brick = new FormData();
+
+    // The name-less field was causing problems here: https://github.com/pixiebrix/pixiebrix-extension/issues/4593
+    document.body.innerHTML = html`
+      <form id="logout-form" method="post" action="/admin/logout/">
+        <input type="hidden" name="csrfmiddlewaretoken" value="redacted" />
+        <button type="submit">Log out</button>
+      </form>
+    `;
+
+    const arg = unsafeAssumeValidArg({ selector: "#logout-form" });
+
+    const result = await brick.run(arg, {
+      ctxt: null,
+      logger: null,
+      root: null,
+      runPipeline: neverPromise,
+      runRendererPipeline: neverPromise,
+    });
+
+    expect(result).toEqual({
+      csrfmiddlewaretoken: "redacted",
+    });
+  });
+
+  test("Multiple matching forms iterates all fields", async () => {
+    const brick = new FormData();
+    document.body.innerHTML = html`
+      <div>
+        <form>
+          <input type="text" name="foo" value="42" />
+        </form>
+        <form>
+          <input type="text" name="bar" value="42" />
+        </form>
+      </div>
+    `;
+
+    const arg = unsafeAssumeValidArg({ selector: "form" });
+
+    const result = await brick.run(arg, {
+      ctxt: null,
+      logger: null,
+      root: null,
+      runPipeline: neverPromise,
+      runRendererPipeline: neverPromise,
+    });
+
+    expect(result).toEqual({
+      foo: "42",
+      bar: "42",
     });
   });
 });
