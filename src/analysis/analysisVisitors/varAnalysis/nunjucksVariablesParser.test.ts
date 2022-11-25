4b0de0142579e2e31fb74956fc206501a3978c1e
balehok <zhmurik@yandex.ru>
2022-11-25 18:17:20 +0100

    nunjucksVariablesParser

---
+++ src/analysis/analysisVisitors/varAnalysis/nunjucksVariablesParser.test.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2022 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import parseTemplateVariables from "./nunjucksVariablesParser";
+
+test("simple template", () => {
+  const result = parseTemplateVariables(
+    "a {{@variableA}} {{ @variableB }} template"
+  );
+
+  expect(result[0]).toBe("@variableA");
+  expect(result[1]).toBe("@variableB");
+});
+
+test("access with .", () => {
+  const result = parseTemplateVariables(
+    "a {{@foo.bar}} {{ @foo.baz}} {{ @foo.bar.baz }} {{ @qux.quux }} template"
+  );
+
+  expect(result).toEqual(["@foo.bar", "@foo.baz", "@foo.bar.baz", "@qux.quux"]);
+});
+
+test("access with []", () => {
+  const template = "Hello {{ @foo['bar baz'] }}";
+  const result = parseTemplateVariables(template);
+  expect(result[0]).toBe('@foo["bar baz"]');
+});
+
+test("indexed access", () => {
+  const template = "Hello {{ @foo[0] }}";
+  const result = parseTemplateVariables(template);
+  expect(result[0]).toBe("@foo.0");
+});
+
+test("works with filters", () => {
+  const template =
+    'A template with a filter {{ @foo | replace("foo", "bar") | capitalize }}.';
+  const result = parseTemplateVariables(template);
+
+  expect(result[0]).toBe("@foo");
+});
+
+test("outside braces", () => {
+  const template = "Hello @foo";
+  const result = parseTemplateVariables(template);
+  expect(result).toHaveLength(0);
+});
+
+test("if tag", () => {
+  const template = `
+    {% if @hungry %}
+      I am hungry
+    {% elif @tired %}
+      I am tired
+    {% else %}
+      I am good!
+    {% endif %}`;
+  const result = parseTemplateVariables(template);
+  expect(result[0]).toBe("@hungry");
+  expect(result[1]).toBe("@tired");
+});
+
+test("no @ prefix", () => {
+  const template = "Hello {{ variable }}";
+  const result = parseTemplateVariables(template);
+  expect(result[0]).toBe("variable");
+});
+
+test("complex conditions", () => {
+  const template = `
+    {% if @happy and @hungry %}
+      I am happy *and* hungry; both are true.
+    {% endif %}`;
+  const result = parseTemplateVariables(template);
+  expect(result[0]).toBe("@happy");
+  expect(result[1]).toBe("@hungry");
+});
+
+test("nunjucks for loop item", () => {
+  const template = `
+    {% for item in @items %}
+      Item value: {{ item }}
+    {% endfor %}`;
+  const result = parseTemplateVariables(template);
+  expect(result).toEqual(["@items"]);
+});
+
+test("nunjucks for loop item 2", () => {
+  const template = `
+    {% for qux in @foo.bar.baz %}
+      Item value: {{ qux.quux.quuux }}
+      Context var: {{ @corge.grault.garply}}
+    {% endfor %}`;
+  const result = parseTemplateVariables(template);
+  expect(result).toEqual(["@foo.bar.baz", "@corge.grault.garply"]);
+});
---
+++ src/analysis/analysisVisitors/varAnalysis/nunjucksVariablesParser.ts
@@ -0,0 +1,313 @@
+/*
+ * Copyright (C) 2022 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// The logic here is heavily based on the nunjucks-variable-parser
+// https://github.com/archersado/nunjucks-variable-parser
+
+import { joinName } from "@/utils";
+import * as nunjucks from "nunjucks";
+// The typings of nunjucks do not expose parser, nodes
+// Have to hack the types
+const { parser, nodes } = nunjucks as any;
+
+const VARIABLE_PARENT_SYMBOL = Symbol("#Variable_parent");
+const VARIABLE_CHILDREN_SYMBOL = Symbol("#Variable_children");
+const VARIABLE_TYPE_SYMBOL = Symbol("#Variable_type");
+
+export interface VariableOptions {
+  type: string;
+  parent: Variable | undefined;
+}
+
+const {
+  LookupVal,
+  Symbol: NodeSymbol,
+  Pair,
+  Compare,
+  Filter,
+  FunCall,
+  For,
+  If,
+  Literal,
+} = nodes;
+
+const expressionTags = [
+  "And",
+  "Or",
+  "Not",
+  "Add",
+  "Sub",
+  "Mul",
+  "Div",
+  "Mod",
+  "Pow",
+  "Neg",
+  "Pos",
+  "FloorDiv",
+];
+
+export class Variable {
+  value: string;
+  [VARIABLE_PARENT_SYMBOL]: Variable | undefined;
+  [VARIABLE_CHILDREN_SYMBOL]: Variable[];
+  [VARIABLE_TYPE_SYMBOL]: string;
+
+  constructor(value: string, options: Partial<VariableOptions> = {}) {
+    const { type = "string", parent } = options;
+
+    this[VARIABLE_PARENT_SYMBOL] = parent;
+    this[VARIABLE_CHILDREN_SYMBOL] = [];
+    this[VARIABLE_TYPE_SYMBOL] = type;
+    this.value = value;
+  }
+
+  get parent() {
+    return this[VARIABLE_PARENT_SYMBOL];
+  }
+
+  set parent(parent) {
+    this[VARIABLE_PARENT_SYMBOL] = parent;
+  }
+
+  get children() {
+    return this[VARIABLE_CHILDREN_SYMBOL];
+  }
+
+  addChild(child: Variable) {
+    this[VARIABLE_CHILDREN_SYMBOL].push(child);
+  }
+
+  get type() {
+    return this[VARIABLE_TYPE_SYMBOL];
+  }
+
+  set type(type) {
+    this[VARIABLE_TYPE_SYMBOL] = type;
+  }
+}
+
+function traverseNode(node: any, inLoop = false) {
+  if (node instanceof NodeSymbol) {
+    return [new Variable(node.value)];
+  }
+
+  if (node instanceof Pair) {
+    return parsePair(node, inLoop);
+  }
+
+  if (node instanceof LookupVal) {
+    return parseLookUp(node, inLoop);
+  }
+
+  if (node instanceof For) {
+    return parseFor(node);
+  }
+
+  if (node instanceof If) {
+    return parseIf(node, inLoop);
+  }
+
+  if (node instanceof FunCall || node instanceof Filter) {
+    return parseFnOrFilter(node, inLoop);
+  }
+
+  if (expressionTags.includes(node.typename)) {
+    return parseExpression(node, inLoop);
+  }
+
+  if (node instanceof Compare) {
+    return parseCompare(node, inLoop);
+  }
+
+  return [];
+}
+
+function traverse(ast: any, inLoop = false): Variable[] {
+  if (!ast.children) {
+    return traverseNode(ast, inLoop);
+  }
+
+  const variables: Variable[] = [];
+  for (const node of ast.children) {
+    variables.push(...traverse(node, inLoop));
+  }
+
+  return variables;
+}
+
+function parseCompare(node: any, inLoop = false): any {
+  if (!(node instanceof Compare)) {
+    throw new TypeError(
+      `Current node type is not Compare, it is ${node.typename}`
+    );
+  }
+
+  const { expr } = node;
+
+  return traverse(expr, inLoop);
+}
+
+function parseFnOrFilter(node: any, inLoop = false) {
+  if (!(node instanceof FunCall) && !(node instanceof Filter)) {
+    throw new TypeError(
+      `Current node type is not FunCall or Filter, it is ${node.typename}`
+    );
+  }
+
+  const { args } = node;
+
+  return traverse(args, inLoop);
+}
+
+function parseExpression(node: any, inLoop = false) {
+  if (!expressionTags.includes(node.typename)) {
+    throw new TypeError(
+      `Current node type is not in Expression, it is ${node.typename}`
+    );
+  }
+
+  const { left, right } = node;
+
+  return [...traverse(left, inLoop), ...traverse(right, inLoop)];
+}
+
+function parseIf(node: any, inLoop = false) {
+  if (!(node instanceof If)) {
+    throw new TypeError(
+      `Current node type is not LookupVal, it is ${node.typename}`
+    );
+  }
+
+  const { body, cond, else_ } = node;
+
+  return [...traverse(body, inLoop), ...traverse(cond, inLoop)].concat(
+    else_ ? traverse(else_, inLoop) : []
+  );
+}
+
+function parseLookUp(node: any, inLoop = false): Variable[] {
+  if (!(node instanceof LookupVal)) {
+    throw new TypeError(
+      `Current node type is not LookupVal, it is ${node.typename}`
+    );
+  }
+
+  const { target, val } = node;
+  if (target.value === "loop") {
+    return [];
+  }
+
+  const targetVars =
+    target instanceof NodeSymbol
+      ? [new Variable(target.value, { type: "object" })]
+      : parseLookUp(target, inLoop);
+
+  if (val instanceof Literal) {
+    const parentVar = targetVars.at(-1);
+    const newVar = new Variable(val.value, { parent: parentVar });
+    parentVar.addChild(newVar);
+    return [...targetVars, newVar];
+  }
+
+  return [...targetVars, ...traverse(val, inLoop)];
+}
+
+function parsePair(node: any, inLoop = false) {
+  if (!(node instanceof Pair)) {
+    throw new TypeError(
+      `Current node type is not Pair, it is ${node.typename}`
+    );
+  }
+
+  const { key, value } = node;
+  return [...traverse(key, inLoop), ...traverse(value, inLoop)];
+}
+
+function parseFor(node: any) {
+  if (!(node instanceof For)) {
+    throw new TypeError(`Current node type is not For, it is ${node.typename}`);
+  }
+
+  const { arr: listNode, name: itemNode, body } = node;
+  const variables: Variable[] = [];
+
+  let listVars: Variable[];
+  if (listNode instanceof NodeSymbol) {
+    const listVar = new Variable(listNode.value, { type: "list" });
+    listVars = [listVar];
+    variables.push(listVar);
+  } else {
+    listVars = traverse(listNode, true);
+    variables.push(...listVars);
+  }
+
+  let itemVars: Variable[];
+  if (itemNode instanceof NodeSymbol) {
+    const itemVar = new Variable(itemNode.value);
+    itemVars = [itemVar];
+  } else {
+    itemVars = traverse(itemNode, true);
+  }
+
+  const bodyVars = traverse(body, true);
+  const bodyVarsFromContext = bodyVars.filter((bodyVar) =>
+    itemVars.every((itemVar) => {
+      if (itemVar.value === bodyVar.value) {
+        return false;
+      }
+
+      let { parent } = itemVar;
+      while (parent) {
+        if (parent.value === bodyVar.value) {
+          return false;
+        }
+
+        parent = parent.parent;
+      }
+
+      return true;
+    })
+  );
+
+  variables.push(...bodyVarsFromContext);
+
+  return variables;
+}
+
+function getVariableName(variable: Variable, path = ""): string {
+  const joinedName = joinName(path, String(variable.value));
+  if (variable.children.length > 0) {
+    return getVariableName(variable.children[0], joinedName);
+  }
+
+  return joinedName;
+}
+
+function parseTemplateVariables(tpl: string): string[] {
+  const ast = parser.parse(tpl, true);
+  const variables = traverse(ast);
+  const vars: string[] = [];
+  for (const x of variables) {
+    if (x[VARIABLE_PARENT_SYMBOL] == null) {
+      vars.push(getVariableName(x));
+    }
+  }
+
+  return vars;
+}
+
+export default parseTemplateVariables;
