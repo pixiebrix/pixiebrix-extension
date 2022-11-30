92789a72bb866f79d872a5237b87f72c428b7744
balehok <zhmurik@yandex.ru>
2022-11-30 21:50:11 +0100

    4690 loading _c olors globaly

--- webpack.config.js
+++ webpack.config.js
@@ -413,6 +413,9 @@ module.exports = (env, options) =>
               options: {
                 // Due to warnings in dart-sass https://github.com/pixiebrix/pixiebrix-extension/pull/1070
                 implementation: require("node-sass"),
+                // The aliases ("@") don't work here
+                additionalData:
+                  '@import "src/vendors/theme/assets/styles/_colors";',
               },
             },
           ],
