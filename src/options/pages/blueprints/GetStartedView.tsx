1edfb67b088c1a759c1d1fe00b5914f3b6d85e59
Misha Holtz <mishmish@android-93d80e96b57764f1.localdomain>
2022-12-21 11:55:02 -0800

    console log onboardingBlueprintId in Get Started view

--- src/options/pages/blueprints/GetStartedView.tsx
+++ src/options/pages/blueprints/GetStartedView.tsx
@@ -22,6 +22,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row } from "react-bootstrap";
 import { isMac } from "@/utils";
+import useMilestones from "@/hooks/useMilestones";
 
 const ExternalLink: React.VoidFunctionComponent<{
   linkText: string;
@@ -42,64 +43,77 @@ const ExternalLink: React.VoidFunctionComponent<{
 const GetStartedView: React.VoidFunctionComponent<{
   width: number;
   height: number;
-}> = ({ width, height }) => (
-  <div
-    style={{ height: `${height}px`, width: `${width}px` }}
-    className={styles.root}
-  >
-    <Row className={styles.infoRow}>
-      <Col>
-        <h4>Want to create a new Blueprint?</h4>
-        <ul>
-          <li>
-            Start by opening a new browser tab navigating to the webpage
-            you&apos;d like to modify.
-          </li>
-          <li>
-            Go to the PixieBrix tab via the <strong>Chrome DevTools</strong>{" "}
-            using{" "}
-            {isMac() ? (
-              <kbd>Cmd + Option + C</kbd>
-            ) : (
-              <kbd>Ctrl + Shift + C</kbd>
-            )}{" "}
-            or <kbd>F12</kbd> and start editing your page.
-          </li>
-          <li>
-            Save your Blueprint in the Page Editor and you&apos;ll see it show
-            up here as a personal Blueprint.
-          </li>
-        </ul>
-      </Col>
-    </Row>
-    <Row className={styles.infoRow}>
-      <Col>
-        <h4>Need more help?</h4>
-        <p>
-          Visit the{" "}
-          <ExternalLink
-            linkText="Quick Start Guide"
-            url="https://docs.pixiebrix.com/quick-start-guide"
-          />{" "}
-          or ask questions in the{" "}
-          <ExternalLink
-            linkText="Slack Community"
-            url="https://pixiebrixcommunity.slack.com/join/shared_invite/zt-13gmwdijb-Q5nVsSx5wRLmRwL3~lsDww#/shared-invite/email"
-          />
-          .
-        </p>
-        <p>
-          {" "}
-          Visit the{" "}
-          <ExternalLink
-            linkText="PixieBrix Marketplace"
-            url="https://www.pixiebrix.com/marketplace/"
-          />{" "}
-          for ideas.
-        </p>
-      </Col>
-    </Row>
-  </div>
-);
+}> = ({ width, height }) => {
+  const { hasEveryMilestone, getMilestone } = useMilestones();
+
+  const onboardingBlueprintId = hasEveryMilestone([
+    "account_setup_via_activate_marketplace_blueprint",
+    "first_time_public_blueprint_install",
+  ])
+    ? getMilestone("first_time_public_blueprint_install")
+    : null;
+
+  console.log("onboardingBlueprintId", onboardingBlueprintId);
+
+  return (
+    <div
+      style={{ height: `${height}px`, width: `${width}px` }}
+      className={styles.root}
+    >
+      <Row className={styles.infoRow}>
+        <Col>
+          <h4>Want to create a new Blueprint?</h4>
+          <ul>
+            <li>
+              Start by opening a new browser tab navigating to the webpage
+              you&apos;d like to modify.
+            </li>
+            <li>
+              Go to the PixieBrix tab via the <strong>Chrome DevTools</strong>{" "}
+              using{" "}
+              {isMac() ? (
+                <kbd>Cmd + Option + C</kbd>
+              ) : (
+                <kbd>Ctrl + Shift + C</kbd>
+              )}{" "}
+              or <kbd>F12</kbd> and start editing your page.
+            </li>
+            <li>
+              Save your Blueprint in the Page Editor and you&apos;ll see it show
+              up here as a personal Blueprint.
+            </li>
+          </ul>
+        </Col>
+      </Row>
+      <Row className={styles.infoRow}>
+        <Col>
+          <h4>Need more help?</h4>
+          <p>
+            Visit the{" "}
+            <ExternalLink
+              linkText="Quick Start Guide"
+              url="https://docs.pixiebrix.com/quick-start-guide"
+            />{" "}
+            or ask questions in the{" "}
+            <ExternalLink
+              linkText="Slack Community"
+              url="https://pixiebrixcommunity.slack.com/join/shared_invite/zt-13gmwdijb-Q5nVsSx5wRLmRwL3~lsDww#/shared-invite/email"
+            />
+            .
+          </p>
+          <p>
+            {" "}
+            Visit the{" "}
+            <ExternalLink
+              linkText="PixieBrix Marketplace"
+              url="https://www.pixiebrix.com/marketplace/"
+            />{" "}
+            for ideas.
+          </p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default GetStartedView;
