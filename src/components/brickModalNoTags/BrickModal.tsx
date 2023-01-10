3af427ad8eaeebf8095752463aa5bf78bd6b5c00
Federico Brigante <me@fregante.com>
2023-01-10 11:59:27 +0800

    Add `useAutoFocus` hook (#4868)

--- src/components/brickModalNoTags/BrickModal.tsx
+++ src/components/brickModalNoTags/BrickModal.tsx
@@ -50,6 +50,7 @@ import { type Except } from "type-fest";
 import cx from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import useAutoFocus from "@/hooks/useAutoFocus";
 
 type BrickOption<T extends IBrick = IBlock> = {
   data: T;
@@ -191,9 +192,7 @@ function ActualModal<T extends IBrick>({
   const brickResultSizePx = 87;
 
   // Auto-focus search input upon opening Modal
-  useEffect(() => {
-    searchInput.current.focus();
-  }, []);
+  useAutoFocus(searchInput);
 
   const { data: listings = {} } = useGetMarketplaceListingsQuery();
 
