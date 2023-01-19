c462a054713152481c4dfc836a0e821d814b6b08
Federico Brigante <me@fregante.com>
2023-01-20 01:51:09 +0800

    Revert "Use `waitForEffect`"
    This reverts commit 2f672edf3758604ead70dab7eb5c320cdc02138c.

--- src/hooks/useExtensionPermissions.test.ts
+++ src/hooks/useExtensionPermissions.test.ts
@@ -15,10 +15,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useExtensionPermissions from "./useExtensionPermissions";
 import { selectAdditionalPermissionsSync } from "webext-additional-permissions";
-import { getChromeEventMocks, waitForEffect } from "@/testUtils/testHelpers";
+import { getChromeEventMocks } from "@/testUtils/testHelpers";
 
 browser.permissions.getAll = jest.fn();
 browser.permissions.onAdded = getChromeEventMocks();
@@ -50,7 +50,7 @@ describe("useExtensionPermissions", () => {
     mockOrigins();
     const { result } = renderHook(useExtensionPermissions);
     expect(result.current).toMatchInlineSnapshot("[]");
-    await waitForEffect();
+    await act(async () => {});
     expect(result.current).toMatchInlineSnapshot(`
       [
         {
@@ -66,7 +66,7 @@ describe("useExtensionPermissions", () => {
   test("includes additional permissions", async () => {
     mockOrigins("https://added.example.com/*", "https://more.example.com/*");
     const { result } = renderHook(useExtensionPermissions);
-    await waitForEffect();
+    await act(async () => {});
     expect(result.current).toMatchInlineSnapshot(`
       [
         {
@@ -94,7 +94,7 @@ describe("useExtensionPermissions", () => {
   test("detects overlapping permissions", async () => {
     mockOrigins("https://added.example.com/*", "https://*.example.com/*");
     const { result } = renderHook(useExtensionPermissions);
-    await waitForEffect();
+    await act(async () => {});
     expect(result.current).toMatchInlineSnapshot(`
       [
         {
