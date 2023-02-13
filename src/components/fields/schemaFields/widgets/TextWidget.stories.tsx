ea611d887be9b60c53ca37e7f4937328723e2f58
Todd Schiller <todd.schiller@gmail.com>
2023-02-13 15:44:22 -0500

    Text widget storybook file

---
+++ src/components/fields/schemaFields/widgets/TextWidget.stories.tsx
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2023 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import { type ComponentMeta, type ComponentStory } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import TextWidget from "@/components/fields/schemaFields/widgets/TextWidget";
+import { settingsStore } from "@/testUtils/storyUtils";
+import { Provider } from "react-redux";
+import { Formik } from "formik";
+
+export default {
+  title: "Widgets/TextWidget",
+  component: TextWidget,
+  args: {
+    value: "",
+  },
+} as ComponentMeta<typeof TextWidget>;
+
+const Template: ComponentStory<typeof TextWidget> = ({ value }) => (
+  <Provider store={settingsStore()}>
+    <Formik
+      initialValues={{
+        apiVersion: "v3",
+        example: value,
+      }}
+      onSubmit={action("onSubmit")}
+    >
+      <TextWidget
+        name="example"
+        onChange={action("onChange")}
+        schema={{
+          type: "string",
+        }}
+      />
+    </Formik>
+  </Provider>
+);
+
+export const Empty = Template.bind({});
+Empty.args = {};
+
+export const SingleLine = Template.bind({});
+SingleLine.args = {
+  value: "The quick brown fox jumps over the lazy dog",
+};
+
+export const Longline = Template.bind({});
+Longline.args = {
+  value:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, magna vel viverra rutrum, mi nisi venenatis arcu, at tincidunt orci sapien a ante. Donec ac massa a urna dictum mollis. Ut feugiat accumsan ipsum eget vehicula. Sed ultricies, lorem sit amet aliquam lobortis, sem erat dictum elit, laoreet rhoncus nulla felis id purus. Etiam consequat tincidunt ipsum vitae pulvinar. Nam at turpis elementum, dignissim nulla ut, eleifend est. Nullam rutrum justo quis sapien semper pretium.",
+};
+
+export const NunjucksExpression = Template.bind({});
+NunjucksExpression.args = {
+  value: "Hello, {{ @input.name }}!",
+};
+
+export const NunjucksTags = Template.bind({});
+NunjucksTags.args = {
+  value:
+    "My favorite color is {% if @input.day == 'Monday' %}red{% else %}blue{% endif %}",
+};
