9f1262a53a062a00e70f7478660416ea534ade26
Todd Schiller <todd.schiller@gmail.com>
2023-02-13 07:50:55 -0500

    #5172: isolate quickbar from host page hot-keys (#5173)

--- src/components/quickBar/QuickBarApp.tsx
+++ src/components/quickBar/QuickBarApp.tsx
@@ -39,6 +39,8 @@ import QuickBarResults from "./QuickBarResults";
 import useActionGenerators from "@/components/quickBar/useActionGenerators";
 import useActions from "@/components/quickBar/useActions";
 
+import FocusLock from "react-focus-lock";
+
 /**
  * Set to true if the KBar should be displayed on initial mount (i.e., because it was triggered by the
  * shortcut giving the page activeTab).
@@ -89,16 +91,34 @@ const KBarComponent: React.FC = () => {
     selection.restore();
   }
 
+  // We're using the Shadow DOM to isolate the style. However, that also means keydown events look like they're
+  // coming from the div instead of the search input.
+  //
+  // - Include div.contentEditable to indicate to hotkey libraries that the user is interacting with the quick bar
+  // - Salesforce looks for the `cke_editable` instead of contentEditable, so provide that class
+  //
+  // Library references:
+  // - hotkey: https://github.com/github/hotkey/blob/main/src/utils.ts#L1
+  // - Salesforce: https://salesforce.stackexchange.com/questions/183771/disable-keyboard-shortcuts-in-lightning-experience
+
   return (
     <KBarPortal>
       <KBarPositioner style={{ zIndex: MAX_Z_INDEX }}>
         <KBarAnimator style={animatorStyle}>
-          <ReactShadowRoot mode="open">
-            <Stylesheets href={faStyleSheet} mountOnLoad>
-              <KBarSearch style={searchStyle} />
-              <QuickBarResults />
-            </Stylesheets>
-          </ReactShadowRoot>
+          <div
+            className="cke_editable"
+            contentEditable
+            suppressContentEditableWarning
+          >
+            <ReactShadowRoot mode="closed">
+              <Stylesheets href={faStyleSheet} mountOnLoad>
+                <FocusLock>
+                  <KBarSearch style={searchStyle} />
+                  <QuickBarResults />
+                </FocusLock>
+              </Stylesheets>
+            </ReactShadowRoot>
+          </div>
         </KBarAnimator>
       </KBarPositioner>
     </KBarPortal>
