fb4c5b1a60f7b1cf6cccaf35155ebd5b27702971
Ben Loe <below413@gmail.com>
2023-02-18 11:11:25 -0500

    Marketplace UI followup - permissions & quick bar hotkey info (#5211)

---
+++ src/utils/includesQuickBarExtensionPoint.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { type ResolvedExtensionPointConfig } from "@/types/definitions";
+import extensionPointRegistry from "@/extensionPoints/registry";
+import { QuickBarExtensionPoint } from "@/extensionPoints/quickBarExtension";
+import { QuickBarProviderExtensionPoint } from "@/extensionPoints/quickBarProviderExtension";
+
+export default async function includesQuickBarExtensionPoint(
+  extensionPointConfigs?: ResolvedExtensionPointConfig[]
+): Promise<boolean> {
+  if (!extensionPointConfigs) {
+    return;
+  }
+
+  for (const { id } of extensionPointConfigs) {
+    // eslint-disable-next-line no-await-in-loop -- can break when we find one
+    const extensionPoint = await extensionPointRegistry.lookup(id);
+    if (
+      QuickBarExtensionPoint.isQuickBarExtensionPoint(extensionPoint) ||
+      QuickBarProviderExtensionPoint.isQuickBarProviderExtensionPoint(
+        extensionPoint
+      )
+    ) {
+      return true;
+    }
+  }
+}
