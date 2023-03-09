82fb79e38876640aa6e7c290655b78de0b6830a0
Todd Schiller <todd.schiller@gmail.com>
2023-03-09 14:51:46 -0600

    Update useSpreadsheetId tests

--- src/contrib/google/sheets/useSpreadsheetId.test.ts
+++ src/contrib/google/sheets/useSpreadsheetId.test.ts
@@ -21,20 +21,36 @@ import {
   sanitizedServiceConfigurationFactory,
   uuidSequence,
 } from "@/testUtils/factories";
-import { type RegistryId } from "@/core";
 import { waitForEffect } from "@/testUtils/testHelpers";
 import useDependency, { type Dependency } from "@/services/useDependency";
+import { validateRegistryId } from "@/types/helpers";
+import { services } from "@/background/messenger/api";
+import { type SanitizedServiceConfiguration } from "@/core";
 
 const TEST_SPREADSHEET_ID = uuidSequence(1);
-const GOOGLE_SHEET_SERVICE_ID = "google/sheet" as RegistryId;
+const GOOGLE_SHEET_SERVICE_ID = validateRegistryId("google/sheet");
 
-jest.mock("@/services/useDependency", () => ({
-  __esModule: true,
-  default: jest.fn(),
+jest.mock("@/background/messenger/api", () => ({
+  services: {
+    locate: jest.fn(),
+  },
 }));
+
+jest.mock("@/services/useDependency", () => {
+  const actual = jest.requireActual("@/services/useDependency");
+  return {
+    __esModule: true,
+    ...actual,
+    default: jest.fn(),
+  };
+});
 
 const useDependencyMock = useDependency as jest.MockedFunction<
   typeof useDependency
+>;
+
+const serviceLocatorMock = services.locate as jest.MockedFunction<
+  typeof services.locate
 >;
 
 describe("useSpreadsheetId", () => {
@@ -57,15 +73,25 @@ describe("useSpreadsheetId", () => {
         spreadsheetId: TEST_SPREADSHEET_ID,
       },
     });
-    expect(result.current).toBe(TEST_SPREADSHEET_ID);
+    expect(result.current).toEqual({
+      spreadsheetId: TEST_SPREADSHEET_ID,
+      error: null,
+    });
   });
 
   test("works with service value", async () => {
+    serviceLocatorMock.mockResolvedValue({
+      id: GOOGLE_SHEET_SERVICE_ID,
+      config: {
+        spreadsheetId: TEST_SPREADSHEET_ID,
+      },
+    } as unknown as SanitizedServiceConfiguration);
+
     const { result } = renderHook(() => useSpreadsheetId(""), {
       initialValues: {
         spreadsheetId: {
           __type__: "var",
-          __value__: GOOGLE_SHEET_SERVICE_ID,
+          __value__: "@google",
         },
         services: [
           {
@@ -79,6 +105,36 @@ describe("useSpreadsheetId", () => {
 
     await waitForEffect();
 
-    expect(result.current).toBe(TEST_SPREADSHEET_ID);
+    expect(result.current).toEqual({
+      error: null,
+      spreadsheetId: TEST_SPREADSHEET_ID,
+    });
+  });
+
+  test("handles error", async () => {
+    serviceLocatorMock.mockRejectedValue(new Error("Test Error"));
+
+    const { result } = renderHook(() => useSpreadsheetId(""), {
+      initialValues: {
+        spreadsheetId: {
+          __type__: "var",
+          __value__: "@google",
+        },
+        services: [
+          {
+            id: GOOGLE_SHEET_SERVICE_ID,
+            outputKey: "google",
+            config: uuidSequence(2),
+          },
+        ],
+      },
+    });
+
+    await waitForEffect();
+
+    expect(result.current).toEqual({
+      error: expect.not.toBeNil(),
+      spreadsheetId: null,
+    });
   });
 });
