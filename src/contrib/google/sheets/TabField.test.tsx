9954fb480ece5fae82fb70d51f81257c27d47efb
Todd Schiller <todd.schiller@gmail.com>
2023-03-09 20:21:42 -0600

    Add error handling test

--- src/contrib/google/sheets/TabField.test.tsx
+++ src/contrib/google/sheets/TabField.test.tsx
@@ -21,14 +21,18 @@ import { render } from "@/pageEditor/testHelpers";
 import TabField from "@/contrib/google/sheets/TabField";
 import { waitForEffect } from "@/testUtils/testHelpers";
 import registerDefaultWidgets from "@/components/fields/schemaFields/widgets/registerDefaultWidgets";
-import { sheets } from "@/background/messenger/api";
 import { makeTemplateExpression } from "@/runtime/expressionCreators";
+import { sheets } from "@/background/messenger/api";
 
 jest.mock("@/background/messenger/api", () => ({
   sheets: {
     getTabNames: jest.fn().mockResolvedValue(["Tab1", "Tab2"]),
   },
 }));
+
+const getTabNamesMock = sheets.getTabNames as jest.MockedFunction<
+  typeof sheets.getTabNames
+>;
 
 beforeAll(() => {
   registerDefaultWidgets();
@@ -94,5 +98,30 @@ describe("TabField", () => {
     // Should have defaulted
     expect(rendered.queryByText("Select...")).not.toBeInTheDocument();
     expect(rendered.queryByText("Tab1")).toBeInTheDocument();
+  });
+
+  it("shows error message", async () => {
+    getTabNamesMock.mockRejectedValue(new Error("Test error"));
+
+    const rendered = render(
+      <TabField
+        name="tabName"
+        schema={{}} // Does not currently check the passed-in schema
+        spreadsheetId={"testId"}
+      />,
+      {
+        initialValues: {
+          tabName: makeTemplateExpression("nunjucks", ""),
+        },
+      }
+    );
+
+    await waitForEffect();
+
+    // Should have defaulted
+    expect(rendered.queryByText("Select...")).not.toBeInTheDocument();
+
+    // XXX: can this do partial matches? In any case, it's not in the document
+    expect(rendered.queryByText("Error loading tab names")).toBeInTheDocument();
   });
 });
