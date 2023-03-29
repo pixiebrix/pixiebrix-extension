7978af48ce460d81e3e3ec3a299ac540eea0680a
Ben Loe <ben@pixiebrix.com>
2023-03-29 17:52:13 -0400

    remove unused services when sheet picker loads

--- src/contrib/google/sheets/SheetsFileWidget.test.tsx
+++ src/contrib/google/sheets/SheetsFileWidget.test.tsx
@@ -22,6 +22,14 @@ import { BASE_SHEET_SCHEMA } from "@/contrib/google/sheets/schemas";
 import { waitForEffect } from "@/testUtils/testHelpers";
 import { sheets } from "@/background/messenger/api";
 import { makeVariableExpression } from "@/runtime/expressionCreators";
+import {
+  blockConfigFactory,
+  extensionFactory,
+  formStateFactory,
+  uuidSequence,
+} from "@/testUtils/factories";
+import { validateRegistryId } from "@/types/helpers";
+import { OutputKey } from "@/core";
 
 jest.mock("@/background/messenger/api", () => ({
   sheets: {
@@ -98,5 +106,76 @@ describe("SheetsFileWidget", () => {
     expect(wrapper.container.querySelector("input")).toHaveValue(
       "Use Workshop to edit"
     );
+  });
+
+  it("removes unused service on mount", async () => {
+    const initialValues = formStateFactory(
+      {
+        services: [
+          {
+            id: validateRegistryId("google/sheet"),
+            outputKey: "google" as OutputKey,
+            config: uuidSequence(1),
+          },
+        ],
+      },
+      [
+        blockConfigFactory({
+          config: {
+            spreadsheetId: "abc123",
+          },
+        }),
+      ]
+    );
+
+    const { getFormState } = render(
+      <SheetsFileWidget
+        name="extension.blockPipeline[0].config.spreadsheetId"
+        schema={BASE_SHEET_SCHEMA}
+      />,
+      { initialValues }
+    );
+
+    await waitForEffect();
+
+    const formState = await getFormState();
+
+    expect(formState.services).toHaveLength(0);
+  });
+
+  it("does not remove used service on mount", async () => {
+    const service = {
+      id: validateRegistryId("google/sheet"),
+      outputKey: "google" as OutputKey,
+      config: uuidSequence(1),
+    };
+
+    const initialValues = formStateFactory(
+      {
+        services: [service],
+      },
+      [
+        blockConfigFactory({
+          config: {
+            spreadsheetId: makeVariableExpression("@google"),
+          },
+        }),
+      ]
+    );
+
+    const { getFormState } = render(
+      <SheetsFileWidget
+        name="extension.blockPipeline[0].config.spreadsheetId"
+        schema={BASE_SHEET_SCHEMA}
+      />,
+      { initialValues }
+    );
+
+    await waitForEffect();
+
+    const formState = await getFormState();
+
+    expect(formState.services).toHaveLength(1);
+    expect(formState.services[0]).toEqual(service);
   });
 });
