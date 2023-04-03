56185b54a5ca45418d212855765ced79afb32b84
Daniel Soetanto <turbochef.ds@gmail.com>
2023-04-03 18:48:09 -0500

    clear services on remove

--- src/pageEditor/slices/editorSlice.ts
+++ src/pageEditor/slices/editorSlice.ts
@@ -93,6 +93,10 @@ import { serializeError } from "serialize-error";
 import { isExtension } from "@/pageEditor/sidebar/common";
 import { type StorageInterface } from "@/store/StorageInterface";
 import { localStorage } from "redux-persist-webextension-storage";
+import {
+  keyToFieldValue,
+  selectServiceVariables,
+} from "@/components/fields/schemaFields/serviceFieldUtils";
 
 export const initialState: EditorState = {
   selectionSeq: 0,
@@ -732,6 +736,7 @@ export const editorSlice = createSlice({
         pipelineIndex: number;
       }>
     ) {
+      console.log({ testaction: action });
       const { block, pipelinePath, pipelineIndex } = action.payload;
 
       const element = state.elements.find(
@@ -808,6 +813,12 @@ export const editorSlice = createSlice({
           ? pipeline[index - 1] // Last item, select previous
           : pipeline[index + 1]; // Not last item, select next
       pipeline.splice(index, 1);
+
+      const used = selectServiceVariables(element);
+
+      element.services = element.services.filter((x) =>
+        used.has(keyToFieldValue(x.outputKey).__value__)
+      );
 
       syncElementNodeUIStates(state, element);
 
