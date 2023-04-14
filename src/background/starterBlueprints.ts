e26e00a374afd1214dfccc59725ededc866f15d1
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-04-14 13:09:37 -0700

    Enable starter mod installation with built-in integration configurations (#5529)
    * refactor decideRemoteLabel by extraction
    
    * add getBuiltInServiceAuths
    
    * add factories
    
    * add getRequiredServiceIds to recipeUtils
    
    * add return type to getRequiredServiceIds
    
    * pass required service auths to installblueprints
    
    * add tests to starterblueprints tests
    
    * refactor tests & fix type errors
    
    * add ts-expect-error to serviceauthfactory"
    
    * add null client check & error
    
    * use lodash uniq instead of Set
    
    * rename decideRemoteLabel -> getRemoteLabel
    
    * fix config type on SanitizedAuth
    
    * fix SanitizedAuth type in contract.ts
    
    * refactor serviceConfigruationfactory
    
    * update tests with new types
    
    * remove unused comment
    
    * remove new error from no client'
    
    ---------
    
    Co-authored-by: Misha Holtz <mishmish@Mishas-MacBook-Pro.local>

--- src/background/starterBlueprints.ts
+++ src/background/starterBlueprints.ts
@@ -23,9 +23,14 @@ import { forEachTab } from "@/background/activeTab";
 import { queueReactivateTab } from "@/contentScript/messenger/api";
 import { type ExtensionOptionsState } from "@/store/extensionsTypes";
 import reportError from "@/telemetry/reportError";
-import { debounce } from "lodash";
+import { debounce, uniq } from "lodash";
 import { refreshRegistries } from "./refreshRegistries";
 import { memoizeUntilSettled } from "@/utils";
+import { type SanitizedAuth } from "@/types/contract";
+import { getSharingType } from "@/hooks/auth";
+import { getRequiredServiceIds } from "@/utils/recipeUtils";
+import { type RegistryId } from "@/types/registryTypes";
+import { type UUID } from "@/types/stringTypes";
 
 const { reducer, actions } = extensionsSlice;
 
@@ -33,15 +38,66 @@ const PLAYGROUND_URL = "https://www.pixiebrix.com/playground";
 const BLUEPRINT_INSTALLATION_DEBOUNCE_MS = 10_000;
 const BLUEPRINT_INSTALLATION_MAX_MS = 60_000;
 
+export async function getBuiltInServiceAuths(): Promise<SanitizedAuth[]> {
+  const client = await maybeGetLinkedApiClient();
+  if (client == null) {
+    return [];
+  }
+
+  try {
+    const { data: serviceAuths } = await client.get<SanitizedAuth[]>(
+      "/api/services/shared/?meta=1"
+    );
+
+    return serviceAuths.filter((auth) => getSharingType(auth) === "built-in");
+  } catch (error) {
+    reportError(error);
+    return [];
+  }
+}
+
+export function getAllRequiredServiceIds(
+  blueprints: RecipeDefinition[]
+): RegistryId[] {
+  const requiredServiceIds = blueprints.flatMap((blueprint) =>
+    getRequiredServiceIds(blueprint)
+  );
+  return uniq(requiredServiceIds);
+}
+
+export async function getBuiltInAuthsByRequiredServiceIds(
+  serviceIds: RegistryId[]
+): Promise<Record<RegistryId, UUID>> {
+  const builtInServiceAuths = await getBuiltInServiceAuths();
+
+  return Object.fromEntries(
+    serviceIds.map((serviceId) => {
+      const builtInAuth = builtInServiceAuths.find(
+        (auth) => auth.service.config.metadata.id === serviceId
+      );
+
+      if (!builtInAuth) {
+        throw new Error(
+          `No built-in auth found for service ${serviceId}. Check that starter mods have built-in configuration options for all required services.`
+        );
+      }
+
+      return [serviceId, builtInAuth.id];
+    })
+  );
+}
+
 function installBlueprint(
   state: ExtensionOptionsState,
-  blueprint: RecipeDefinition
+  blueprint: RecipeDefinition,
+  services: Record<RegistryId, UUID>
 ): ExtensionOptionsState {
   return reducer(
     state,
     actions.installRecipe({
       recipe: blueprint,
       extensionPoints: blueprint.extensionPoints,
+      services,
     })
   );
 }
@@ -54,6 +110,11 @@ async function installBlueprints(
     return installed;
   }
 
+  const requiredServiceIds = getAllRequiredServiceIds(blueprints);
+  const builtInServiceAuths = await getBuiltInAuthsByRequiredServiceIds(
+    requiredServiceIds
+  );
+
   let extensionsState = await loadOptions();
   for (const blueprint of blueprints) {
     const blueprintAlreadyInstalled = extensionsState.extensions.some(
@@ -61,7 +122,11 @@ async function installBlueprints(
     );
 
     if (!blueprintAlreadyInstalled) {
-      extensionsState = installBlueprint(extensionsState, blueprint);
+      extensionsState = installBlueprint(
+        extensionsState,
+        blueprint,
+        builtInServiceAuths
+      );
       installed = true;
     }
   }
