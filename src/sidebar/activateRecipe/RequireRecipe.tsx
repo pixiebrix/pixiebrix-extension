b525e9cd68e34337d94d237d2e7ae81e6e9c0afd
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-04-28 12:06:00 -0700

    #5627 Fix one-click activation with built-in services (#5628)
    * add a useEffect for initialValues
    
    * remove console logs
    
    * pass defaultAuthOptions down to ActivateRecipePanel and useWizard
    
    * remove console logs
    
    * fix type errors
    
    ---------
    
    Co-authored-by: Misha Holtz <mishmish@Mishas-MacBook-Pro.local>

--- src/sidebar/activateRecipe/RequireRecipe.tsx
+++ src/sidebar/activateRecipe/RequireRecipe.tsx
@@ -31,6 +31,7 @@ import { containsPermissions } from "@/background/messenger/api";
 import { getDefaultAuthOptionsForRecipe, useAuthOptions } from "@/hooks/auth";
 import { isEmpty, uniq } from "lodash";
 import { PIXIEBRIX_SERVICE_ID } from "@/services/constants";
+import { type AuthOption } from "@/auth/authTypes";
 
 export type RecipeState = {
   recipe: RecipeDefinition;
@@ -38,6 +39,7 @@ export type RecipeState = {
   includesQuickBar: boolean;
   needsPermissions: (formValues: WizardValues) => Promise<boolean>;
   canAutoActivate: boolean;
+  defaultAuthOptions: Record<RegistryId, AuthOption>;
 };
 
 type RequireRecipeProps = {
@@ -184,6 +186,7 @@ const RequireRecipe: React.FC<RequireRecipeProps> = ({
     includesQuickBar: quickBarAndPermissions?.includesQuickBar,
     needsPermissions: quickBarAndPermissions?.needsPermissions,
     canAutoActivate,
+    defaultAuthOptions,
   };
 
   return children(recipeState);
