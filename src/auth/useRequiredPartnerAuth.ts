cb420b349224583a2992d1d8a0fec76bee3e7dcb
Ben Loe <ben@pixiebrix.com>
2023-05-24 19:35:33 -0400

    revert this change for now, more work needed here

--- src/auth/useRequiredPartnerAuth.ts
+++ src/auth/useRequiredPartnerAuth.ts
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { useGetMeQuery } from "@/services/api";
+import { appApi } from "@/services/api";
 import { useSelector } from "react-redux";
 import { selectAuth } from "@/auth/authSelectors";
 import { selectConfiguredServices } from "@/store/servicesSelectors";
@@ -116,14 +116,7 @@ function decidePartnerServiceIds({
  * - Integration required, using partner JWT for authentication
  */
 function useRequiredPartnerAuth(): RequiredPartnerState {
-  const {
-    data: me,
-    isLoading,
-    error,
-  } = useGetMeQuery(undefined, {
-    // Prefer the most recent /api/me/ data from the server
-    refetchOnMountOrArgChange: true,
-  });
+  const { isLoading, data: me, error } = appApi.endpoints.getMe.useQueryState();
   const localAuth = useSelector(selectAuth);
   const {
     authServiceId: authServiceIdOverride,
