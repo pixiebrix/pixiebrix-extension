eb4fb67b8d83bb89bf07838c0a86244cafe1e898
Todd Schiller <todd.schiller@gmail.com>
2023-06-02 12:24:33 -0400

    #5835: add support for a `Run` brick to enable custom-defined control flow/brick decorators (#5836)

---
+++ src/blocks/transformers/controlFlow/Run.test.ts
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2023 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import blockRegistry from "@/blocks/registry";
+import {
+  echoBlock,
+  simpleInput,
+  testOptions,
+  throwBlock,
+} from "@/runtime/pipelineTests/pipelineTestHelpers";
+import { reducePipeline } from "@/runtime/reducePipeline";
+import { makePipelineExpression } from "@/runtime/expressionCreators";
+import Run from "@/blocks/transformers/controlFlow/Run";
+import { Block } from "@/types/blockTypes";
+import { validateRegistryId } from "@/types/helpers";
+import { propertiesToSchema } from "@/validators/generic";
+import { type BlockArgs } from "@/types/runtimeTypes";
+import pDefer from "p-defer";
+
+const runBlock = new Run();
+
+class DeferredEchoBlock extends Block {
+  static BLOCK_ID = validateRegistryId("test/deferred");
+  readonly promise: Promise<unknown>;
+  constructor(promise: Promise<unknown>) {
+    super(DeferredEchoBlock.BLOCK_ID, "Deferred Block");
+    this.promise = promise;
+  }
+
+  inputSchema = propertiesToSchema({
+    message: {
+      type: "string",
+    },
+  });
+
+  async run({ message }: BlockArgs) {
+    await this.promise;
+    return { message };
+  }
+}
+
+beforeEach(() => {
+  blockRegistry.clear();
+  blockRegistry.register([throwBlock, echoBlock, runBlock]);
+});
+
+describe("Run", () => {
+  test("throws error body fails", async () => {
+    const pipeline = {
+      id: runBlock.id,
+      config: {
+        body: makePipelineExpression([
+          {
+            id: throwBlock.id,
+            config: {
+              message: "This is an error message!",
+            },
+          },
+        ]),
+      },
+    };
+
+    return expect(
+      reducePipeline(pipeline, simpleInput({}), testOptions("v3"))
+    ).rejects.toThrow();
+  });
+
+  test("returns result on success", async () => {
+    const pipeline = {
+      id: runBlock.id,
+      config: {
+        body: makePipelineExpression([
+          {
+            id: echoBlock.id,
+            config: {
+              message: "Hello, world!",
+            },
+          },
+        ]),
+      },
+    };
+
+    const result = await reducePipeline(
+      pipeline,
+      simpleInput({}),
+      testOptions("v3")
+    );
+
+    expect(result).toStrictEqual({
+      message: "Hello, world!",
+    });
+  });
+
+  test("async returns immediately", async () => {
+    const deferred = pDefer();
+    const asyncBlock = new DeferredEchoBlock(deferred.promise);
+
+    blockRegistry.register([asyncBlock]);
+    const pipeline = {
+      id: runBlock.id,
+      config: {
+        async: true,
+        body: makePipelineExpression([
+          {
+            id: asyncBlock.id,
+            config: {
+              message: "Hello, world!",
+            },
+          },
+        ]),
+      },
+    };
+
+    const result = await reducePipeline(
+      pipeline,
+      simpleInput({}),
+      testOptions("v3")
+    );
+
+    expect(result).toStrictEqual({});
+  });
+});
--- src/components/fields/schemaFields/schemaFieldUtils.ts
+++ src/components/fields/schemaFields/schemaFieldUtils.ts
@@ -18,6 +18,14 @@
 import type React from "react";
 import { fieldLabel } from "@/components/fields/fieldUtils";
 import { type SchemaFieldProps } from "@/components/fields/schemaFields/propTypes";
+import { type Schema, type UiSchema } from "@/types/schemaTypes";
+import { inputProperties } from "@/helpers";
+import {
+  isDatabaseField,
+  isServiceField,
+} from "@/components/fields/schemaFields/fieldTypeCheckers";
+import pipelineSchema from "@schemas/pipeline.json";
+import { isEmpty, sortBy } from "lodash";
 
 export function makeLabelForSchemaField({
   name,
@@ -30,4 +38,89 @@ export function makeLabelForSchemaField({
   }
 
   return label ?? title ?? fieldLabel(name);
+}
+
+type FieldConfig = {
+  prop: string;
+  isRequired: boolean;
+  fieldSchema: Schema;
+  propUiSchema: unknown;
+};
+
+/**
+ * Return an array of fields
+ * @param schema the inputSchema, e.g., IBlock.inputSchema
+ * @param uiSchema an optional uiSchema
+ * @param preserveSchemaOrder if true, use the inputSchema order if no explicit uiSchema is specified. Otherwise, apply
+ * a default sorting based on type and optional fields
+ * @param includePipelines if true, include pipeline fields in the output. Otherwise, exclude them
+ */
+export function sortedFields(
+  schema: Schema,
+  uiSchema: UiSchema | null,
+  {
+    preserveSchemaOrder = false,
+    includePipelines = false,
+  }: { preserveSchemaOrder?: boolean; includePipelines?: boolean } = {}
+): FieldConfig[] {
+  const optionSchema = inputProperties(schema);
+
+  const order = uiSchema?.["ui:order"] ?? ["*"];
+  const asteriskIndex = order.indexOf("*");
+
+  const uiSchemaOrder = (field: FieldConfig) => {
+    // https://react-jsonschema-form.readthedocs.io/en/docs/usage/objects/#specifying-property-order
+    const propIndex = order.indexOf(field.prop);
+    const index = propIndex === -1 ? asteriskIndex : propIndex;
+    return index === -1 ? order.length : index;
+  };
+
+  const fieldTypeOrder = (field: FieldConfig) => {
+    // Integration configurations
+    if (isServiceField(field.fieldSchema)) {
+      return 0;
+    }
+
+    // Databases
+    if (isDatabaseField(field.fieldSchema)) {
+      return 1;
+    }
+
+    // Required fields, and fields that will have input values pre-filled
+    if (field.isRequired || field.fieldSchema.default != null) {
+      return 2;
+    }
+
+    // Optional fields that are excluded by default
+    return Number.MAX_SAFE_INTEGER;
+  };
+
+  // Order by label
+  const labelOrder = (field: FieldConfig) =>
+    (field.fieldSchema.title ?? field.prop).toLowerCase();
+
+  const fields: FieldConfig[] = Object.entries(optionSchema)
+    .filter(
+      ([, fieldSchema]) =>
+        includePipelines ||
+        (typeof fieldSchema === "object" &&
+          fieldSchema.$ref !== pipelineSchema.$id)
+    )
+    .map(([prop, fieldSchema]) => {
+      // Fine because coming from Object.entries for the schema
+      // eslint-disable-next-line security/detect-object-injection
+      const propUiSchema = uiSchema?.[prop];
+
+      return {
+        prop,
+        isRequired: schema.required?.includes(prop),
+        // The fieldSchema type has been filtered so its safe to assume it is Schema
+        fieldSchema: fieldSchema as Schema,
+        propUiSchema,
+      };
+    });
+
+  return preserveSchemaOrder && isEmpty(uiSchema)
+    ? fields
+    : sortBy(fields, uiSchemaOrder, fieldTypeOrder, labelOrder);
 }
