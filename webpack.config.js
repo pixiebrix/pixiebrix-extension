b78c98dbd1c61af85ceae89ae9a30dcc399bb046
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-06-08 10:50:51 -0700

    swapout marketplace file with empty 'justMarketplace' file

--- webpack.config.js
+++ webpack.config.js
@@ -282,7 +282,7 @@ module.exports = (env, options) =>
       [
         "background/background",
         "contentScript/contentScript",
-        "contentScript/marketplace",
+        "contentScript/justMarketplace",
         "contentScript/browserActionInstantHandler",
         "pageEditor/pageEditor",
         "extensionConsole/options",
