cf5025d42381f4edaf73a661cc63d16e1160253e
Daniel Soetanto <turbochef@users.noreply.github.com>
2023-06-12 12:45:25 -0500

    #5859: add Floating Action Button behind feature flag (#5869)
    * add floating action button setting
    
    * add quickbar button, add settings hook
    
    * add comments, add bare version of logo for buttons, remove console.log
    
    * comment and add zindex
    
    * more styles and refactor
    
    * refactor, separate floating actions into its own init
    
    * comments
    
    * iterate
    
    * dead code
    
    * add feature flag
    
    * move flag
    
    * add comment
    
    * mini refactor
    
    * move setting to contentscript instead of action component
    
    * iterate
    
    * alt text change

---
+++ src/components/floatingActions/QuickbarButton.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import logoUrl from "@/icons/custom-icons/logo.svg";
+import { Button } from "react-bootstrap";
+import { toggleQuickBar } from "@/components/quickBar/QuickBarApp";
+
+/**
+ * Opens the quickbar menu
+ */
+export function QuickbarButton() {
+  return (
+    // Using standard css here because the shadow dom in `FloatingActions.tsx`
+    // prevents us from using regular css modules.
+    <Button className="button" onClick={toggleQuickBar}>
+      {/* <img> tag since we're using a different svg than the <Logo> component and it overrides all the styles
+              anyway */}
+      <img src={logoUrl} className="logo" alt="open the PixieBrix quick bar" />
+    </Button>
+  );
+}
