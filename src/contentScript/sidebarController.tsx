5490011914829b0abf79ffd84583dfd900cf7cd8
Todd Schiller <todd.schiller@gmail.com>
2023-06-14 11:04:56 -0400

    #5906: side panel lifecycle logging and pre/post-condition checks (#5907)

--- src/contentScript/sidebarController.tsx
+++ src/contentScript/sidebarController.tsx
@@ -85,6 +85,12 @@ export async function showSidebar(
 
     // Currently, this runs the listening SidebarExtensionPoint.run callbacks in not particular order. Also note that
     // we're not awaiting their resolution (because they may contain long-running bricks).
+    if (!isSidebarFrameVisible()) {
+      console.error(
+        "Pre-condition failed: sidebar is not attached in the page for call to sidebarShowEvents.emit"
+      );
+    }
+
     sidebarShowEvents.emit({ reason: RunReason.MANUAL });
   }
 
