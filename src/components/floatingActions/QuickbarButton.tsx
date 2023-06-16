dd7c4719c84bf3473d87707b85e3fa01ed77000c
Daniel Soetanto <turbochef.ds@gmail.com>
2023-06-16 11:58:12 -0500

    add hide button, update fab styles

--- src/components/floatingActions/QuickbarButton.tsx
+++ src/components/floatingActions/QuickbarButton.tsx
@@ -15,29 +15,62 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import logoUrl from "@/icons/custom-icons/logo.svg";
 import { Button } from "react-bootstrap";
 import { toggleQuickBar } from "@/components/quickBar/QuickBarApp";
 import { reportEvent } from "@/telemetry/events";
+import AsyncButton from "@/components/AsyncButton";
+import { getSettingsState, saveSettingsState } from "@/store/settingsStorage";
 
 /**
  * Opens the quickbar menu
  */
 export function QuickbarButton() {
-  return (
+  // Using this boolean to hide the FAB since the setting state doesn't refresh immediately
+  const [hidden, setHidden] = useState<boolean>(false);
+
+  return hidden ? null : (
     // Using standard css here because the shadow dom in `FloatingActions.tsx`
     // prevents us from using regular css modules.
-    <Button
-      className="button"
-      onClick={() => {
-        reportEvent("FloatingQuickBarButtonClick");
-        toggleQuickBar();
-      }}
-    >
-      {/* <img> tag since we're using a different svg than the <Logo> component and it overrides all the styles
+    <div className="quickbar-button-container">
+      <div className="hide-button-container">
+        <AsyncButton
+          className="hide-button"
+          onClick={async () => {
+            try {
+              setHidden(true);
+              const settings = await getSettingsState();
+              await saveSettingsState({
+                ...settings,
+                isFloatingActionButtonEnabled: false,
+              });
+            } catch {
+              setHidden(false);
+            }
+          }}
+          variant="outline"
+        >
+          Hide Button
+        </AsyncButton>
+      </div>
+      <div>
+        <Button
+          className="quickbar-button"
+          onClick={() => {
+            reportEvent("FloatingQuickBarButtonClick");
+            toggleQuickBar();
+          }}
+        >
+          {/* <img> tag since we're using a different svg than the <Logo> component and it overrides all the styles
               anyway */}
-      <img src={logoUrl} className="logo" alt="open the PixieBrix quick bar" />
-    </Button>
+          <img
+            src={logoUrl}
+            className="logo"
+            alt="open the PixieBrix quick bar"
+          />
+        </Button>
+      </div>
+    </div>
   );
 }
