34f40c317770b9e1a5154f42379fd31439eb32d2
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-07-10 18:00:03 -0700

    add check for auto-mod-updates flag

--- src/background/modUpdater.tsx
+++ src/background/modUpdater.tsx
@@ -15,6 +15,40 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export function initModUpdater(): void {
-  console.log("*** initModUpdater");
+import type { Me } from "@/types/contract";
+import { maybeGetLinkedApiClient } from "@/services/apiClient";
+import reportError from "@/telemetry/reportError";
+
+// TODO: we should start extracting this request pattern into an api of some
+//  kind that the background script can use
+async function autoModUpdatesEnabled(): Promise<boolean> {
+  const client = await maybeGetLinkedApiClient();
+  if (client == null) {
+    console.debug(
+      "Skipping automatic mod updates because the extension is not linked to the PixieBrix service"
+    );
+    return false;
+  }
+
+  try {
+    const { data: profile } = await client.get<Me>("/api/me/");
+
+    //  TODO: check if status code of >= 400 is caught by the try/catch
+
+    return profile.flags.includes("automatic-mod-updates");
+  } catch (error) {
+    reportError(error);
+    return false;
+  }
+}
+
+export async function initModUpdater(): Promise<void> {
+  // Check for automatic-mod-updates flag
+  if (await autoModUpdatesEnabled()) {
+    console.log("*** automatic mod updates enabled :)");
+    return;
+  }
+
+  // if not present, return
+  console.log("*** automatic mod updates not enabled");
 }
