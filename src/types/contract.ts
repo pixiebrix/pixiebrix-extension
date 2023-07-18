643411c8164b7e1b3c14cf7b255136eb54c62d03
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-07-18 15:01:23 -0700

    add comment to updates type

--- src/types/contract.ts
+++ src/types/contract.ts
@@ -280,6 +280,12 @@ export type ErrorItem = Except<
   user_agent_extension_version: SemVerString;
 };
 
+/**
+ * "Force updates" available for a list of packages.
+ * There is no auto-generated swagger Type for this because it serializes
+ * the Package config, which is a JSON object.
+ * @see ModDefinition
+ */
 export type PackageVersionUpdates = {
   updates: Array<{
     backwards_compatible: ModDefinition | null;
