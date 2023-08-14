577c70a2a50bbd1cf55c3c7f90579656878b33ae
Daniel Soetanto <turbochef.ds@gmail.com>
2023-08-14 11:32:40 -0500

    reorder margin options

--- src/components/fields/schemaFields/widgets/CssClassWidgets/CssSpacingWidget.tsx
+++ src/components/fields/schemaFields/widgets/CssClassWidgets/CssSpacingWidget.tsx
@@ -61,17 +61,17 @@ const paddingOptions: Option[] = [
 ];
 
 const marginOptions: Option[] = [
-  { label: "-5", value: "-5" },
-  { label: "-4", value: "-4" },
-  { label: "-3", value: "-3" },
-  { label: "-2", value: "-2" },
-  { label: "-1", value: "-1" },
   { label: "0", value: "0" },
   { label: "1", value: "1" },
   { label: "2", value: "2" },
   { label: "3", value: "3" },
   { label: "4", value: "4" },
   { label: "5", value: "5" },
+  { label: "-5", value: "-5" },
+  { label: "-4", value: "-4" },
+  { label: "-3", value: "-3" },
+  { label: "-2", value: "-2" },
+  { label: "-1", value: "-1" },
 ];
 
 function getValue(options: Option[], spacing: Spacing[], side: string | null) {
