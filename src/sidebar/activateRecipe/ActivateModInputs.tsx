c65c7bf9771ab03c9eafb8bf49f4aa01681715fd
Daniel Soetanto <turbochef.ds@gmail.com>
2023-08-18 14:05:35 -0500

    remove cancel

--- src/sidebar/activateRecipe/ActivateModInputs.tsx
+++ src/sidebar/activateRecipe/ActivateModInputs.tsx
@@ -158,18 +158,6 @@ const ActivateModInputs: React.FC<ActivateRecipeInputsProps> = ({
       )}
       <div className={styles.footer}>
         <Button
-          type="button"
-          variant="outline-danger"
-          onClick={() => {
-            reportEvent(Events.MOD_ACTIVATION_CANCEL, {
-              recipeId: recipe?.metadata?.id,
-            });
-            onClickCancel();
-          }}
-        >
-          Cancel
-        </Button>
-        <Button
           type="submit"
           onClick={() => {
             reportEvent(Events.MOD_ACTIVATION_SUBMIT, {
