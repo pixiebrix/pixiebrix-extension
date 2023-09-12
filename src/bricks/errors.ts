c3201d1a1c6c9b46346625cfabe3eeafe6f34f94
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-09-11 20:44:55 -0700

    fix RootErrorPanel tests

--- src/bricks/errors.ts
+++ src/bricks/errors.ts
@@ -82,12 +82,6 @@ export class InputValidationError extends BusinessError {
   }
 }
 
-export function isInputValidationError(
-  error: unknown
-): error is InputValidationError {
-  return typeof error === "object" && "schema" in error && "errors" in error;
-}
-
 /**
  * Error indicating output elements of a block did not match the schema.
  *
@@ -105,6 +99,12 @@ export class OutputValidationError extends BusinessError {
   ) {
     super(message);
   }
+}
+
+export function isIOValidationError(
+  error: unknown
+): error is InputValidationError | OutputValidationError {
+  return typeof error === "object" && "schema" in error && "errors" in error;
 }
 
 export class RemoteExecutionError extends BusinessError {
