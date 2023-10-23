32065c79c25f1819cd760accaea8086c542d5ebf
Jonathan Metz <jcmetz21@gmail.com>
2023-10-23 14:48:12 -0400

    Set type to unknown

--- src/errors/genericErrors.ts
+++ src/errors/genericErrors.ts
@@ -92,9 +92,9 @@ export class IncompatibleServiceError extends SuspiciousOperationError {
 export class InvalidTypeError extends Error {
   override name = "InvalidTypeError";
 
-  public readonly context?: MessageContext;
+  public readonly context?: unknown;
 
-  constructor(message: string, context?: MessageContext) {
+  constructor(message: string, context?: unknown) {
     super(message);
     this.context = context;
   }
