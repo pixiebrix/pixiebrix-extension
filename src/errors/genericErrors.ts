286a1b9d2ea3d27c8ca7c3f753c72f1837c66b16
Johnny Metz <jcmetz21@gmail.com>
2023-10-23 15:23:37 -0400

    Fix lint and prettier errors (#6689)

--- src/errors/genericErrors.ts
+++ src/errors/genericErrors.ts
@@ -92,9 +92,9 @@ export class IncompatibleServiceError extends SuspiciousOperationError {
 export class InvalidTypeError extends Error {
   override name = "InvalidTypeError";
 
-  public readonly context?: any;
+  public readonly context?: unknown;
 
-  constructor(message: string, context?: any) {
+  constructor(message: string, context?: unknown) {
     super(message);
     this.context = context;
   }
