5d79be76becd2dbb930bf3a0f9da560604895e1c
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-10-25 09:11:39 -0700

    move event into if statement and add a comment

--- src/tinyPages/devtools.ts
+++ src/tinyPages/devtools.ts
@@ -18,9 +18,11 @@
 import reportEvent from "@/telemetry/reportEvent";
 import { Events } from "@/telemetry/events";
 
-reportEvent(Events.DEVTOOLS_OPEN);
-
+// Only add the Page Editor to the devtools it's being used to inspect a modifiable web page,
+// i.e. the Page Editor is not relevant when inspecting a background page or the devtools itself.
 if (typeof chrome.devtools.inspectedWindow.tabId === "number") {
+  reportEvent(Events.DEVTOOLS_OPEN);
+
   chrome.devtools.panels.create(
     "PixieBrix",
     "",
