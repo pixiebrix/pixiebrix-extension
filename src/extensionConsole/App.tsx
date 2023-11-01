f43cc0f4b51ee45de4ec63dd3213591de9ccf485
Todd Schiller <todd.schiller@gmail.com>
2023-11-01 08:54:30 -0400

    Revert lazy loading in extension console

--- src/extensionConsole/App.tsx
+++ src/extensionConsole/App.tsx
@@ -15,19 +15,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React, { lazy, Suspense, useEffect } from "react";
+import React, { useEffect } from "react";
 import store, { hashHistory, persistor } from "@/store/optionsStore";
 import { Provider, useDispatch } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Container } from "react-bootstrap";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import ServicesEditor from "@/extensionConsole/pages/integrations/IntegrationsPage";
+import BrickCreatePage from "@/extensionConsole/pages/brickEditor/CreatePage";
+import BrickEditPage from "@/extensionConsole/pages/brickEditor/EditPage";
 import ModsPage from "@/extensionConsole/pages/mods/ModsPage";
+import SettingsPage from "@/extensionConsole/pages/settings/SettingsPage";
 import Navbar from "@/extensionConsole/Navbar";
 import Footer from "@/layout/Footer";
 import Sidebar from "@/extensionConsole/Sidebar";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import EnvironmentBanner from "@/layout/EnvironmentBanner";
+import ActivateRecipePage from "@/extensionConsole/pages/activateRecipe/ActivateRecipePage";
+import ActivateExtensionPage from "@/extensionConsole/pages/activateExtension/ActivateExtensionPage";
 import useRefreshRegistries from "@/hooks/useRefreshRegistries";
 import SetupPage from "@/extensionConsole/pages/onboarding/SetupPage";
 import UpdateBanner from "@/extensionConsole/pages/UpdateBanner";
@@ -36,6 +42,7 @@ import registerContribBlocks from "@/contrib/registerContribBlocks";
 import registerEditors from "@/contrib/editors";
 import DeploymentBanner from "@/extensionConsole/pages/deployments/DeploymentBanner";
 import { ModalProvider } from "@/components/ConfirmationModal";
+import WorkshopPage from "./pages/workshop/WorkshopPage";
 import InvitationBanner from "@/extensionConsole/pages/InvitationBanner";
 import BrowserBanner from "./pages/BrowserBanner";
 import useFlags from "@/hooks/useFlags";
@@ -48,64 +55,6 @@ import ReduxPersistenceContext, {
 } from "@/store/ReduxPersistenceContext";
 import IDBErrorDisplay from "@/extensionConsole/components/IDBErrorDisplay";
 import { DeploymentsProvider } from "@/extensionConsole/pages/deployments/DeploymentsContext";
-
-// Dynamically fetch the non-essential pages to reduce baseline resource footprint
-
-const IntegrationsPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "IntegrationsPage" */
-      "@/extensionConsole/pages/integrations/IntegrationsPage"
-    )
-);
-
-const ActivateModPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "ActivatePage" */
-      "@/extensionConsole/pages/activateRecipe/ActivateRecipePage"
-    )
-);
-
-const ActivateModComponentPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "ActivatePage" */
-      "@/extensionConsole/pages/activateExtension/ActivateExtensionPage"
-    )
-);
-
-const BrickCreatePage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "WorkshopPage" */
-      "@/extensionConsole/pages/brickEditor/CreatePage"
-    )
-);
-
-const BrickEditPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "WorkshopPage" */
-      "@/extensionConsole/pages/brickEditor/EditPage"
-    )
-);
-
-const WorkshopPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "WorkshopPage" */
-      "./pages/workshop/WorkshopPage"
-    )
-);
-
-const SettingsPage = lazy(
-  async () =>
-    import(
-      /* webpackChunkName: "SettingsPage" */
-      "@/extensionConsole/pages/settings/SettingsPage"
-    )
-);
 
 // Register the built-in bricks
 registerEditors();
@@ -148,62 +97,56 @@ const Layout = () => {
                 <DeploymentBanner />
                 <InvitationBanner />
                 <div className="content-wrapper">
-                  <Suspense fallback={<div></div>}>
-                    <ErrorBoundary ErrorComponent={IDBErrorDisplay}>
-                      <Switch>
+                  <ErrorBoundary ErrorComponent={IDBErrorDisplay}>
+                    <Switch>
+                      <Route
+                        exact
+                        path="/extensions/install/:extensionId"
+                        component={ActivateExtensionPage}
+                      />
+                      <Route
+                        exact
+                        path="/:sourcePage/activate/:recipeId"
+                        component={ActivateRecipePage}
+                      />
+
+                      <Route exact path="/settings" component={SettingsPage} />
+
+                      {permit("services") && (
+                        <Route
+                          path="/services/:id?"
+                          component={ServicesEditor}
+                        />
+                      )}
+
+                      {/* Switch does not support consolidating Routes using a React fragment */}
+                      {permit("workshop") && (
                         <Route
                           exact
-                          path="/extensions/install/:extensionId"
-                          component={ActivateModComponentPage}
+                          path="/workshop"
+                          component={WorkshopPage}
                         />
+                      )}
+
+                      {permit("workshop") && (
                         <Route
                           exact
-                          path="/:sourcePage/activate/:recipeId"
-                          component={ActivateModPage}
+                          path="/workshop/create/"
+                          component={BrickCreatePage}
                         />
+                      )}
 
+                      {permit("workshop") && (
                         <Route
                           exact
-                          path="/settings"
-                          component={SettingsPage}
+                          path="/workshop/bricks/:id/"
+                          component={BrickEditPage}
                         />
+                      )}
 
-                        {permit("services") && (
-                          <Route
-                            path="/services/:id?"
-                            component={IntegrationsPage}
-                          />
-                        )}
-
-                        {/* Switch does not support consolidating Routes using a React fragment */}
-                        {permit("workshop") && (
-                          <Route
-                            exact
-                            path="/workshop"
-                            component={WorkshopPage}
-                          />
-                        )}
-
-                        {permit("workshop") && (
-                          <Route
-                            exact
-                            path="/workshop/create/"
-                            component={BrickCreatePage}
-                          />
-                        )}
-
-                        {permit("workshop") && (
-                          <Route
-                            exact
-                            path="/workshop/bricks/:id/"
-                            component={BrickEditPage}
-                          />
-                        )}
-
-                        <Route component={ModsPage} />
-                      </Switch>
-                    </ErrorBoundary>
-                  </Suspense>
+                      <Route component={ModsPage} />
+                    </Switch>
+                  </ErrorBoundary>
                 </div>
                 <Footer />
               </div>
