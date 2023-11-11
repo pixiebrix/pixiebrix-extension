e9e4418245789d75b5d8b906f751965b344039b1
Federico Brigante <me@fregante.com>
2023-11-12 00:21:13 +0800

    Move to shared config

--- .eslintrc.js
+++ .eslintrc.js
@@ -91,9 +91,6 @@ module.exports = {
 
     // Rules that depend on https://github.com/pixiebrix/pixiebrix-extension/issues/775
     "@typescript-eslint/restrict-template-expressions": "warn",
-
-    // Enabled on CI and in the IDE, but it's disabled in the local `lint` script because it's excessively slow
-    "import/no-cycle": "error",
   },
   overrides: [
     {
