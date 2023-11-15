5e1575a62ef03b2bb0188c7763d1cf3e4d05d45e
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2023-11-15 15:08:50 -0800

    add content for first step in walkthrough

--- src/components/walkthroughModal/WalkthroughModalApp.tsx
+++ src/components/walkthroughModal/WalkthroughModalApp.tsx
@@ -15,19 +15,50 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Carousel, Modal } from "react-bootstrap";
-import React from "react";
+import { Button, Carousel, Col, Container, Modal, Row } from "react-bootstrap";
+import React, { useState } from "react";
 import { expectContext } from "@/utils/expectContext";
 import { showModal } from "@/bricks/transformers/ephemeralForm/modalUtils";
 import { getThisFrame } from "webext-messenger";
 import { registerModal } from "@/contentScript/walkthroughModalProtocol";
 import { closeWalkthroughModal } from "@/contentScript/messenger/api";
 import { Target } from "@/types/messengerTypes";
+import inspectContextMenuImage from "@img/inspect-context-menu.svg";
+import devtoolsShortcutWindowsImage from "@img/devtools-shortcut-windows.svg";
+import devtoolsShortcutMacImage from "@img/devtools-shortcut-mac.svg";
 
 let controller: AbortController;
 export const WalkthroughModalApp: React.FunctionComponent = () => {
+  const [stepIndex, setStepIndex] = useState(0);
   const params = new URLSearchParams(location.search);
   const opener = JSON.parse(params.get("opener")) as Target;
+
+  const steps = [
+    <Modal.Body className="show-grid">
+      <Container>
+        <Row>
+          <Modal.Title>Opening the Chrome dev tools</Modal.Title>
+        </Row>
+        <Row>
+          The Page Editor lives in the Chrome Dev tools. So the first step is to
+          open them. You can open it in two different ways.
+        </Row>
+        <Row>
+          <Col>
+            <img src={inspectContextMenuImage} alt="" />
+          </Col>
+          <Col>
+            <img src={devtoolsShortcutMacImage} alt="" />
+            <img src={devtoolsShortcutWindowsImage} alt="" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>Right click anywhere on the page and select “Inspect”</Col>
+          <Col>Or Utilize the keyboard shortcut for your system</Col>
+        </Row>
+      </Container>
+    </Modal.Body>,
+  ];
 
   return (
     <Modal
@@ -39,9 +70,37 @@ export const WalkthroughModalApp: React.FunctionComponent = () => {
       }}
     >
       <Modal.Header closeButton />
-      <Carousel>
-        <Modal.Body>hello world!</Modal.Body>
+      <Carousel
+        activeIndex={stepIndex}
+        interval={null}
+        slide={false}
+        controls={false}
+        indicators={false}
+      >
+        {steps.map((step) => {
+          return <Carousel.Item>{step}</Carousel.Item>;
+        })}
       </Carousel>
+      <Modal.Footer>
+        <Button
+          onClick={() => {
+            if (stepIndex > 0) {
+              setStepIndex(stepIndex - 1);
+            }
+          }}
+        >
+          Prev
+        </Button>
+        <Button
+          onClick={() => {
+            if (stepIndex < steps.length - 1) {
+              setStepIndex(stepIndex + 1);
+            }
+          }}
+        >
+          Next
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
