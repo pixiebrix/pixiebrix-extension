41aaec5fb1c7a5d99a65f3ca5250ef83bd0cc2ff
Todd Schiller <todd.schiller@gmail.com>
2023-11-28 11:12:54 -0500

    Add test

---
+++ src/bricks/transformers/IdentityOptions.test.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2023 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import { render } from "@/pageEditor/testHelpers";
+import IdentityOptions from "@/bricks/transformers/IdentityOptions";
+import { getExampleBlockConfig } from "@/pageEditor/exampleBlockConfigs";
+import { IdentityTransformer } from "@/bricks/transformers/identity";
+import brickRegistry from "@/bricks/registry";
+import { screen } from "@testing-library/react";
+import registerDefaultWidgets from "@/components/fields/schemaFields/widgets/registerDefaultWidgets";
+import registerEditors from "@/contrib/editors";
+import { waitForEffect } from "@/testUtils/testHelpers";
+
+beforeAll(() => {
+  brickRegistry.register([new IdentityTransformer()]);
+  registerDefaultWidgets();
+  registerEditors();
+});
+
+describe("IdentityOptions", () => {
+  test("shows object widget by default", async () => {
+    render(<IdentityOptions name="foo" configKey="config" />, {
+      initialValues: {
+        foo: {
+          config: getExampleBlockConfig(IdentityTransformer.BRICK_ID),
+        },
+      },
+    });
+
+    await waitForEffect();
+
+    expect(
+      screen.getByRole("button", { name: "Add Property" })
+    ).toBeInTheDocument();
+  });
+});
