bf6140956177039a29e18fec960609878ebcbc47
Todd Schiller <todd.schiller@gmail.com>
2023-12-22 11:25:45 -0500

    Address PR comments

--- knip.js
+++ knip.js
@@ -5,10 +5,8 @@ const config = configFactory(process.env, {});
 // https://knip.dev/overview/configuration#customize
 const knipConfig = {
   $schema: "https://unpkg.com/knip@3/schema.json",
-  // Try to get webpack plugin working
-  webpack: {
-    config: "webpack.config.js",
-  },
+  // Force to use webpack plugin. :shrug: it doesn't seem to be picking up entry points/aliases automatically though
+  webpack: true,
   entry: [
     ...Object.values(config.entry).map((x) =>
       `${x}.{ts,tsx,js,jsx}`.replace("./", ""),
@@ -26,6 +24,8 @@ const knipConfig = {
     "@contrib/**",
     // Aliases defined in tsconfig.json
     "src/contrib/uipath/quietLogger.ts",
+    // Development/debugging helpers
+    "src/development/hook/**",
   ],
 
   rules: {
@@ -46,6 +46,7 @@ const knipConfig = {
   },
 };
 
+// Echo settings to console to make CI results easier to understand/debug
 console.log(JSON.stringify(knipConfig, null, 2));
 
 export default knipConfig;
