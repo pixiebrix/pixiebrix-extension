3725dfeaa7f0a5c9e465c21069ca2a346b712de8
Todd Schiller <todd.schiller@gmail.com>
2024-01-01 21:39:15 -0600

    Knip unused dependencies (#7258)

--- knip.mjs
+++ knip.mjs
@@ -5,25 +5,33 @@ const config = configFactory(process.env, {});
 // https://knip.dev/overview/configuration#customize
 const knipConfig = {
   $schema: "https://unpkg.com/knip@3/schema.json",
-  // Force to use webpack plugin. :shrug: it doesn't seem to be picking up entry points/aliases automatically though
-  webpack: true,
+  webpack: {
+    config: [
+      "webpack.config.mjs",
+      "scripts/webpack.scripts.js",
+      ".storybook/main.js",
+    ],
+  },
   entry: [
     ...Object.values(config.entry).map((x) =>
       `${x}.{ts,tsx,js,jsx}`.replace("./", ""),
     ),
     "src/development/headers.ts",
+    // App messenger
+    "src/contentScript/externalProtocol.ts",
     // Jest setup files
     "src/testUtils/testEnv.js",
     "src/testUtils/testAfterEnv.ts",
     "src/testUtils/injectRegistries.ts",
+    "src/testUtils/FixJsdomEnvironment.js",
+    // Script helpers
+    "scripts/manifest.mjs",
   ],
   project: ["src/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
   // https://knip.dev/guides/handling-issues#mocks-and-other-implicit-imports
   ignore: [
     "**/__mocks__/**",
     "@contrib/**",
-    // App external messenger
-    "src/contentScript/externalProtocol.ts",
     // Polyfills
     "src/vendors/process.js",
     // Aliases defined in tsconfig.json
@@ -31,10 +39,27 @@ const knipConfig = {
     // Development/debugging helpers
     "src/development/hooks/**",
   ],
-
+  ignoreDependencies: [
+    // Browser environment types
+    "@types/chrome",
+    "@types/dom-navigation",
+    // Webpack environment types, provides require.context, etc.
+    "@types/webpack-env",
+    // Used by sheetsApi
+    "@types/gapi.client.sheets-v4",
+    // Referenced in global.d.ts
+    "@total-typescript/ts-reset",
+    // Referenced in scss files
+    "webext-base-css",
+    "compass-mixins",
+    // Polyfills via ProvidePlugin aren't detected by webpack plugin
+    "min-document",
+    // Not getting detected by webpack plugin for .storybook/main.js
+    "style-loader",
+    "@storybook/react-webpack5",
+  ],
   rules: {
-    // https://knip.dev/reference/issue-types/
-    dependencies: "warn",
+    // Issue Type reference: https://knip.dev/reference/issue-types/
     unlisted: "warn",
     binaries: "warn",
     unresolved: "warn",
@@ -43,10 +68,12 @@ const knipConfig = {
     types: "warn",
     nsTypes: "warn",
     enumMembers: "warn",
+    // Warns on uiSchema/defaultOutputKey
     classMembers: "warn",
     // Incrementally enforce rules over time
     files: "error",
     duplicates: "error",
+    dependencies: "error",
   },
 };
 
