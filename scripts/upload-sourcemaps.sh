d20caee90966f442913b6bc9886e7b77d9e1bbf2
Todd Schiller <todd.schiller@gmail.com>
2024-01-14 13:21:20 -0700

    [WIP] try fixing @datadog/datadog-ci

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -13,8 +13,8 @@ set -e
 : "${AWS_DEFAULT_REGION?Need to set AWS_DEFAULT_REGION}"
 
 # Upload to S3 for debugging in Chrome
-S3_UPLOAD_BASE_URL="s3://pixiebrix-extension-source-maps/$SOURCE_MAP_PATH"
-aws s3 cp ./dist "$S3_UPLOAD_BASE_URL" --exclude '*' --include '*.map' --include '*.js' --recursive --no-progress
+# S3_UPLOAD_BASE_URL="s3://pixiebrix-extension-source-maps/$SOURCE_MAP_PATH"
+# aws s3 cp ./dist "$S3_UPLOAD_BASE_URL" --exclude '*' --include '*.map' --include '*.js' --recursive --no-progress
 
 # Datadog uses release-version, not the code commit version. So get from produced manifest
 # TODO: verify that the `+<commit_hash>` is working if enabled for CI builds
@@ -24,7 +24,7 @@ RELEASE_VERSION=$(jq '.version_name' dist/manifest.json)
 # Because this command runs from a Git repo context, Datadog should also automatically link to our project from the UI.
 # https://docs.datadoghq.com/real_user_monitoring/guide/upload-javascript-source-maps/?tab=webpackjs
 # `release-version` must match the version in initErrorReporter.ts and performance.ts
-npx --yes @datadog/datadog-ci sourcemaps upload dist \
+npx --yes @datadog/datadog-ci sourcemaps upload ./dist \
   --service=pixiebrix-browser-extension \
   --release-version="$RELEASE_VERSION" \
-  --minified-path-prefix="extension://dynamichost/"
+  --minified-path-prefix="/"
