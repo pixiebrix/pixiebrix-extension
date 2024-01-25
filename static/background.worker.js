a0704409ebfeff18c65808bae05e29768ff4fb04
Federico Brigante <me@fregante.com>
2024-01-25 23:31:14 +0800

    Make worker more resilient to DOM API usage attempts (#7425)

--- static/background.worker.js
+++ static/background.worker.js
@@ -1,2 +1,25 @@
 // Don't include `background.worker.js` in webpack, there's no advantage in doing so
+
+function get() {
+  /*
+	When scripts use DOM APIs, the worker will crash on load with the error
+	`ReferenceError: window is not defined` without pointing to the correct line.
+	This listener defines some globals so that they return `undefined` instead
+	of throwing an error on access.
+	*/
+
+  // Debug helpers: trace or breakable line
+
+  // There are a lot of safe `window` checks in the code so the logger cannot be enabled by default.
+  // console.trace('DOM access in worker');
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  1; // This line lets you add a breakpoint so that you can stop the debugger
+}
+
+Object.defineProperties(self, {
+  document: { get },
+  window: { get },
+});
+
 self.importScripts("./grayIconWhileLoading.js", "./background.js");
