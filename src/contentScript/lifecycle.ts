fea2236a304043761cd46f04e326635dd8e41794
Federico Brigante <me@fregante.com>
2024-02-11 17:51:13 +0800

    Remove extension points on context invalidated

--- src/contentScript/lifecycle.ts
+++ src/contentScript/lifecycle.ts
@@ -46,6 +46,7 @@ import {
 } from "@/utils/promiseUtils";
 import { $safeFind } from "@/utils/domUtils";
 import { onContextInvalidated } from "webext-events";
+import { ContextMenuStarterBrickABC } from "@/starterBricks/contextMenu";
 
 /**
  * True if handling the initial page load.
@@ -676,4 +677,14 @@ export async function initNavigation() {
     }),
     { signal: onContextInvalidated.signal },
   );
+
+  onContextInvalidated.addListener(() => {
+    for (const [extensionId, extensionPoint] of _persistedExtensions) {
+      if (!(extensionPoint instanceof ContextMenuStarterBrickABC)) {
+        extensionPoint.removeModComponent(extensionId);
+      }
+    }
+
+    _persistedExtensions.clear();
+  });
 }
