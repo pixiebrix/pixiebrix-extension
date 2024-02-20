b8653f86dd82892ae20def14cf92619e02922f94
Todd Schiller <todd.schiller@gmail.com>
2024-02-20 14:21:22 -0500

    #7671: add text command popover behind skunkworks flag (#7672)

---
+++ src/bricks/effects/AddTextCommand.ts
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { validateRegistryId } from "@/types/helpers";
+import { propertiesToSchema } from "@/validators/generic";
+import type {
+  BrickArgs,
+  BrickOptions,
+  PipelineExpression,
+} from "@/types/runtimeTypes";
+import { type Schema } from "@/types/schemaTypes";
+import { EffectABC } from "@/types/bricks/effectTypes";
+import {
+  commandRegistry,
+  initCommandController,
+} from "@/contentScript/commandPopover/commandController";
+import { BusinessError } from "@/errors/businessErrors";
+import { getSettingsState } from "@/store/settings/settingsStorage";
+import { validateOutputKey } from "@/runtime/runtimeTypes";
+
+type CommandArgs = {
+  /**
+   * The shortcut for the text command.
+   */
+  shortcut: string;
+  /**
+   * The title for the Text Command
+   */
+  title: string;
+  /**
+   * The text generator to run
+   */
+  generate: PipelineExpression;
+};
+
+class AddTextCommand extends EffectABC {
+  static BRICK_ID = validateRegistryId("@pixiebrix/command/text-command");
+
+  static DEFAULT_PIPELINE_VAR = validateOutputKey("currentText");
+
+  constructor() {
+    super(
+      AddTextCommand.BRICK_ID,
+      "[Experimental] Add Text Command",
+      "Add a dynamic text command",
+    );
+  }
+
+  override async isPure(): Promise<boolean> {
+    // Safe default -- need to be able to inspect the inputs to determine if pure
+    return false;
+  }
+
+  override async isRootAware(): Promise<boolean> {
+    // Safe default -- need to be able to inspect the inputs to determine if any sub-calls are root aware
+    return true;
+  }
+
+  inputSchema: Schema = propertiesToSchema(
+    {
+      shortcut: {
+        type: "string",
+        description: "The shortcut for the text command",
+      },
+      title: {
+        type: "string",
+        description: "The title for the Text Command",
+      },
+      generate: {
+        $ref: "https://app.pixiebrix.com/schemas/pipeline#",
+        description: "The text generator to run",
+      },
+    },
+    ["shortcut", "title", "generate"],
+  );
+
+  async effect(
+    { shortcut, title, generate: generatePipeline }: BrickArgs<CommandArgs>,
+    { logger, runPipeline, abortSignal }: BrickOptions,
+  ): Promise<void> {
+    // The runtime checks the abortSignal for each brick. But check here too to avoid flickering in the popover
+    if (abortSignal?.aborted) {
+      return;
+    }
+
+    if (logger.context.extensionId == null) {
+      throw new Error("Must be run in the context of a mod");
+    }
+
+    // Counter to keep track of the action run number for tracing
+    let counter = 0;
+
+    commandRegistry.register({
+      componentId: logger.context.extensionId,
+      // Trim leading slash to be resilient to user input
+      shortcut: shortcut.replace(/^\//, ""),
+      title,
+      async handler(currentText: string): Promise<string> {
+        counter++;
+
+        const last = await runPipeline(
+          generatePipeline,
+          { key: "generate", counter },
+          {
+            [`@${AddTextCommand.DEFAULT_PIPELINE_VAR}`]: currentText,
+          },
+        );
+
+        if (last == null) {
+          throw new BusinessError("Text generator did not return a value");
+        }
+
+        if (typeof last === "object") {
+          throw new BusinessError("Text generator returned an object/array");
+        }
+
+        if (typeof last !== "string") {
+          logger.debug("Text generator returned non-string value", { last });
+        }
+
+        return (last as string | boolean | number).toLocaleString();
+      },
+    });
+
+    const { textCommandPopover } = await getSettingsState();
+    if (textCommandPopover) {
+      initCommandController();
+    }
+  }
+}
+
+export default AddTextCommand;
---
+++ src/contentScript/commandPopover/commandPopoverSlice.ts
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { TextCommand } from "@/contentScript/commandPopover/CommandRegistry";
+import type { Nullishable } from "@/utils/nullishUtils";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { sortBy } from "lodash";
+import type { AsyncState } from "@/types/sliceTypes";
+import {
+  errorToAsyncState,
+  loadingAsyncStateFactory,
+  valueToAsyncState,
+} from "@/utils/asyncStateUtils";
+
+type PopoverState = {
+  /**
+   * The active query string corresponding to the search results, or null if there's no active query (e.g., because the
+   * user pressed the escape key).
+   */
+  query: Nullishable<string>;
+  /**
+   * The sorted list of search results
+   */
+  results: TextCommand[];
+  /**
+   * The index of the selected result, or nullish if no result is selected/available.
+   */
+  selectedIndex: Nullishable<number>;
+
+  /**
+   * The latest active command, or null if no command has been run.
+   */
+  activeCommand: Nullishable<{
+    command: TextCommand;
+    state: AsyncState;
+  }>;
+};
+
+export const initialState: PopoverState = {
+  query: null,
+  results: [],
+  selectedIndex: null,
+  activeCommand: null,
+};
+
+export function selectSelectedCommand(
+  state: PopoverState,
+): Nullishable<TextCommand> {
+  return state.selectedIndex == null
+    ? null
+    : state.results[state.selectedIndex];
+}
+
+export const popoverSlice = createSlice({
+  name: "popoverSlice",
+  initialState,
+  reducers: {
+    offsetSelectedIndex(
+      state,
+      action: PayloadAction<{
+        offset: number;
+      }>,
+    ) {
+      const { offset } = action.payload;
+
+      if (state.selectedIndex == null || state.results.length === 0) {
+        state.selectedIndex = null;
+      } else {
+        const next = (state.selectedIndex ?? 0) + offset;
+        if (next >= 0) {
+          state.selectedIndex = next % state.results.length;
+        } else {
+          state.selectedIndex =
+            state.results.length + (next % state.results.length);
+        }
+      }
+    },
+    search(
+      state,
+      action: PayloadAction<{
+        commands: TextCommand[];
+        query: Nullishable<string>;
+      }>,
+    ) {
+      const { commands, query } = action.payload;
+      state.query = query;
+
+      // For performance, could consider adding a setCommands action to avoid sorting on every search
+      const sortedCommands = sortBy(commands, "shortcut");
+      if (query == null) {
+        state.results = sortedCommands;
+        state.selectedIndex = null;
+      } else {
+        state.results = sortedCommands.filter((command) =>
+          command.shortcut.startsWith(query),
+        );
+        state.selectedIndex = state.results.length > 0 ? 0 : null;
+      }
+    },
+
+    // Async thunks don't work with React useReducer so write async logic as a hook
+    // https://github.com/reduxjs/redux-toolkit/issues/754
+    setCommandLoading(
+      state,
+      action: PayloadAction<{
+        command: TextCommand;
+      }>,
+    ) {
+      const { command } = action.payload;
+      state.activeCommand = { command, state: loadingAsyncStateFactory() };
+    },
+    // In practice, the popover will be hidden on success. This state is for Storybook where the component is persistent
+    setCommandSuccess(
+      state,
+      action: PayloadAction<{
+        text: string;
+      }>,
+    ) {
+      const { text } = action.payload;
+      if (state.activeCommand) {
+        state.activeCommand.state = valueToAsyncState(text);
+      }
+    },
+    setCommandError(
+      state,
+      action: PayloadAction<{
+        error: unknown;
+      }>,
+    ) {
+      // Assumes only one command at a time
+      const { error } = action.payload;
+      if (state.activeCommand) {
+        state.activeCommand.state = errorToAsyncState(error);
+      }
+    },
+  },
+});
