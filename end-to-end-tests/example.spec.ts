a1fd453d4790667183d3353b8e7e80b0ff2fa86b
Eduardo Fungairino <eduardo@pixiebrix.com>
2024-02-26 16:34:48 -0500

    Introducing playwright for e2e tests
    - Including a sample test hitting our test sites
    - Github workflow

---
+++ end-to-end-tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("create-react-app", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/create-react-app/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Home/);
+  });
+
+  test("get started link", async ({ page }) => {
+    await page.goto("/create-react-app/");
+
+    // Click the get started link.
+    await page.getByRole("link", { name: "Table" }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(
+      page.getByRole("heading", { name: "Transsaction Table" }),
+    ).toBeVisible();
+  });
+});
