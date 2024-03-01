3266b44487375c0e191f5f76892c8ec4c454083c
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-02-29 16:55:29 -0800

    fix dead code lint error

--- end-to-end-tests/env.ts
+++ end-to-end-tests/env.ts
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { loadEnv } from "@/../scripts/env";
+// eslint-disable-next-line no-restricted-imports
+import { loadEnv } from "../scripts/env";
 
 process.env.ENV_FILE ??= ".env.development";
 loadEnv();
