8e43794b69ff9f35f5f708e87c9501bad37be796
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-03-01 08:24:56 -0800

    #7689 Add auth utility for Playwright tests (#7761)
    * add basic auth steps
    
    * reuse loadEnv for playwright env variable access
    
    * suppress and add TODO to env.mjs import errors
    
    * introduce test user config env variables
    
    * export auth to user.json
    
    * introduce basic auth smoke test to demonstrate auth
    
    * add assertion to authentication setup"
    
    * add files to strictnullchecks
    
    * add setup as a dependency of other tests
    
    * fix loadEnv import type error
    
    * default to ENV_FILE value if available
    
    * add parseEnv to env.d.ts
    
    * fix lint error
    
    * introduce env.ts file
    
    * fix dead code lint error
    
    * remove unused env.d.ts file
    
    * add e2e env variables to ci

---
+++ end-to-end-tests/env.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// eslint-disable-next-line no-restricted-imports
+import { loadEnv } from "../scripts/env";
+
+process.env.ENV_FILE ??= ".env.development";
+loadEnv();
+
+const requiredEnvVariables = [
+  "SERVICE_URL",
+  "E2E_TEST_USER_EMAIL_UNAFFILIATED",
+  "E2E_TEST_USER_PASSWORD_UNAFFILIATED",
+] as const;
+
+const optionalEnvVariables = ["CI"] as const;
+
+type RequiredEnvVariables = Record<
+  (typeof requiredEnvVariables)[number],
+  string
+>;
+type OptionalEnvVariables = Record<
+  (typeof optionalEnvVariables)[number],
+  string | undefined
+>;
+
+for (const key of requiredEnvVariables) {
+  // eslint-disable-next-line security/detect-object-injection -- key is a constant
+  if (process.env[key] === undefined) {
+    throw new Error(`Required environment variable is not configured: ${key}`);
+  }
+
+  // eslint-disable-next-line security/detect-object-injection -- key is a constant
+  if (typeof process.env[key] !== "string") {
+    // For the time being we expect all of our requiredEnvVariables to be strings
+    throw new TypeError(
+      `Required environment variable is not configured: ${key}`,
+    );
+  }
+}
+
+export const {
+  SERVICE_URL,
+  E2E_TEST_USER_EMAIL_UNAFFILIATED,
+  E2E_TEST_USER_PASSWORD_UNAFFILIATED,
+} = process.env as RequiredEnvVariables;
+
+export const { CI } = process.env as OptionalEnvVariables;
--- scripts/env.d.ts
+++ scripts/env.d.ts
@@ -15,10 +15,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-interface process {
-  env: {
-    // Default environment variable used in github CI
-    // https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-    CI?: string;
-  };
-}
+export function loadEnv(): void;
+export function parseEnv(): void;
