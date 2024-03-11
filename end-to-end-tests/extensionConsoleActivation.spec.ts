09e85805bc0c08ae28c31e8751dac1823384f870
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-03-11 16:08:37 -0700

    add handling for dialog

--- end-to-end-tests/extensionConsoleActivation.spec.ts
+++ end-to-end-tests/extensionConsoleActivation.spec.ts
@@ -14,6 +14,15 @@ test.describe("Extension Console Activation", () => {
     );
     await expect(page.getByText("Activate Mod")).toBeVisible();
     await expect(page.getByText("[Template]: Translation")).toBeVisible();
+
+    page.on("dialog", async (dialog) => {
+      console.log("*** dialog handler");
+      return dialog.accept();
+    });
     await page.click("button:has-text('Activate')");
+    await page.waitForURL(`chrome-extension://${extensionId}/options.html`);
+    await expect(
+      page.getByText("Installed [Template]: Translation"),
+    ).toBeVisible();
   });
 });
