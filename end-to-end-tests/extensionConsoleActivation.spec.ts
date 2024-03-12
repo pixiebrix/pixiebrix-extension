8f4f2560269a1e690865105c3281c3e5e95c2e72
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-03-12 16:54:11 -0700

    add quickbar interaction to extensionConsoleActivation.spec

--- end-to-end-tests/extensionConsoleActivation.spec.ts
+++ end-to-end-tests/extensionConsoleActivation.spec.ts
@@ -4,6 +4,7 @@ test.describe("Extension Console Activation", () => {
   test("can activate a mod with no config options", async ({
     page,
     extensionId,
+    baseURL,
   }) => {
     await linkBrowserExtensionViaAdminConsole(page, expect);
 
@@ -17,5 +18,14 @@ test.describe("Extension Console Activation", () => {
     await expect(page.getByText("GIPHY Search")).toBeVisible();
     await page.click("button:has-text('Activate')");
     await expect(page.getByText("Installed GIPHY Search")).toBeVisible();
+
+    // TODO assert on mods page that the mod is activated
+    await page.goto(baseURL);
+    await page.waitForTimeout(5000);
+    await page.getByText("Index of  /").click();
+    await page.locator("body").press("Meta+M");
+    await page.waitForTimeout(5000);
+    await page.keyboard.press("Enter");
+    await page.getByText("GIPHY Search").click();
   });
 });
