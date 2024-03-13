4223309a706706c05de7d50971cb283c6f447242
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-03-13 16:08:08 -0700

    #7690 First Playwright regression checklist test (#7912)
    * introduce test base extesnionConsoleActivation
    
    * add handling for dialog
    
    * add basic activation test steps
    
    * add quickbar interaction to extensionConsoleActivation.spec
    
    * change test mod to @pixies/highlight-keywords
    
    * clean up test
    
    * move tests to tests/ directory
    
    * rename test

---
+++ end-to-end-tests/tests/extensionConsoleActivation.spec.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { test, expect } from "../fixtures/extensionBase";
+
+test("can activate and use highlight keywords mod", async ({
+  page,
+  extensionId,
+}) => {
+  const modName = "Highlight Specific Keywords When Page Loads";
+  const modId = "@pixies/highlight-keywords";
+
+  await page.goto(
+    `chrome-extension://${extensionId}/options.html#/marketplace/activate/${encodeURIComponent(
+      modId,
+    )}`,
+  );
+
+  await expect(page.getByText("Activate Mod")).toBeVisible();
+  await expect(page.getByText(modName)).toBeVisible();
+  await page.click("button:has-text('Activate')");
+  await expect(page.getByText(`Installed ${modName}`)).toBeVisible();
+
+  await page.goto("/bootstrap-5");
+
+  const highlightedElements = await page.locator("mark").all();
+
+  await Promise.all(
+    highlightedElements.map(async (element) => {
+      await expect(element).toHaveText("PixieBrix");
+    }),
+  );
+});
