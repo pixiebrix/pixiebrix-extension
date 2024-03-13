60eb63419229df4334f41470387fc942fbce2298
Federico Brigante <me@fregante.com>
2024-03-13 22:25:10 +0800

    Lint
    Co-authored-by: Graham Langford <30706330+grahamlangford@users.noreply.github.com>

--- src/components/brickModalNoTags/BrickModal.tsx
+++ src/components/brickModalNoTags/BrickModal.tsx
@@ -235,7 +235,7 @@ function ActualModal<T extends Metadata>({
       keyboard={false}
     >
       <Modal.Body className={cx(styles.body, "gap-4")}>
-        <div className=" d-flex flex-column gap-3">
+        <div className="d-flex flex-column gap-3">
           <InputGroup>
             <InputGroup.Prepend>
               <InputGroup.Text>Search</InputGroup.Text>
