99276657eac9e18c728047ac864a1157f98d1fd0
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-03-13 12:44:56 -0700

    change test mod to @pixies/highlight-keywords

--- end-to-end-tests/extensionConsoleActivation.spec.ts
+++ end-to-end-tests/extensionConsoleActivation.spec.ts
@@ -1,31 +1,29 @@
-import { test, expect, linkBrowserExtensionViaAdminConsole } from "./fixtures";
+import { test, expect } from "./fixtures/extensionBase";
 
-test.describe("Extension Console Activation", () => {
-  test("can activate a mod with no config options", async ({
-    page,
-    extensionId,
-    baseURL,
-  }) => {
-    await linkBrowserExtensionViaAdminConsole(page, expect);
+test("can activate a mod with config options", async ({
+  page,
+  extensionId,
+  baseURL,
+}) => {
+  const modName = "Highlight Specific Keywords When Page Loads";
+  const modId = "@pixies/highlight-keywords";
 
-    await page.goto(
-      `chrome-extension://${extensionId}/options.html#/marketplace/activate/${encodeURIComponent(
-        "@pixies/giphy/giphy-search",
-      )}`,
-    );
+  await page.goto(
+    `chrome-extension://${extensionId}/options.html#/marketplace/activate/${encodeURIComponent(
+      modId,
+    )}`,
+  );
 
-    await expect(page.getByText("Activate Mod")).toBeVisible();
-    await expect(page.getByText("GIPHY Search")).toBeVisible();
-    await page.click("button:has-text('Activate')");
-    await expect(page.getByText("Installed GIPHY Search")).toBeVisible();
+  await expect(page.getByText("Activate Mod")).toBeVisible();
+  await expect(page.getByText(modName)).toBeVisible();
+  await page.click("button:has-text('Activate')");
+  await expect(page.getByText(`Installed ${modName}`)).toBeVisible();
 
-    // TODO assert on mods page that the mod is activated
-    await page.goto(baseURL);
-    await page.waitForTimeout(5000);
-    await page.getByText("Index of  /").click();
-    await page.locator("body").press("Meta+M");
-    await page.waitForTimeout(5000);
-    await page.keyboard.press("Enter");
-    await page.getByText("GIPHY Search").click();
-  });
+  // TODO -- assert on mods page that the mod is activated
+  await page.goto(baseURL);
+  await page.click("a[title='/bootstrap-5']");
+
+  for (const element of await page.locator("mark").all()) {
+    await expect(element).toHaveText("PixieBrix");
+  }
 });
