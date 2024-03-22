dbe6eb6657e8c4acba5f49fdd591d21da80f86d1
Federico Brigante <me@fregante.com>
2024-03-22 14:24:33 +0700

    strictnullchecks

--- src/utils/tabUtils.ts
+++ src/utils/tabUtils.ts
@@ -2,6 +2,7 @@ import { reactivateTab } from "@/contentScript/messenger/api";
 import { forEachTab } from "./extensionUtils";
 
 // XXX: It can be moved into `extensionUtils.ts` after `strictNullChecks` migration
+// https://github.com/pixiebrix/pixiebrix-extension/issues/6526
 export function reactivateEveryTab(): void {
   console.debug("Reactivate all tabs");
   void forEachTab(reactivateTab);
