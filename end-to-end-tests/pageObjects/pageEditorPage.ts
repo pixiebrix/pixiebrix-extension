f3ba4b9841c122fa8b8fd7748d78f19df4083936
Eduardo Fungairino <eduardo@pixiebrix.com>
2024-04-01 11:10:24 -0400

    #7961 pageEditor e2e smoke test (#8098)
    * pageEditor e2e smoke test
    
    * fix inconsistent page editor viewport

---
+++ end-to-end-tests/pageObjects/pageEditorPage.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { getBasePageEditorUrl } from "./constants";
+import { type BrowserContext } from "@playwright/test";
+import { expect } from "../fixtures/extensionBase";
+
+export class PageEditorPage {
+  private readonly pageEditorUrl: string;
+
+  constructor(
+    private readonly context: BrowserContext,
+    private readonly urlToConnectTo: string,
+    extensionId: string,
+  ) {
+    this.pageEditorUrl = getBasePageEditorUrl(extensionId);
+  }
+
+  async goto() {
+    const pageEditorPage = await this.context.newPage();
+    await pageEditorPage.goto(this.pageEditorUrl);
+    // Set the viewport size to the expected in horizontal layout size of the devconsole when docked on the bottom.
+    await pageEditorPage.setViewportSize({ width: 1280, height: 300 });
+    await pageEditorPage.getByTestId(`tab-${this.urlToConnectTo}`).click();
+    const heading = pageEditorPage.getByRole("heading", {
+      name: "Welcome to the Page Editor!",
+    });
+    await expect(heading).toBeVisible();
+  }
+}
