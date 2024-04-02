136208dda7f6d28d47074ddec0c882c1d63742c9
Federico Brigante <me@fregante.com>
2024-04-02 16:27:25 +0700

    /2

--- src/activation/useActivateRecipe.ts
+++ src/activation/useActivateRecipe.ts
@@ -44,7 +44,7 @@ export type ActivateRecipeFormCallback =
    *
    * @param formValues The form values for recipe configuration options
    * @param recipe The recipe definition to install
-   * @returns {Promise<ActivateResult>} a promise that resolves to an ActivateResult
+   * @returns a promise that resolves to an ActivateResult
    */
   (formValues: WizardValues, recipe: ModDefinition) => Promise<ActivateResult>;
 
@@ -61,8 +61,9 @@ function selectActivateEventData(recipe: ModDefinition) {
  * React hook to install a recipe.
  *
  * Prompts the user to grant permissions if PixieBrix does not already have the required permissions.
- * @param source The source of the activation, only used for reporting purposes checkPermissions - Whether to check for permissions before activating the recipe
- * @returns - A callback that can be used to activate a recipe
+ * @param source The source of the activation, only used for reporting purposes
+ * @param checkPermissions Whether to check for permissions before activating the recipe
+ * @returns A callback that can be used to activate a recipe
  * @see useActivateRecipeWizard
  */
 function useActivateRecipe(
--- src/bricks/transformers/tourStep/overlay.ts
+++ src/bricks/transformers/tourStep/overlay.ts
@@ -30,7 +30,7 @@
  * @param [x] Offset from top left corner in x axis. default 0.
  * @param [y] Offset from top left corner in y axis. default 0.
  * @param [r] Corner Radius. Keep this smaller than half of width or height.
- * @returns - Rounded rectangle overlay path data.
+ * @returns Rounded rectangle overlay path data.
  */
 function makeOverlayPath({
   width,
