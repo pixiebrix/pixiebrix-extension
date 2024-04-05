6cf04abf75ae43b8f37f43549ad3a3aef02f9b61
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-04-05 10:20:18 -0700

    create useDeactivateUnassignedDeploymentsEffect

---
+++ src/extensionConsole/pages/deployments/useDeactivateUnassignedDeploymentsEffext.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { deactivateUnassignedModComponents } from "@/extensionConsole/pages/deployments/activateDeployments";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import type { Dispatch } from "@reduxjs/toolkit";
+import { type ModComponentBase } from "@/types/modComponentTypes";
+
+const useDeactivateUnassignedDeploymentsEffect = (
+  unassignedModComponents: ModComponentBase[],
+) => {
+  const dispatch = useDispatch<Dispatch>();
+  useEffect(() => {
+    deactivateUnassignedModComponents({
+      dispatch,
+      unassignedModComponents,
+    });
+  }, []);
+};
