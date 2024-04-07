4225a47b935828bccf5b10a107a5a5af72b18273
Federico Brigante <me@fregante.com>
2024-04-07 12:33:37 +0700

    Extract jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,41 +43,9 @@ module.exports = {
   extends: [
     // Full config: https://github.com/pixiebrix/eslint-config-pixiebrix/blob/main/index.js
     "pixiebrix",
-    "plugin:jsdoc/recommended-typescript-error",
   ],
-  plugins: ["local-rules", "@shopify", "jsdoc"],
+  plugins: ["local-rules", "@shopify"],
   rules: {
-    // No need to add or remove spacing
-    "jsdoc/tag-lines": "off",
-
-    // Conflicts with our usage of @since
-    "jsdoc/check-values": "off",
-
-    // Function/parameter names should be self-explanatory, descriptions are useful but not required
-    "jsdoc/require-jsdoc": "off",
-    "jsdoc/require-returns": "off",
-    "jsdoc/require-param": "off",
-
-    // Enable later, most objects don't follow the correct format
-    "jsdoc/check-param-names": "off",
-
-    // Add our own tags
-    "jsdoc/check-tag-names": [
-      "error",
-      {
-        definedTags: ["warning", "note", "knip", "context"],
-      },
-    ],
-
-    // Enable more on top of the recommendations
-    "jsdoc/require-hyphen-before-param-description": ["error", "never"],
-    "jsdoc/require-asterisk-prefix": "error",
-    "jsdoc/check-line-alignment": "error",
-    "jsdoc/check-indentation": "warn", // Can't outfix the existing errors,I'll leave it as warning
-    "jsdoc/no-bad-blocks": ["error", { ignore: ["jest-environment-options"] }],
-    "jsdoc/no-blank-blocks": "error",
-    "jsdoc/no-blank-block-descriptions": "error",
-
     "@shopify/react-hooks-strict-return": "error",
     "@shopify/prefer-module-scope-constants": "error",
     "@shopify/jest/no-snapshots": "warn",
