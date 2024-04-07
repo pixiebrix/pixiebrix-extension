2b82ca10f1b968bf44d0bc815bdc0bbc30d7044b
Federico Brigante <me@fregante.com>
2024-04-07 17:12:32 +0700

    Fix storyshot

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* global jest */
 import initStoryshots from "@storybook/addon-storyshots";
 
 // https://plainenglish.io/blog/storybook-snapshot-testing
