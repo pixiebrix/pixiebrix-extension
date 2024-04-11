5a680e07b2cbfb8be29ac526a15319bec2d346c3
Todd Schiller <todd.schiller@gmail.com>
2024-04-11 09:32:51 -0400

    Add commentary on MS Edge behavior and try to fix

--- end-to-end-tests/tests/sidebar/sidebarLinks.spec.ts
+++ end-to-end-tests/tests/sidebar/sidebarLinks.spec.ts
@@ -18,8 +18,15 @@
 import { test, expect } from "../../fixtures/extensionBase";
 import { ActivateModPage } from "../../pageObjects/extensionConsole/modsPage";
 // @ts-expect-error -- https://youtrack.jetbrains.com/issue/AQUA-711/Provide-a-run-configuration-for-Playwright-tests-in-specs-with-fixture-imports-only
-import { test as base } from "@playwright/test";
+import { type Page, test as base } from "@playwright/test";
 import { getSidebarPage } from "../../utils";
+
+async function openSidebar(page: Page, extensionId: string) {
+  // The mod contains a trigger to open the sidebar on h1. If the sidePanel is already open, it's a NOP
+  await page.click("h1");
+
+  return getSidebarPage(page, extensionId);
+}
 
 test("8206: clicking links doesn't crash browser", async ({
   page,
@@ -34,26 +41,37 @@ test("8206: clicking links doesn't crash browser", async ({
 
   await page.goto("/");
 
-  // The mod contains a trigger to open the sidebar on h1
-  await page.click("h1");
+  let sideBarPage = await openSidebar(page, extensionId);
 
-  const sideBarPage = await getSidebarPage(page, extensionId);
   await expect(
     sideBarPage.getByRole("heading", { name: "Sidebar Links" }),
   ).toBeVisible();
 
   await sideBarPage.getByTitle("Open Extension Console").click();
 
+  // On MS Edge, opening a new tab closes the sidebar, so we have to re-open it on the page after clicking each link
+  // See https://github.com/w3c/webextensions/issues/588. Alternatively, it might work to change the
+  // openAllLinksInPopups behavior to open in a new window instead of a new tab.
+  sideBarPage = await openSidebar(page, extensionId);
+
   await sideBarPage.getByRole("link", { name: "Markdown Text Link" }).click();
+
+  sideBarPage = await openSidebar(page, extensionId);
 
   // Is a react-bootstrap button link variant
   await sideBarPage.getByRole("button", { name: "Open a Tab Link" }).click();
 
+  sideBarPage = await openSidebar(page, extensionId);
+
   await sideBarPage.getByRole("link", { name: "HTML Renderer Link" }).click();
+
+  sideBarPage = await openSidebar(page, extensionId);
 
   await sideBarPage.getByRole("link", { name: "Embedded Form Link" }).click();
 
-  // FIXME: get these selectors working
+  // TODO: get these selectors working. Also, clicking link in IFrame will crash MS Edge until we fix the issue:
+  //   - https://github.com/pixiebrix/pixiebrix-extension/pull/8223
+  //   - https://github.com/pixiebrix/pixiebrix-extension/pull/8224
   // PixieBrix uses 2 layers of frames to get around the host page CSP. Test page has 2 layers
   // const pixiebrixFrame = sideBarPage.frameLocator("iframe").first();
   // const mainFrame = pixiebrixFrame.frameLocator("iframe").first();
