ffbd6aa5b726edfa4b9f91c6daf7669808ddcec3
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-04-23 18:02:45 -0700

    add mv2 logic for test

--- end-to-end-tests/tests/telemetry/errors.spec.ts
+++ end-to-end-tests/tests/telemetry/errors.spec.ts
@@ -5,31 +5,12 @@ import { type Page, test as base } from "@playwright/test";
 import { getBaseExtensionConsoleUrl } from "../../pageObjects/constants";
 import { MV } from "../../env";
 
-// TODO: Fix this test for MV2
-test.skip(
-  MV === "2",
-  "Temporarily skipping this test due to inconsistencies with error display in manifest versions",
-);
 test("can report application error to telemetry service", async ({
   page,
   context,
   extensionId,
 }) => {
   const errorServiceEndpoint = "https://browser-intake-datadoghq.com/api/v2/*";
-  const expectRequestBody = (request: Request) => {
-    expect(
-      request
-        ?.postData()
-        ?.split("\n")
-        .map((log) => JSON.parse(log)),
-    ).toContainEqual(
-      expect.objectContaining({
-        service: "pixiebrix-browser-extension",
-        manifestVersion: Number(MV),
-        error: expect.anything(),
-      }),
-    );
-  };
 
   await context.route(
     "https://app.pixiebrix.com/api/extensions/",
@@ -42,38 +23,51 @@ test("can report application error to telemetry service", async ({
     },
   );
 
-  await context.route(errorServiceEndpoint, async (route) => {
-    if (route.request().serviceWorker()) {
-      expectRequestBody(route.request());
-    }
-
-    return route.fulfill({
+  await context.route(errorServiceEndpoint, async (route) =>
+    route.fulfill({
       status: 202,
-    });
-  });
+    }),
+  );
 
   await page.goto(getBaseExtensionConsoleUrl(extensionId));
   await expect(page.getByText("Something went wrong.")).toBeVisible();
 
-  // Due to service worker limitations with the Datadog SDK, we need to report errors via an offscreen document
-  // (see https://github.com/pixiebrix/pixiebrix-extension/issues/8268). The offscreen document is created when
-  // the first error is reported, so we need to wait for it to be created before we can interact with it.
-  let offscreenPage: Page | undefined;
-  await expect(async () => {
-    offscreenPage = context
-      .pages()
-      .find((value) =>
-        value
-          .url()
-          .startsWith(`chrome-extension://${extensionId}/offscreen.html`),
-      );
+  let waitForRequest: Promise<Request | undefined>;
+  if (MV === "3") {
+    // Due to service worker limitations with the Datadog SDK, we need to report errors via an offscreen document
+    // (see https://github.com/pixiebrix/pixiebrix-extension/issues/8268). The offscreen document is created when
+    // the first error is reported, so we need to wait for it to be created before we can interact with it.
+    let offscreenPage: Page | undefined;
+    await expect(async () => {
+      offscreenPage = context
+        .pages()
+        .find((value) =>
+          value
+            .url()
+            .startsWith(`chrome-extension://${extensionId}/offscreen.html`),
+        );
 
-    expect(offscreenPage?.url()).toBeDefined();
-  }).toPass({ timeout: 5000 });
+      expect(offscreenPage?.url()).toBeDefined();
+    }).toPass({ timeout: 5000 });
+    waitForRequest = offscreenPage?.waitForRequest(errorServiceEndpoint);
+  } else {
+    const backgroundPage = context.backgroundPages()[0];
+    waitForRequest = backgroundPage?.waitForRequest(errorServiceEndpoint);
+  }
 
   // TODO: due to Datadog SDK implementation, it will take ~30 seconds for the
   //  request to be sent. We should figure out a way to induce the request to be sent sooner.
-  const request = await offscreenPage?.waitForRequest(errorServiceEndpoint);
+  const request = await waitForRequest;
+  const errorLogsJson = request
+    ?.postData()
+    ?.split("\n")
+    .map((log) => JSON.parse(log));
 
-  expectRequestBody(request);
+  expect(errorLogsJson).toContainEqual(
+    expect.objectContaining({
+      service: "pixiebrix-browser-extension",
+      manifestVersion: Number(MV),
+      error: expect.anything(),
+    }),
+  );
 });
