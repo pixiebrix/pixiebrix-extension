44a38a9768390b6eeeaba5b43cbdbb651667bd2d
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-04-25 14:14:14 -0700

    add envSetup test group

--- end-to-end-tests/env.ts
+++ end-to-end-tests/env.ts
@@ -24,8 +24,6 @@ const requiredEnvVariables = [
   "SERVICE_URL",
   "E2E_TEST_USER_EMAIL_UNAFFILIATED",
   "E2E_TEST_USER_PASSWORD_UNAFFILIATED",
-  "DEV_EVENT_TELEMETRY",
-  "DATADOG_CLIENT_TOKEN",
 ] as const;
 
 // It's not strictly required for the test run itself, but the extension manifest.json must have been built with
@@ -47,20 +45,24 @@ type OptionalEnvVariables = Record<
   string | undefined
 >;
 
-for (const key of requiredEnvVariables) {
-  // eslint-disable-next-line security/detect-object-injection -- key is a constant
-  if (process.env[key] === undefined) {
-    throw new Error(`Required environment variable is not configured: ${key}`);
-  }
+export const ensureRequiredEnvVariables = () => {
+  for (const key of requiredEnvVariables) {
+    // eslint-disable-next-line security/detect-object-injection -- key is a constant
+    if (process.env[key] === undefined) {
+      throw new Error(
+        `Required environment variable is not configured: ${key}`,
+      );
+    }
 
-  // eslint-disable-next-line security/detect-object-injection -- key is a constant
-  if (typeof process.env[key] !== "string") {
-    // For the time being we expect all of our requiredEnvVariables to be strings
-    throw new TypeError(
-      `Required environment variable is not configured: ${key}`,
-    );
+    // eslint-disable-next-line security/detect-object-injection -- key is a constant
+    if (typeof process.env[key] !== "string") {
+      // For the time being we expect all of our requiredEnvVariables to be strings
+      throw new TypeError(
+        `Required environment variable is not configured: ${key}`,
+      );
+    }
   }
-}
+};
 
 export const {
   SERVICE_URL,
