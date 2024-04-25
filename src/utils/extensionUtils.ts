75415e6032975d3518ed62dcca8155cd105d28c2
Graham Langford <grahamlangford87@gmail.com>
2024-04-25 12:08:40 -0500

    add documentation for getExtensionVersion

--- src/utils/extensionUtils.ts
+++ src/utils/extensionUtils.ts
@@ -67,7 +67,19 @@ export function getExtensionConsoleUrl(page?: string): string {
   return url.href;
 }
 
-// TODO: Add linting rule to prefer getExtensionVersion over browser.runtime.getManifest().version
+/**
+ * Gets the Extension version from the manifest and normalizes it to a valid semver string.
+ * @since 1.8.13, the Extension version is a four part format x.x.x.x
+ * This allows us to publish pre-release versions to the CWS, especially the BETA listing
+ * Each version published in CWS must have a unique version number
+ *
+ * @see manifest.mjs:getVersion()
+ *
+ * TODO: Add linting rule to prefer getExtensionVersion over browser.runtime.getManifest().version
+ *  @see https://github.com/pixiebrix/pixiebrix-extension/issues/8349
+ *
+ * @returns the version of the Extension in valid semver format (x.x.x)
+ */
 export function getExtensionVersion(): SemVerString {
   return normalizeSemVerString(browser.runtime.getManifest().version, {
     coerce: true,
