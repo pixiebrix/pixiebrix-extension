6df2f3265ce5d74742b24d4a7876c1baaa19fe2c
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-04-26 14:56:53 -0700

    introduce sidebar theme test

---
+++ end-to-end-tests/tests/runtime/sidebarPanelTheme.spec.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { test } from "../../fixtures/extensionBase";
+import { ActivateModPage } from "../../pageObjects/extensionConsole/modsPage";
+
+test("custom sidebar theme css file is applied to all levels of sidebar document", async ({
+  page,
+  extensionId,
+}) => {
+  const modId = "@pixies/testing/panel-theme";
+
+  const modActivationPage = new ActivateModPage(page, extensionId, modId);
+  await modActivationPage.goto();
+
+  await modActivationPage.clickActivateAndWaitForModsPageRedirect();
+});
