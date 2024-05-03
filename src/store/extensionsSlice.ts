caf039a9635fc99d22df44375b393b5161fc71b5
Graham Langford <grahamlangford87@gmail.com>
2024-05-03 10:15:30 -0500

    adds more fixes

--- src/store/extensionsSlice.ts
+++ src/store/extensionsSlice.ts
@@ -109,7 +109,9 @@ const extensionsSlice = createSlice({
     ) {
       const { extensionId, recipeMetadata } = payload;
       const extension = state.extensions.find((x) => x.id === extensionId);
-      extension._recipe = recipeMetadata;
+      if (extension != null) {
+        extension._recipe = recipeMetadata;
+      }
     },
 
     activateMod(
@@ -271,7 +273,7 @@ const extensionsSlice = createSlice({
       // eslint-disable-next-line security/detect-object-injection -- index is number
       state.extensions[index] = {
         ...state.extensions.at(index),
-        ...extensionUpdate,
+        ...(extensionUpdate as ActivatedModComponent),
       };
     },
 
@@ -281,7 +283,7 @@ const extensionsSlice = createSlice({
     ) {
       const metadata = action.payload;
       const recipeExtensions = state.extensions.filter(
-        (extension) => extension._recipe?.id === metadata.id,
+        (extension) => extension._recipe?.id === metadata?.id,
       );
       for (const extension of recipeExtensions) {
         extension._recipe = metadata;
