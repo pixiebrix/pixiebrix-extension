ad32c9e255d163aa6b68124af55408b211a173d3
Graham Langford <grahamlangford87@gmail.com>
2024-05-08 16:28:31 -0500

    fix test

--- src/errors/errorHelpers.test.tsx
+++ src/errors/errorHelpers.test.tsx
@@ -77,7 +77,7 @@ function testStore() {
 
 const TEST_MESSAGE = "Test message";
 
-function createUncaughtRejection(reason: string | Error) {
+function createUncaughtRejection(reason: string | Error | null) {
   // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors -- We want to test both
   const promise = Promise.reject(reason);
   // eslint-disable-next-line promise/prefer-await-to-then -- Test only
@@ -524,7 +524,7 @@ describe("selectErrorFromRejectionEvent", () => {
   it("handles PromiseRejectionEvent with null reason", () => {
     const errorEvent = new PromiseRejectionEvent(
       "error",
-      createUncaughtRejection("test"),
+      createUncaughtRejection(null),
     );
 
     const selectedError = selectErrorFromRejectionEvent(errorEvent);
