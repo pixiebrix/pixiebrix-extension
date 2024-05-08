9c4ce5d73442ad97be7a184e5e1b60dea29036b7
Eduardo <eduardo@pixiebrix.com>
2024-05-08 17:10:38 -0400

    Do not automatically close the sidebar when saving in the Page Editor

---
+++ end-to-end-tests/tests/regressions/doNotCloseSidebarOnPageEditorSave.spec.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { test, expect } from "../../fixtures/extensionBase";
+// @ts-expect-error -- https://youtrack.jetbrains.com/issue/AQUA-711/Provide-a-run-configuration-for-Playwright-tests-in-specs-with-fixture-imports-only
+import { type Page, test as base } from "@playwright/test";
+import { getSidebarPage } from "../../utils";
+
+test("#8104: Do not automatically close the sidebar when saving in the Page Editor", async ({
+  page,
+  newPageEditorPage,
+  extensionId,
+}) => {
+  await page.goto("/");
+  const pageEditorPage = await newPageEditorPage(page.url());
+
+  const modName = await pageEditorPage.addStarterBrick("Sidebar Panel");
+
+  const sidebar = await getSidebarPage(page, extensionId);
+  await expect(
+    sidebar.getByRole("tab", { name: "pbx.vercel.app side panel" }),
+  ).toBeVisible();
+
+  const updatedTabTitle = "Updated Tab Title";
+  await pageEditorPage.fillInBrickField("Tab Title", updatedTabTitle);
+  await pageEditorPage.getRenderPanelButton().click();
+  await expect(
+    sidebar.getByRole("tab", { name: updatedTabTitle }),
+  ).toBeVisible();
+
+  await pageEditorPage.saveStandaloneMod(modName);
+
+  await expect(
+    sidebar.getByRole("tab", { name: updatedTabTitle }),
+  ).toBeVisible();
+});
