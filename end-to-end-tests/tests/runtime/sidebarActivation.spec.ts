cd3c3d348551473e331fac1a6f5f7aef9cc5511d
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-06-05 09:12:45 -0700

    extract createActivationLink

--- end-to-end-tests/tests/runtime/sidebarActivation.spec.ts
+++ end-to-end-tests/tests/runtime/sidebarActivation.spec.ts
@@ -20,19 +20,63 @@ import { getSidebarPage } from "../../utils";
 // @ts-expect-error -- https://youtrack.jetbrains.com/issue/AQUA-711/Provide-a-run-configuration-for-Playwright-tests-in-specs-with-fixture-imports-only
 import { type Page, test as base } from "@playwright/test";
 
-test("sidebar mod activation initiates from activate url click", async ({
+/**
+ * Convert an existing link element on the page into a mod activation link
+ * @param page the page on which to run the script
+ * @param selector the selector of the element to convert
+ * @param nextUrl the url to which to redirect after activation
+ */
+const createActivationLink = async (
+  page: Page,
+  selector: string,
+  nextUrl: string,
+) => {
+  await page.evaluate(
+    ({ selector, nextUrl }: { selector: string; nextUrl: string }) => {
+      const link = document.querySelector(selector);
+      link.setAttribute(
+        "href",
+        `http://app.pixiebrix.com/activate?id=%40misha-holtz%2Freverse-gitlink&nextUrl=${nextUrl}`,
+      );
+    },
+    { selector, nextUrl },
+  );
+};
+
+test("initiates sidebar mod activation from activate url click", async ({
   page,
   extensionId,
 }) => {
   await page.goto("/bootstrap-5");
-  await page.evaluate(() => {
-    const validRedirectUrl = "https://www.pixiebrix.com/";
-    const link = document.querySelector("a[href*='#alpha']");
-    link.setAttribute(
-      "href",
-      `http://app.pixiebrix.com/activate?id=%40misha-holtz%2Freverse-gitlink&nextUrl=${validRedirectUrl}`,
-    );
+  await createActivationLink(
+    page,
+    "a[href*='#alpha']",
+    "https://www.pixiebrix.com/",
+  );
+  await page.getByText("Alpha").click();
+  await page.waitForURL("https://app.pixiebrix.com/*");
+  // Using page.waitforUrl below is not working as expected, so we use the following workaround
+  await expect(() => {
+    expect(page.url()).toBe("https://www.pixiebrix.com/");
+  }).toPass({
+    timeout: 5000,
   });
+
+  const sidebarPage = await getSidebarPage(page, extensionId);
+  const activateTab = sidebarPage.getByText("Activating");
+  const modName = sidebarPage.getByText("Reverse GitLink");
+
+  await expect(activateTab).toBeVisible();
+  await expect(modName).toBeVisible();
+});
+
+test("does not redirect to non-pixiebrix domain", async ({
+  page,
+  extensionId,
+}) => {
+  const invalidRedirectUrl = "https://pbx.vercel.app/";
+  await page.goto("/bootstrap-5");
+  await createActivationLink(page, "a[href*='#alpha']", invalidRedirectUrl);
   await page.getByText("Alpha").click();
   await page.waitForURL("https://app.pixiebrix.com/*");
   // Using page.waitforUrl below is not working as expected, so we use the following workaround
