5cd6c35212311278409bb6593f34a265bb4c09ee
Eduardo <eduardo@pixiebrix.com>
2024-06-12 20:34:54 -0400

    fix renaming

--- end-to-end-tests/fixtures/pageContext.ts
+++ end-to-end-tests/fixtures/pageContext.ts
@@ -15,11 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-  test as base,
-  mergeTests,
-  type BrowserContext,
-} from "@playwright/test";
+import { test as base, type BrowserContext } from "@playwright/test";
 import path from "node:path";
 import fs from "node:fs/promises";
 import {
@@ -28,7 +24,6 @@ import {
   launchPersistentContextWithExtension,
 } from "./utils";
 import { ModsPage } from "../pageObjects/extensionConsole/modsPage";
-import { test as envSetup } from "./environmentCheck";
 import { PageEditorPage } from "../pageObjects/pageEditorPage";
 
 // This environment variable is used to attach the browser sidepanel window that opens automatically to Playwright.
@@ -39,99 +34,94 @@ process.env.PW_CHROMIUM_ATTACH_TO_OTHER = "1";
 // See https://playwright.dev/docs/service-workers-experimental
 process.env.PW_EXPERIMENTAL_SERVICE_WORKER_NETWORK_EVENTS = "1";
 
-export const test = mergeTests(
-  envSetup,
-  base.extend<
-    {
-      context: BrowserContext;
-      extensionId: string;
-      chromiumChannel: "chrome" | "msedge";
-      newPageEditorPage: (urlToConnectTo: string) => Promise<PageEditorPage>;
-    },
-    {
-      checkRequiredEnvironmentVariables: () => void;
+export const test = base.extend<
+  {
+    context: BrowserContext;
+    extensionId: string;
+    chromiumChannel: "chrome" | "msedge";
+    newPageEditorPage: (urlToConnectTo: string) => Promise<PageEditorPage>;
+  },
+  {
+    checkRequiredEnvironmentVariables: () => void;
+  }
+>({
+  chromiumChannel: ["chrome", { option: true }],
+  async context({ chromiumChannel }, use) {
+    let authSetupProfileDirectory: string;
+
+    try {
+      authSetupProfileDirectory = await fs.readFile(
+        getAuthProfilePathFile(chromiumChannel),
+        "utf8",
+      );
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        "code" in error &&
+        error.code === "ENOENT"
+      ) {
+        console.log(
+          "No auth setup profile found. Make sure that the `auth.setup` project has been run first to create the " +
+            "profile. (If using UI mode, make sure that the chromeSetup and/or the edgeSetup projects are not filtered out)",
+        );
+      }
+
+      throw error;
     }
-  >({
-    chromiumChannel: ["chrome", { option: true }],
-    async context({ chromiumChannel }, use) {
-      let authSetupProfileDirectory: string;
 
-      try {
-        authSetupProfileDirectory = await fs.readFile(
-          getAuthProfilePathFile(chromiumChannel),
-          "utf8",
-        );
-      } catch (error) {
-        if (
-          error instanceof Error &&
-          "code" in error &&
-          error.code === "ENOENT"
-        ) {
-          console.log(
-            "No auth setup profile found. Make sure that the `auth.setup` project has been run first to create the " +
-              "profile. (If using UI mode, make sure that the chromeSetup and/or the edgeSetup projects are not filtered out)",
-          );
-        }
+    const temporaryProfileDirectory = await fs.mkdtemp(
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion,@typescript-eslint/no-non-null-assertion -- checked above
+      path.join(path.dirname(authSetupProfileDirectory!), "e2e-test-"),
+    );
+    // Copy the auth setup profile to a new temp directory to avoid modifying the original auth profile
+    await fs.cp(authSetupProfileDirectory, temporaryProfileDirectory, {
+      recursive: true,
+    });
 
-        throw error;
-      }
+    const context = await launchPersistentContextWithExtension(
+      chromiumChannel,
+      temporaryProfileDirectory,
+    );
 
-      const temporaryProfileDirectory = await fs.mkdtemp(
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion,@typescript-eslint/no-non-null-assertion -- checked above
-        path.join(path.dirname(authSetupProfileDirectory!), "e2e-test-"),
+    await use(context);
+    await context.close();
+  },
+  async page({ context, extensionId }, use) {
+    // Re-use the initial context page if it exists
+    const page = context.pages()[0] || (await context.newPage());
+
+    // Start off test from the extension console, and ensure it is done loading
+    const modsPage = new ModsPage(page, extensionId);
+    await modsPage.goto();
+
+    await use(page);
+    // The page is closed by the context fixture `.close` cleanup step
+  },
+  /**
+   * Create a new page editor instance for the given URL. Cleans up any saved standalone mods after the test.
+   * TODO: support "packaged" mod cleanup
+   */
+  async newPageEditorPage({ context, extensionId }, use) {
+    const pageEditorPages: PageEditorPage[] = [];
+    await use(async (urlToConnectTo: string) => {
+      const newPage = await context.newPage();
+      const newPageEditorPage = new PageEditorPage(
+        newPage,
+        urlToConnectTo,
+        extensionId,
       );
-      // Copy the auth setup profile to a new temp directory to avoid modifying the original auth profile
-      await fs.cp(authSetupProfileDirectory, temporaryProfileDirectory, {
-        recursive: true,
-      });
+      await newPageEditorPage.goto();
+      pageEditorPages.push(newPageEditorPage);
+      return newPageEditorPage;
+    });
 
-      const context = await launchPersistentContextWithExtension(
-        chromiumChannel,
-        temporaryProfileDirectory,
-      );
-
-      await use(context);
-      await context.close();
-    },
-    async page({ context, extensionId }, use) {
-      // Re-use the initial context page if it exists
-      const page = context.pages()[0] || (await context.newPage());
-
-      // Start off test from the extension console, and ensure it is done loading
-      const modsPage = new ModsPage(page, extensionId);
-      await modsPage.goto();
-
-      await use(page);
-      // The page is closed by the context fixture `.close` cleanup step
-    },
-    /**
-     * Create a new page editor instance for the given URL. Cleans up any saved standalone mods after the test.
-     * TODO: support "packaged" mod cleanup
-     */
-    async newPageEditorPage({ context, extensionId }, use) {
-      const pageEditorPages: PageEditorPage[] = [];
-      await use(async (urlToConnectTo: string) => {
-        const newPage = await context.newPage();
-        const newPageEditorPage = new PageEditorPage(
-          newPage,
-          urlToConnectTo,
-          extensionId,
-        );
-        await newPageEditorPage.goto();
-        pageEditorPages.push(newPageEditorPage);
-        return newPageEditorPage;
-      });
-
-      for (const page of pageEditorPages) {
-        // eslint-disable-next-line no-await-in-loop -- optimization via parallelization not relevant here
-        await page.cleanup();
-      }
-    },
-    async extensionId({ context }, use) {
-      const extensionId = await getExtensionId(context);
-      await use(extensionId);
-    },
-  }),
-);
-
-export const { expect } = test;
+    for (const page of pageEditorPages) {
+      // eslint-disable-next-line no-await-in-loop -- optimization via parallelization not relevant here
+      await page.cleanup();
+    }
+  },
+  async extensionId({ context }, use) {
+    const extensionId = await getExtensionId(context);
+    await use(extensionId);
+  },
+});
