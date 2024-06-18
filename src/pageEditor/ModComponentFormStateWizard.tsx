9177cffd58c8ad6a0884405955bab1bf9160b97b
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-06-17 18:47:27 -0700

    fix styles import in ModComponentFormStateWizard

--- src/pageEditor/ModComponentFormStateWizard.tsx
+++ src/pageEditor/ModComponentFormStateWizard.tsx
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import styles from "./ElementWizard.module.scss";
+import styles from "./ModComponentFormStateWizard.module.scss";
 
 import React, { useState } from "react";
 import { useFormikContext } from "formik";
