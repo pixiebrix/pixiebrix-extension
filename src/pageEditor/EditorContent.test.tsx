52e6c3173502371f497cfbe3f0cffe769172e877
Graham Langford <grahamlangford87@gmail.com>
2024-06-20 16:11:56 -0500

    fix test label

--- src/pageEditor/EditorContent.test.tsx
+++ src/pageEditor/EditorContent.test.tsx
@@ -44,7 +44,7 @@ jest.mock("@/pageEditor/hooks/useCurrentInspectedUrl");
 jest.mock("@/contentScript/messenger/api");
 
 describe("error alerting in the UI", () => {
-  test("shows error when checkAvailableDraftElements fails", async () => {
+  test("shows error when checkAvailableDraftModComponents fails", async () => {
     const message = "testing error";
     jest.mocked(getCurrentInspectedURL).mockImplementation(() => {
       throw new Error(message);
