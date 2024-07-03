3ab86b4acee5112ce28b4df5a0cab2d393bfbdd5
Eduardo Fungairino <eduardo@pixiebrix.com>
2024-07-03 16:36:58 -0400

    #8581: More page editor poms (#8748)
    * brick action panel
    
    * adds more page editor poms
    
    * fix type import

---
+++ end-to-end-tests/pageObjects/pageEditor/brickActionsPanel.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { BasePageObject } from "../basePageObject";
+import { ModifiesModState } from "./utils";
+
+export class BrickActionsPanel extends BasePageObject {
+  getAddBrickButton(n: number) {
+    return this.getByTestId(/icon-button-.*-add-brick/).nth(n);
+  }
+
+  @ModifiesModState
+  async addBrick(brickName: string, { index = 0 }: { index?: number } = {}) {
+    await this.getAddBrickButton(index).click();
+
+    // Add brick modal
+    await this.page.getByTestId("tag-search-input").fill(brickName);
+    await this.page.getByRole("button", { name: brickName }).first().click();
+
+    await this.page.getByRole("button", { name: "Add brick" }).click();
+  }
+}
