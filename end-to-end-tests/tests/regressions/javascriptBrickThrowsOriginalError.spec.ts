2b2fed194ba0c316723a10ab8522fa7c9e8e225b
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-07-12 16:41:26 -0700

    add test base

---
+++ end-to-end-tests/tests/regressions/javascriptBrickThrowsOriginalError.spec.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { getSidebarPage, runModViaQuickBar } from "../../utils";
+import { test, expect } from "../../fixtures/testBase";
+import { ActivateModPage } from "../../pageObjects/extensionConsole/modsPage";
+
+// @ts-expect-error -- https://youtrack.jetbrains.com/issue/AQUA-711/Provide-a-run-configuration-for-Playwright-tests-in-specs-with-fixture-imports-only
+import { type Page, test as base } from "@playwright/test";
+
+test("#8821: ensure Javascript script errors are thrown during brick runtime", async ({
+  page,
+  extensionId,
+  newPageEditorPage,
+}) => {
+  const modId = "@e2e-testing/8821-repro";
+  const modActivationPage = new ActivateModPage(page, extensionId, modId);
+  await modActivationPage.goto();
+  await modActivationPage.clickActivateAndWaitForModsPageRedirect();
+
+  await page.goto("/bootstrap-5");
+  const pageEditorPage = await newPageEditorPage(page.url());
+  await pageEditorPage.modListingPanel
+    .getModListItemByName("8821 Repro")
+    .click();
+
+  await pageEditorPage.modListingPanel
+    .getModStarterBrick("8821 Repro", "8821 Repro")
+    .select();
+
+  await pageEditorPage.getByText("Run Trigger").click();
+});
