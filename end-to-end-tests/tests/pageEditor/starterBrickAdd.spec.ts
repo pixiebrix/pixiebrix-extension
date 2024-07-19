6825b6e26557fa81e8b5d881c1b97adb0131aa6f
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-07-19 12:31:31 -0700

    add several more starter brick tests

--- end-to-end-tests/tests/pageEditor/starterBrickAdd.spec.ts
+++ end-to-end-tests/tests/pageEditor/starterBrickAdd.spec.ts
@@ -19,6 +19,7 @@ import { test, expect } from "../../fixtures/testBase";
 
 // @ts-expect-error -- https://youtrack.jetbrains.com/issue/AQUA-711/Provide-a-run-configuration-for-Playwright-tests-in-specs-with-fixture-imports-only
 import { type Page, test as base } from "@playwright/test";
+import { getSidebarPage } from "../../utils";
 
 test("Add new Button starter brick", async ({ page, newPageEditorPage }) => {
   await page.goto("/");
@@ -34,4 +35,62 @@ test("Add new Button starter brick", async ({ page, newPageEditorPage }) => {
       name: "Name",
     }),
   ).toHaveValue("My pbx.vercel.app button");
+});
+
+test("Add new Context Menu starter brick", async ({
+  page,
+  newPageEditorPage,
+}) => {
+  await page.goto("/");
+  const pageEditorPage = await newPageEditorPage(page.url());
+  await pageEditorPage.modListingPanel.addStarterBrick("Context Menu");
+  const brickPipeline =
+    await pageEditorPage.brickActionsPanel.getBricksInPipeline();
+  expect(brickPipeline).toHaveLength(1);
+  await expect(brickPipeline[0]).toContainText("Context Menu");
+  await expect(
+    pageEditorPage.brickConfigurationPanel.getByRole("textbox", {
+      name: "Name",
+    }),
+  ).toHaveValue("Context menu item");
+});
+
+test("Add new Quick Bar Action starter brick", async ({
+  page,
+  newPageEditorPage,
+}) => {
+  await page.goto("/");
+  const pageEditorPage = await newPageEditorPage(page.url());
+  await pageEditorPage.modListingPanel.addStarterBrick("Quick Bar Action");
+  const brickPipeline =
+    await pageEditorPage.brickActionsPanel.getBricksInPipeline();
+  expect(brickPipeline).toHaveLength(1);
+  await expect(brickPipeline[0]).toContainText("Quick Bar Action");
+  await expect(
+    pageEditorPage.brickConfigurationPanel.getByRole("textbox", {
+      name: "Name",
+    }),
+  ).toHaveValue("Quick Bar item");
+});
+
+test("Add new Sidebar Panel starter brick", async ({
+  page,
+  newPageEditorPage,
+  extensionId,
+}) => {
+  await page.goto("/");
+  const pageEditorPage = await newPageEditorPage(page.url());
+  await pageEditorPage.modListingPanel.addStarterBrick("Sidebar Panel");
+  const brickPipeline =
+    await pageEditorPage.brickActionsPanel.getBricksInPipeline();
+  expect(brickPipeline).toHaveLength(2);
+  await expect(brickPipeline[0]).toContainText("Sidebar Panel");
+  await expect(
+    pageEditorPage.brickConfigurationPanel.getByRole("textbox", {
+      name: "Name",
+    }),
+  ).toHaveValue("Sidebar Panel");
+
+  const sidebarPage = await getSidebarPage(page, extensionId);
+  await expect(sidebarPage.getByText("Example Document")).toBeVisible();
 });
