76f81eaecaa3f655e1083622b68a965c257be15d
Eduardo <eduardo@pixiebrix.com>
2024-07-26 16:58:09 -0400

    fix this thing

--- src/contentScript/contentScriptCore.ts
+++ src/contentScript/contentScriptCore.ts
@@ -44,6 +44,7 @@ import { markDocumentAsFocusableByUser } from "@/utils/focusTracker";
 import contentScriptPlatform from "@/contentScript/contentScriptPlatform";
 import axios from "axios";
 import { initDeferredLoginController } from "@/contentScript/integrations/deferredLoginController";
+import { debounce } from "lodash";
 
 setPlatform(contentScriptPlatform);
 
@@ -75,14 +76,36 @@ onUncaughtError((error) => {
  * See https://issues.chromium.org/issues/355256366
  */
 const ensureSandboxedSrcdocIframeInjection = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- selecting iframes with srcdoc and sandbox messes up type inference for iframe elements
-  for (const iframe of document.querySelectorAll(
-    "iframe[srcdoc][sandbox]",
-  ) as NodeListOf<HTMLIFrameElement>) {
-    iframe.removeAttribute("sandbox");
-    // eslint-disable-next-line no-self-assign -- force the iframe to reload
-    iframe.srcdoc = iframe.srcdoc;
-  }
+  const applyFixToIframes = () => {
+    // Use the faster querySelector first to check if there are any iframes that need fixing
+    if (!document.querySelector("iframe[srcdoc][sandbox]")) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- selecting iframes with srcdoc and sandbox messes up type inference for iframe elements
+    const iframes = document.querySelectorAll(
+      "iframe[srcdoc][sandbox]",
+    ) as NodeListOf<HTMLIFrameElement>;
+    if (iframes.length > 0) {
+      for (const iframe of iframes) {
+        iframe.removeAttribute("sandbox");
+        // eslint-disable-next-line no-self-assign -- force the iframe to reload
+        iframe.srcdoc = iframe.srcdoc;
+      }
+    }
+  };
+
+  const domObserver = new MutationObserver(
+    debounce(applyFixToIframes, 800, {
+      leading: true,
+      maxWait: 1600,
+    }),
+  );
+
+  domObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 };
 
 export async function init(): Promise<void> {
