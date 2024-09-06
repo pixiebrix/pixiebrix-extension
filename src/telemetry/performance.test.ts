0887c243a258f6c04e64ef18f6b6df986e47d8fc
Eduardo <eduardo@pixiebrix.com>
2024-09-06 10:28:27 -0400

    force session replay recording in sidebar welcome page

--- src/telemetry/performance.test.ts
+++ src/telemetry/performance.test.ts
@@ -45,48 +45,85 @@ describe("initPerformanceMonitoring", () => {
     expect(datadogRum.init).not.toHaveBeenCalled();
   });
 
-  it("should initialize performance monitoring", async () => {
-    jest.mocked(getDNT).mockResolvedValue(false);
-    jest.mocked(flagOn).mockResolvedValue(true);
-    process.env.DATADOG_APPLICATION_ID = "applicationId";
-    process.env.DATADOG_CLIENT_TOKEN = "clientToken";
-    process.env.ENVIRONMENT = "local";
-    jest.mocked(getBaseURL).mockResolvedValue("https://example.com");
-    browser.runtime.getManifest = jest
-      .fn()
-      .mockReturnValue({ version_name: "1.8.8-alpha+293128" });
-    jest.mocked(readAuthData).mockResolvedValue({
-      user: "634b1b49-4382-4292-87f4-d25c6a1db3d7",
-      organizationId: "24a7c934-a531-49d8-a15d-cdf0baa54146",
-    } as any);
-
-    await initPerformanceMonitoring();
-
-    expect(datadogRum.init).toHaveBeenCalledWith({
-      applicationId: "applicationId",
-      clientToken: "clientToken",
-      site: "datadoghq.com",
-      service: "pixiebrix-browser-extension",
-      env: "local",
-      version: expect.any(String),
-      sessionSampleRate: 100,
-      sessionReplaySampleRate: 20,
-      trackUserInteractions: true,
-      trackResources: true,
-      trackLongTasks: true,
-      defaultPrivacyLevel: "mask",
-      allowedTracingUrls: ["https://example.com"],
-      allowFallbackToLocalStorage: true,
+  describe("given the required initialization conditions", () => {
+    beforeEach(() => {
+      jest.mocked(getDNT).mockResolvedValue(false);
+      jest.mocked(flagOn).mockResolvedValue(true);
+      process.env.DATADOG_APPLICATION_ID = "applicationId";
+      process.env.DATADOG_CLIENT_TOKEN = "clientToken";
+      process.env.ENVIRONMENT = "local";
+      jest.mocked(getBaseURL).mockResolvedValue("https://example.com");
+      browser.runtime.getManifest = jest
+        .fn()
+        .mockReturnValue({ version_name: "1.8.8-alpha+293128" });
+      jest.mocked(readAuthData).mockResolvedValue({
+        user: "634b1b49-4382-4292-87f4-d25c6a1db3d7",
+        organizationId: "24a7c934-a531-49d8-a15d-cdf0baa54146",
+      } as any);
     });
-    expect(datadogRum.setGlobalContextProperty).toHaveBeenCalledWith(
-      "code_version",
-      process.env.SOURCE_VERSION,
-    );
-    expect(datadogRum.setUser).toHaveBeenCalledWith({
-      email: undefined,
-      id: "634b1b49-4382-4292-87f4-d25c6a1db3d7",
-      organizationId: "24a7c934-a531-49d8-a15d-cdf0baa54146",
+
+    it("should initialize performance monitoring", async () => {
+      await initPerformanceMonitoring({
+        additionalGlobalContext: { connectedTabUrl: "google.com" },
+      });
+
+      expect(datadogRum.init).toHaveBeenCalledWith({
+        applicationId: "applicationId",
+        clientToken: "clientToken",
+        site: "datadoghq.com",
+        service: "pixiebrix-browser-extension",
+        env: "local",
+        version: expect.any(String),
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 20,
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        defaultPrivacyLevel: "mask",
+        allowedTracingUrls: ["https://example.com"],
+        allowFallbackToLocalStorage: true,
+      });
+      expect(datadogRum.setGlobalContext).toHaveBeenCalledWith({
+        code_version: process.env.SOURCE_VERSION,
+        connectedTabUrl: "google.com",
+      });
+      expect(datadogRum.setUser).toHaveBeenCalledWith({
+        email: undefined,
+        id: "634b1b49-4382-4292-87f4-d25c6a1db3d7",
+        organizationId: "24a7c934-a531-49d8-a15d-cdf0baa54146",
+      });
     });
-    expect(datadogRum.startSessionReplayRecording).toHaveBeenCalled();
+
+    it("should force session replay recording when sessionSampleRate is set to 100", async () => {
+      await initPerformanceMonitoring({ sessionReplaySampleRate: 100 });
+
+      expect(datadogRum.init).toHaveBeenCalledWith({
+        applicationId: "applicationId",
+        clientToken: "clientToken",
+        site: "datadoghq.com",
+        service: "pixiebrix-browser-extension",
+        env: "local",
+        version: expect.any(String),
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 100,
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        defaultPrivacyLevel: "mask",
+        allowedTracingUrls: ["https://example.com"],
+        allowFallbackToLocalStorage: true,
+      });
+      expect(datadogRum.setGlobalContext).toHaveBeenCalledWith({
+        code_version: process.env.SOURCE_VERSION,
+      });
+      expect(datadogRum.setUser).toHaveBeenCalledWith({
+        email: undefined,
+        id: "634b1b49-4382-4292-87f4-d25c6a1db3d7",
+        organizationId: "24a7c934-a531-49d8-a15d-cdf0baa54146",
+      });
+      expect(datadogRum.startSessionReplayRecording).toHaveBeenCalledWith({
+        force: true,
+      });
+    });
   });
 });
