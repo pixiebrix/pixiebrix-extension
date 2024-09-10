975f401ae158b52a68fbb5aab495dea13239e9fe
Ben Loe <below413@gmail.com>
2024-09-10 09:32:59 -0400

    Bug - Fix click propagation on ellipsis menus (#9121)
    * fix bug and add tests
    
    * userEvent
    
    * use basic div
    
    * wrap the entire menu
    
    * update snapshots
    
    ---------
    
    Co-authored-by: Ben Loe <ben@pixiebrix.com>

--- src/components/ellipsisMenu/EllipsisMenu.tsx
+++ src/components/ellipsisMenu/EllipsisMenu.tsx
@@ -15,9 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { type MutableRefObject, type ReactElement } from "react";
-import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu";
-import React from "react";
+import React, { type MutableRefObject, type ReactElement } from "react";
+import { Menu, MenuButton, MenuItem, SubMenu } from "@szhsin/react-menu";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "@szhsin/react-menu/dist/index.css";
@@ -107,7 +106,6 @@ const getMenuItemComponent = (item: EllipsisMenuItem): ReactElement => {
     ) : (
       item.title
     );
-
     return (
       <SubMenu label={label} key={item.title}>
         {item.submenu.map((subItem) => getMenuItemComponent(subItem))}
@@ -134,28 +132,35 @@ const EllipsisMenu: React.FunctionComponent<EllipsisMenuProps> = ({
   portal,
   classNames,
 }) => (
-  <Menu
-    align="end"
-    direction="bottom"
-    gap={4}
-    boundingBoxRef={boundingBoxRef}
-    portal={portal}
-    className={classNames?.menu}
-    menuButton={
-      <MenuButton
-        aria-label={ariaLabel}
-        className={cx(styles.button, classNames?.menuButton)}
-        data-testid="ellipsis-menu-button"
-        onClick={(event) => {
-          event.stopPropagation();
-        }}
-      >
-        <FontAwesomeIcon icon={faEllipsisV} />
-      </MenuButton>
-    }
+  // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions,no-restricted-syntax -- Just stopping propagation, don't need accessibility
+  <div
+    onClick={(event) => {
+      event.stopPropagation();
+    }}
   >
-    {items.filter((x) => !x.hide).map((item) => getMenuItemComponent(item))}
-  </Menu>
+    <Menu
+      align="end"
+      direction="bottom"
+      gap={4}
+      boundingBoxRef={boundingBoxRef}
+      portal={portal}
+      className={classNames?.menu}
+      menuButton={
+        <MenuButton
+          aria-label={ariaLabel}
+          className={cx(styles.button, classNames?.menuButton)}
+          data-testid="ellipsis-menu-button"
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <FontAwesomeIcon icon={faEllipsisV} />
+        </MenuButton>
+      }
+    >
+      {items.filter((x) => !x.hide).map((item) => getMenuItemComponent(item))}
+    </Menu>
+  </div>
 );
 
 export default EllipsisMenu;
