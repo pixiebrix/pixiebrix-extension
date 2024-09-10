2429cb8ed490ef8d04ffdb729b4a63b134a9bfdf
Graham Langford <grahamlangford87@gmail.com>
2024-09-10 15:21:38 -0500

    implement TeamTrialBanner

---
+++ src/extensionConsole/pages/TeamTrialBanner.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Banner from "@/components/banner/Banner";
+import useTeamTrialStatus, {
+  TeamTrialStatus,
+} from "@/extensionConsole/pages/useTeamTrialStatus";
+import React from "react";
+
+const TrialCallToActionLink = () => (
+  <a
+    href="https://calendly.com/pixiebrix-mike/20min"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    here.
+  </a>
+);
+
+const TeamTrialBanner: React.FunctionComponent = () => {
+  const teamTrialStatus = useTeamTrialStatus();
+
+  if (!teamTrialStatus) {
+    return null;
+  }
+
+  return (
+    <Banner
+      variant={
+        teamTrialStatus === TeamTrialStatus.EXPIRED ? "danger" : "warning"
+      }
+    >
+      Your team trial is{" "}
+      {teamTrialStatus === TeamTrialStatus.EXPIRED
+        ? "expired!"
+        : "in progress."}{" "}
+      Talk to an onboarding specialist now to upgrade. Schedule a time{" "}
+      <TrialCallToActionLink />
+    </Banner>
+  );
+};
+
+export default TeamTrialBanner;
