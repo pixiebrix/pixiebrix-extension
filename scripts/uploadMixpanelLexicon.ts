86645192fb50935e1eafe49a4d9c4407c7da34ea
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-09-24 14:28:19 -0700

    Add Mixpanel Lexicon to version control (#9149)
    * add lexicon.ts
    
    * add LexiconTags constant
    
    * remove support for properties - future work
    
    * fix JSONSchema type error
    
    * add comments
    
    * default displayName to null
    
    * fix lexicon schema request
    
    * introduce upload-mixpanel-lexicon workflow file
    
    * refactor rename uploadMixpanelLexicon
    
    * fix workflow file
    
    * add gh ticket to comment
    
    * add ref to comment
    
    * add lexicon.ts and uploadMixpanelLexicon.ts to knip config
    
    * push temporary pull_request trigger to workflow to test job
    
    * add pull_request to github workflow temporarily
    
    * move env up in declaration
    
    * add workflow_dispatch
    
    * move env under Upload Lexicon step
    
    * fix username constant name
    
    * remove pull_request from workflow

---
+++ scripts/uploadMixpanelLexicon.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import axios from "axios";
+import {
+  lexicon,
+  transformLexiconMapToRequestSchema,
+} from "../src/telemetry/lexicon";
+
+async function uploadMixpanelLexicon() {
+  const transformedLexicon = transformLexiconMapToRequestSchema(lexicon);
+
+  const {
+    MIXPANEL_PROJECT_ID,
+    MIXPANEL_SERVICE_ACCOUNT_USERNAME,
+    MIXPANEL_SERVICE_ACCOUNT_SECRET,
+  } = process.env;
+
+  for (const [key, value] of Object.entries({
+    MIXPANEL_PROJECT_ID,
+    MIXPANEL_SERVICE_ACCOUNT_USERNAME,
+    MIXPANEL_SERVICE_ACCOUNT_SECRET,
+  })) {
+    if (!value) {
+      throw new Error(`${key} is required`);
+    }
+  }
+
+  try {
+    const response = await axios.post(
+      `https://mixpanel.com/api/app/projects/${MIXPANEL_PROJECT_ID}/schemas`,
+      transformedLexicon,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${Buffer.from(
+            `${MIXPANEL_SERVICE_ACCOUNT_USERNAME}:${MIXPANEL_SERVICE_ACCOUNT_SECRET}`,
+          ).toString("base64")}`,
+        },
+      },
+    );
+
+    console.log("Lexicon uploaded successfully:", response.data);
+  } catch (error) {
+    console.error("Error uploading lexicon:", error);
+  }
+}
+
+void uploadMixpanelLexicon();
