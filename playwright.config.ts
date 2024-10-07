023b84884efc782cbf16cd7e35b488f67a56609f
Eduardo <eduardo@pixiebrix.com>
2024-10-07 16:02:30 -0400

    collect traces on error always

--- playwright.config.ts
+++ playwright.config.ts
@@ -92,7 +92,8 @@ export default defineConfig<{ chromiumChannel: string }>({
     baseURL: "https://pbx.vercel.app",
 
     /* Collect trace when retrying the failed test in CI, and always on failure when running locally. See https://playwright.dev/docs/trace-viewer */
-    trace: CI ? "on-first-retry" : "retain-on-failure",
+    // Temporarily always collect trace to debug flakiness
+    trace: "retain-on-failure",
 
     /* Set the default timeout for actions such as `click` */
     actionTimeout: 5000,
