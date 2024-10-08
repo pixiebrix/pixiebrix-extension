b02d68f35ced7dba217489eacb2964ad85e0e2e6
Todd Schiller <todd.schiller@gmail.com>
2024-10-08 19:04:18 -0400

    Remove unused import

--- src/pageEditor/utils.ts
+++ src/pageEditor/utils.ts
@@ -35,7 +35,6 @@ import {
   type ModMetadata,
 } from "@/types/modComponentTypes";
 import { type UUID } from "@/types/stringTypes";
-import { type RegistryId } from "@/types/registryTypes";
 import { type Brick } from "@/types/brickTypes";
 import { sortedFields } from "@/components/fields/schemaFields/schemaFieldUtils";
 import { castTextLiteralOrThrow } from "@/utils/expressionUtils";
