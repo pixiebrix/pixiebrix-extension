67b41438887f72325e984cc6c95f28a90ef565bb
Eduardo <eduardo@pixiebrix.com>
2024-10-10 15:09:47 -0400

    fix setup flakiness

--- end-to-end-tests/setup/utils.ts
+++ end-to-end-tests/setup/utils.ts
@@ -16,40 +16,31 @@
  */
 
 import { type BrowserContext, type Page, expect } from "@playwright/test";
-import { DEFAULT_TIMEOUT } from "../../playwright.config";
 
 export const openExtensionConsoleFromAdmin = async (
   adminPage: Page,
   context: BrowserContext,
   userName: string,
 ): Promise<Page> => {
-  let extensionConsolePage: Page | undefined;
-  // Sometimes get the following error "Error: Could not establish connection. Receiving end does not exist."
-  // when trying to click on the "Open Extension Console" button. This happens when the Extension has not fully
-  // initialized to be able to receive messages via the external messenger api, which happens when the Extension
-  // reloads after linking. Thus, we wrap the following with an `expect.toPass` retry.
-  await expect(async () => {
-    const newPagePromise = context.waitForEvent("page", { timeout: 5000 });
+  const newPagePromise = context.waitForEvent("page", { timeout: 5000 });
 
-    await adminPage
-      .locator("button")
-      .filter({ hasText: "Open Extension Console" })
-      .click();
+  await adminPage
+    .locator("button")
+    .filter({ hasText: "Open Extension Console" })
+    .click();
 
-    extensionConsolePage = await newPagePromise;
-    await extensionConsolePage.waitForURL(/\/options\.html#\/$/);
+  const extensionConsolePage = await newPagePromise;
+  await extensionConsolePage.waitForURL(/\/options\.html#\/$/);
 
-    await expect(extensionConsolePage.locator("#container")).toContainText(
-      "Extension Console",
-      {
-        timeout: 5000,
-      },
-    );
-    await expect(extensionConsolePage.getByText(userName)).toBeVisible({
-      timeout: 5000,
-    });
-  }).toPass({ timeout: DEFAULT_TIMEOUT });
+  await expect(extensionConsolePage.locator("#container")).toContainText(
+    "Extension Console",
+    {
+      timeout: 10_000,
+    },
+  );
+  await expect(extensionConsolePage.getByText(userName)).toBeVisible({
+    timeout: 5000,
+  });
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- extensionConsolePage is defined
-  return extensionConsolePage!;
+  return extensionConsolePage;
 };
