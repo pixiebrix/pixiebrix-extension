55b9e5866b4a2a837b969b328c43b8dd4670934f
Eduardo <eduardo@pixiebrix.com>
2024-10-16 16:57:55 -0400

    debugging flakiness

--- end-to-end-tests/tests/pageEditor/copyMod.spec.ts
+++ end-to-end-tests/tests/pageEditor/copyMod.spec.ts
@@ -31,7 +31,7 @@ import {
 } from "end-to-end-tests/utils";
 import { VALID_UUID_REGEX } from "@/types/stringTypes";
 
-test("copying a mod that uses the PixieBrix API is copied correctly", async ({
+test.only("copying a mod that uses the PixieBrix API is copied correctly", async ({
   page,
   newPageEditorPage,
   extensionId,
