3c8aaa4d1be7e04ac56b467037060298eb326542
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-10-23 17:06:49 -0700

    add description for hidden property on LexiconEventEntry

--- src/telemetry/lexicon.ts
+++ src/telemetry/lexicon.ts
@@ -48,6 +48,13 @@ interface LexiconEventEntry {
    * Tags to categorize the event in the Mixpanel interface. Typically used to group related events together.
    */
   tags?: LexiconTag[];
+  /**
+   * True if the event should be hidden from the Mixpanel interface. Hidden events are still tracked (granted they are
+   * reported to Mixpanel), but since they are hidden they will be discouraged from being used in reports.
+   *
+   * "Hidden" is the preferred way to deprecate or make an event obsolete, and differs from "dropped" in that it is less
+   * severe; dropped events are intercepted by Mixpanel and not reported at all, and is thus error-prone.
+   */
   hidden?: boolean;
 }
 
