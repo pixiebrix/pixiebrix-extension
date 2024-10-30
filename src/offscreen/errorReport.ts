2021198cba9b4497afeb2db17d741cbf641a8326
Eduardo Fungairino <eduardo@pixiebrix.com>
2024-10-30 11:40:16 -0400

    Refactor Offscreen page to use webext-messenger (#9383)
    * wip
    
    * wip
    
    * fix tests
    
    * fix unrelated e2e
    
    * pr feedback

---
+++ src/offscreen/errorReport.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 PixieBrix, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { deserializeError } from "serialize-error";
+import type { SerializedError } from "@/types/messengerTypes";
+import type { TelemetryUser } from "@/telemetry/telemetryTypes";
+import type { MessageContext } from "@/types/loggerTypes";
+import type { SemVerString } from "@/types/registryTypes";
+
+type errorMessageData = {
+  error: SerializedError;
+  errorMessage: string;
+  errorReporterInitInfo: {
+    versionName: string;
+    telemetryUser: TelemetryUser;
+  };
+  messageContext: MessageContext &
+    UnknownObject & { extensionVersion: SemVerString };
+};
+
+export async function sendErrorToDataDog(
+  data: errorMessageData,
+): Promise<void> {
+  const { error, errorMessage, errorReporterInitInfo, messageContext } = data;
+
+  // WARNING: the prototype chain is lost during deserialization, so make sure any predicates you call here
+  // to determine log level also handle serialized/deserialized errors.
+  // See https://github.com/sindresorhus/serialize-error/issues/48
+
+  const { getErrorReporter } = await import(
+    /* webpackChunkName: "errorReporter" */
+    "@/telemetry/initErrorReporter"
+  );
+
+  const { versionName, telemetryUser } = errorReporterInitInfo;
+  const reporter = await getErrorReporter(versionName, telemetryUser);
+
+  if (!reporter) {
+    // Error reporter not initialized
+    return;
+  }
+
+  reporter.error({
+    message: errorMessage,
+    error: deserializeError(error),
+    messageContext,
+  });
+}
