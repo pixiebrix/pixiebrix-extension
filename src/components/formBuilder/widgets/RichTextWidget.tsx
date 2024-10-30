cb78f526221e8df44f3be44ff34eb3bb35c56fbb
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-10-30 10:20:18 -0700

    Revert "implement heading selection"
    This reverts commit 87f13e84e565b4f0606e845b89c47b28582dd691.

--- src/components/formBuilder/widgets/RichTextWidget.tsx
+++ src/components/formBuilder/widgets/RichTextWidget.tsx
@@ -24,7 +24,6 @@ import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBold, faItalic } from "@fortawesome/free-solid-svg-icons";
 import styles from "./RichTextWidget.module.scss";
-import Select from "react-select";
 
 const Toolbar: React.FunctionComponent = () => {
   const { editor } = useCurrentEditor();
@@ -36,72 +35,6 @@ const Toolbar: React.FunctionComponent = () => {
   return (
     <ButtonToolbar className={styles.toolbar}>
       <ButtonGroup size="sm" aria-label="Rich-Text Editor Toolbar">
-        <ButtonGroup>
-          <Select
-            className={styles.dropdown}
-            value={{
-              value: editor.isActive("heading", { level: 1 })
-                ? "h1"
-                : editor.isActive("heading", { level: 2 })
-                  ? "h2"
-                  : editor.isActive("heading", { level: 3 })
-                    ? "h3"
-                    : "p",
-              label: editor.isActive("heading", { level: 1 })
-                ? "Heading 1"
-                : editor.isActive("heading", { level: 2 })
-                  ? "Heading 2"
-                  : editor.isActive("heading", { level: 3 })
-                    ? "Heading 3"
-                    : "Paragraph",
-            }}
-            onChange={(selectedOption) => {
-              switch (selectedOption?.value) {
-                case "p": {
-                  editor.chain().focus().setParagraph().run();
-                  break;
-                }
-
-                case "h1": {
-                  editor.chain().focus().toggleHeading({ level: 1 }).run();
-                  break;
-                }
-
-                case "h2": {
-                  editor.chain().focus().toggleHeading({ level: 2 }).run();
-                  break;
-                }
-
-                case "h3": {
-                  editor.chain().focus().toggleHeading({ level: 3 }).run();
-                  break;
-                }
-
-                default: {
-                  break;
-                }
-              }
-            }}
-            options={[
-              {
-                value: "p",
-                label: "Paragraph",
-              },
-              {
-                value: "h1",
-                label: "Heading 1",
-              },
-              {
-                value: "h2",
-                label: "Heading 2",
-              },
-              {
-                value: "h3",
-                label: "Heading 3",
-              },
-            ]}
-          />
-        </ButtonGroup>
         <Button
           variant="default"
           onClick={() => editor.chain().focus().toggleBold().run()}
