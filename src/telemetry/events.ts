48c267144278aa482e1d7c2e9ace3b9a7b43f149
Misha Holtz <36575242+mnholtz@users.noreply.github.com>
2024-11-05 10:15:26 -0800

    alphabetize events.ts

--- src/telemetry/events.ts
+++ src/telemetry/events.ts
@@ -16,15 +16,17 @@
  */
 
 export const Events = {
-  ACTIVATION_INTEGRATION_CONFIG_SELECT: "AuthWidgetSelect",
-  ACTIVATION_INTEGRATION_REFRESH: "AuthWidgetRefresh",
   ACTIVATION_INTEGRATION_ADD_NEW_CLICK: "AuthWidgetShowAddNew",
   ACTIVATION_INTEGRATION_ADD_NEW_CLOSE: "AuthWidgetHideAddNew",
+  ACTIVATION_INTEGRATION_CONFIG_SELECT: "AuthWidgetSelect",
+  ACTIVATION_INTEGRATION_REFRESH: "AuthWidgetRefresh",
 
   BRICK_ADD: "BrickAdd",
   BRICK_COMMENTS_UPDATE: "BrickCommentsUpdate",
 
   BROWSER_ACTION_RESTRICTED_URL: "BrowserActionRestrictedUrl",
+
+  BUTTON_CLICK: "MenuItemClick",
 
   CUSTOM_USER_EVENT: "CustomUserEvent",
 
@@ -33,20 +35,20 @@ export const Events = {
   DEPLOYMENT_ACTIVATE: "DeploymentActivate",
   DEPLOYMENT_DEACTIVATE_ALL: "DeploymentDeactivateAll",
   DEPLOYMENT_DEACTIVATE_UNASSIGNED: "DeploymentDeactivateUnassigned",
-  DEPLOYMENT_REJECT_VERSION: "DeploymentRejectVersion",
-  DEPLOYMENT_REJECT_PERMISSIONS: "DeploymentRejectPermissions",
-  DEPLOYMENT_SYNC: "DeploymentSync",
   DEPLOYMENT_LIST: "DeploymentList",
+  DEPLOYMENT_REJECT_PERMISSIONS: "DeploymentRejectPermissions",
+  DEPLOYMENT_REJECT_VERSION: "DeploymentRejectVersion",
+  DEPLOYMENT_SYNC: "DeploymentSync",
   DEPLOYMENT_UPDATE_LIST: "DeploymentUpdateList",
 
-  DEVTOOLS_OPEN: "DevToolsOpen",
   DEVTOOLS_CLOSE: "DevToolsClose",
+  DEVTOOLS_OPEN: "DevToolsOpen",
 
   FACTORY_RESET: "FactoryReset",
 
   FLOATING_ACTION_BUTTON_CLICK: "FloatingQuickBarButtonClick",
-  FLOATING_ACTION_BUTTON_REPOSITION: "FloatingQuickBarButtonRepositioned",
   FLOATING_ACTION_BUTTON_ON_SCREEN_HIDE: "FloatingQuickBarButtonOnScreenHide",
+  FLOATING_ACTION_BUTTON_REPOSITION: "FloatingQuickBarButtonRepositioned",
 
   GOOGLE_FILE_PICKER_EVENT: "GoogleFilePickerEvent",
 
@@ -57,69 +59,63 @@ export const Events = {
   IDB_RECOVER_CONNECTION: "IDBRecoverConnection",
   IDB_UNRESPONSIVE_BANNER: "IDBUnresponsiveBanner",
 
-  INTEGRATION_WIDGET_SELECT: "IntegrationWidgetSelect",
+  INTEGRATION_ADD: "ServiceAdd",
   INTEGRATION_WIDGET_CLEAR: "IntegrationWidgetClear",
-  INTEGRATION_WIDGET_REFRESH: "IntegrationWidgetRefresh",
   INTEGRATION_WIDGET_CONFIGURE_LINK_CLICK:
     "IntegrationWidgetConfigureLinkClick",
-
-  INTEGRATION_ADD: "ServiceAdd",
+  INTEGRATION_WIDGET_REFRESH: "IntegrationWidgetRefresh",
+  INTEGRATION_WIDGET_SELECT: "IntegrationWidgetSelect",
 
   LINK_EXTENSION: "LinkExtension",
 
   MARKETPLACE_ACTIVATE: "MarketplaceActivate",
   MARKETPLACE_REJECT_PERMISSIONS: "MarketplaceRejectPermissions",
 
-  BUTTON_CLICK: "MenuItemClick",
+  MODS_PAGE_VIEW: "BlueprintsPageView",
 
   MOD_ACTIVATE: "InstallBlueprint",
   MOD_ACTIVATION_CANCEL: "CancelModActivation",
   MOD_ACTIVATION_SUBMIT: "SubmitModActivation",
-  MOD_CREATE_NEW: "ExtensionAddNew",
   MOD_ADD_STARTER_BRICK: "ModAddStarterBrick",
   MOD_COMPONENT_CLOUD_ACTIVATE: "ExtensionCloudActivate",
   MOD_COMPONENT_REMOVE: "ExtensionRemove",
+  MOD_CREATE_NEW: "ExtensionAddNew",
   MOD_REMOVE: "BlueprintRemove",
 
-  PACKAGE_DELETE: "BrickDelete",
-
-  MODS_PAGE_VIEW: "BlueprintsPageView",
-
+  OAUTH2_LOGIN_ERROR: "OAuth2LoginError",
   OAUTH2_LOGIN_START: "OAuth2LoginStart",
   OAUTH2_LOGIN_SUCCESS: "OAuth2LoginSuccess",
-  OAUTH2_LOGIN_ERROR: "OAuth2LoginError",
 
   ORGANIZATION_EXTENSION_LINK: "OrganizationExtensionLink",
 
-  PAGE_EDITOR_MOD_COMPONENT_UPDATE: "PageEditorCreate",
-  PAGE_EDITOR_OPEN: "PageEditorOpen",
+  PACKAGE_DELETE: "BrickDelete",
+  PANEL_ADD: "PanelAdd",
+
+  PAGE_EDITOR_CLEAR_CHANGES: "PageEditorReset",
   PAGE_EDITOR_MANUAL_RUN: "PageEditorManualRun",
   PAGE_EDITOR_MOD_COMPONENT_ERROR: "PageEditorExtensionError",
   PAGE_EDITOR_MOD_COMPONENT_REMOVE: "PageEditorRemove",
-  PAGE_EDITOR_CLEAR_CHANGES: "PageEditorReset",
-  PAGE_EDITOR_VIEW_TEMPLATES: "PageEditorViewTemplates",
+  PAGE_EDITOR_MOD_COMPONENT_UPDATE: "PageEditorCreate",
   PAGE_EDITOR_MOD_CREATE: "PageEditorModCreate",
-  PAGE_EDITOR_MOD_UPDATE: "PageEditorModUpdate",
   PAGE_EDITOR_MOD_SAVE_ERROR: "PageEditorModSaveError",
-
-  PAGE_EDITOR_SESSION_START: "PageEditorSessionStart",
+  PAGE_EDITOR_MOD_UPDATE: "PageEditorModUpdate",
+  PAGE_EDITOR_OPEN: "PageEditorOpen",
   PAGE_EDITOR_SESSION_END: "PageEditorSessionEnd",
-
+  PAGE_EDITOR_SESSION_START: "PageEditorSessionStart",
+  PAGE_EDITOR_VIEW_TEMPLATES: "PageEditorViewTemplates",
   PAGE_EDITOR_WALKTHROUGH_LINK_CLICK: "PageEditorWalkthroughLinkClick",
-  PAGE_EDITOR_WALKTHROUGH_MODAL_VIEW: "PageEditorWalkthroughModalView",
   PAGE_EDITOR_WALKTHROUGH_MODAL_CLOSE: "PageEditorWalkthroughModalClose",
+  PAGE_EDITOR_WALKTHROUGH_MODAL_VIEW: "PageEditorWalkthroughModalView",
 
-  VAR_POPOVER_SHOW: "VarPopoverShow",
-  VAR_POPOVER_SELECT: "VarPopoverSelect",
-
-  PANEL_ADD: "PanelAdd",
+  PERFORMANCE_MESSENGER_MANY_TABS_BROADCAST:
+    "PerformanceMessengerManyTabsBroadcast",
 
   PIXIEBRIX_INSTALL: "PixieBrixInstall",
   PIXIEBRIX_RELOAD: "PixieBrixReload",
   PIXIEBRIX_UPDATE: "PixieBrixUpdate",
 
-  SCHEMA_SELECT_WIDGET_SELECT: "SchemaSelectWidgetSelect",
   SCHEMA_SELECT_WIDGET_CLEAR: "SchemaSelectWidgetClear",
+  SCHEMA_SELECT_WIDGET_SELECT: "SchemaSelectWidgetSelect",
 
   SELECT_GOOGLE_SPREADSHEET_CANCELLED: "SelectGoogleSpreadsheetCancelled",
   SELECT_GOOGLE_SPREADSHEET_ENSURE_TOKEN_START:
@@ -132,31 +128,29 @@ export const Events = {
   SELECT_GOOGLE_SPREADSHEET_START: "SelectGoogleSpreadsheetStart",
   SELECT_GOOGLE_SPREADSHEET_VIEW_WARNING: "SelectGoogleSpreadsheetViewWarning",
 
+  SETTINGS_EXPERIMENTAL_CONFIGURE: "SettingsExperimentalConfigure",
   SIDEBAR_HIDE: "SidePanelHide",
   SIDEBAR_SHOW: "SidePanelShow",
   SIDEBAR_TAB_CLOSE: "SidebarTabClose",
 
+  SHORTCUT_SNIPPET_RUN: "TextCommandRun",
   SNOOZE_UPDATES: "SnoozeUpdates",
-  SETTINGS_EXPERIMENTAL_CONFIGURE: "SettingsExperimentalConfigure",
-
   START_MOD_ACTIVATE: "StartInstallBlueprint",
 
+  TOUR_END: "TourEnd",
   TOUR_START: "TourStart",
   TOUR_STEP: "TourStep",
-  TOUR_END: "TourEnd",
 
   TRIGGER_RUN: "TriggerRun",
-  PERFORMANCE_MESSENGER_MANY_TABS_BROADCAST:
-    "PerformanceMessengerManyTabsBroadcast",
 
   UNINITIALIZED_GAPI_GATE_VIEW: "UninitializedGapiGateView",
-
   UNSUPPORTED_BROWSER_GATE_VIEW: "UnsupportedBrowserGateView",
+
+  VAR_POPOVER_SELECT: "VarPopoverSelect",
+  VAR_POPOVER_SHOW: "VarPopoverShow",
 
   VIEW_ERROR: "ViewError",
   VIEW_SIDEBAR_PANEL: "ViewSidePanelPanel",
 
   ZAPIER_KEY_COPY: "ZapierKeyCopy",
-
-  SHORTCUT_SNIPPET_RUN: "TextCommandRun",
 } as const;
